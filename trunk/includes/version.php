bfa81349217cacef9cbbfa912d23a6eb76330107
pedroix <pedroix@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2008-02-13 03:52:13 +0000

    Updated version nr to 0.9.8 Public BETA Test
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@47 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/includes/version.php
+++ trunk/includes/version.php
@@ -6,7 +6,7 @@ if (!defined('W2P_BASE_DIR')) {
 // Set the version components separately.
 $w2p_version_major = 0;
 $w2p_version_minor = 9;
-$w2p_version_patch = 7; // Can be set to null if not used
+$w2p_version_patch = 8; // Can be set to null if not used
 $w2p_version_prepatch = null; // Set to null if a release version.
 
 ?>
