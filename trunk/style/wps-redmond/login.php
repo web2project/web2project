7100410e4cb882b2a5fa0d147b0b02badfdb7b26
pedroix <pedroix@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2008-05-13 09:44:06 +0000

    Branding issues
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@163 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/style/wps-redmond/login.php
+++ trunk/style/wps-redmond/login.php
@@ -58,8 +58,8 @@
 echo '<span class="error">' . $AppUI->getMsg() . '</span>';
 
 $msg = '';
-$msg .= phpversion() < '4.1' ? '<br /><span class="warning">WARNING: dotproject is NOT SUPPORT for this PHP Version (' . phpversion() . ')</span>' : '';
-$msg .= function_exists('mysql_pconnect') ? '' : '<br /><span class="warning">WARNING: PHP may not be compiled with MySQL support.  This will prevent proper operation of dotProject.  Please check you system setup.</span>';
+$msg .= phpversion() < '4.1' ? '<br /><span class="warning">WARNING: web2Project is NOT SUPPORTED for this PHP Version (' . phpversion() . ')</span>' : '';
+$msg .= function_exists('mysql_pconnect') ? '' : '<br /><span class="warning">WARNING: PHP may not be compiled with MySQL support.  This will prevent proper operation of web2Project.  Please check you system setup.</span>';
 echo $msg;
 ?>
 </div>
