394bbe4ae285252e327d2e0df1d428da8157984f
pedroix <pedroix@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2008-08-15 23:30:11 +0000

    Removed a duplicate title tag
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@207 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- newuser.php
+++ newuser.php
@@ -5,7 +5,7 @@ $uistyle = 'web2project';
 	   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<title><?php echo $AppUI->_('New User Signup'); ?></title>
+	<title><?php echo 'New User Signup'; ?></title>
 	<meta http-equiv="Content-Type" content="text/html;charset=<?php echo 'UTF-8'; ?>" />
 	<meta http-equiv="Pragma" content="no-cache" />
 	<link rel="stylesheet" type="text/css" href="./style/<?php echo $uistyle; ?>/main.css" media="all" />
