b9042fe7052fcb06204f496d95b0bd56910c457b
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2008-08-25 15:40:48 +0000

    converts all the sysval groups into their individual sysvals;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@215 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

---
+++ trunk/install/convert.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+If you don't do this the sysval will only have one record.
+The other way of solving this is to create script to do that for the user something like (this is not tested and is only for mental reference):
+*/
+require_once '../base.php';
+require_once '/home/dpserver/public_html/cs' . '/includes/config.php';
+
+//require_once '/home/dpserver/public_html/cs' . '/includes/main_functions.php';
+require_once '/home/dpserver/public_html/cs' . '/includes/db_adodb.php';
+require_once '/home/dpserver/public_html/cs' . '/classes/ui.class.php';
+require_once '/home/dpserver/public_html/cs' . '/classes/permissions.class.php';
+require_once '/home/dpserver/public_html/cs' . '/includes/session.php';
+
+$_SESSION['AppUI'] = new CAppUI;
+$AppUI = &$_SESSION['AppUI'];
+
+$q = new DBQuery();
+$q->addTable('sysvals');
+$sysvals = $q->loadList();
+$q->clear();
+require_once ('../modules/system/syskeys/syskeys.class.php');
+foreach ($sysvals as $sysval) {
+    $sysval_obj = new CSysVal();
+    $sysval_obj->bind($sysval);
+//print_r($sysval_obj);
+    $sysval_obj->store();
+}
+
+/* 
+ *  Basically  we capture all the records from the sysvals table and we store
+ * them using the new sysvals schema which does the separation of the values
+ * into the sysvals table back again.
+ */
+
+?>
