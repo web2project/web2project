2ae0ff3c851c9cd65529853580a9de0b063f939d
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-01-26 04:50:28 +0000

    cleanup for the install script;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@242 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/includes/config-dist.php
+++ trunk/includes/config-dist.php
@@ -2,57 +2,32 @@
 if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
-/*
-Copyright (c) 2007-2008 The web2Project Development Team <w2p-developers@web2project.net>
-Copyright (c) 2003-2005 The dotProject Development Team <core-developers@dotproject.net>
-
-This file is part of web2Project.
-
-web2Project is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-web2Project is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with web2Project; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-The full text of the GPL is in the COPYING file.
-*/
 
 /*
-* * * INSTALLATION INSTRUCTIONS * * *
+	Copyright (c) 2007-2008 The web2Project Development Team <w2p-developers@web2project.net>
+	Copyright (c) 2003-2005 The dotProject Development Team <core-developers@dotproject.net>
 
-Point your browser to install/index.php and follow the prompts.
-It is no longer necessary to manually create this file unless
-the web server cannot write to the includes directory.
-
+	* *	 * INSTALLATION INSTRUCTIONS * * *
+	Point your browser to install/index.php and follow the prompts. It is no 
+	longer necessary to manually create config.php unless the web server cannot 
+	write to the includes directory.
+		
+	Regardless, this file is used as a template for the autogeneration of the 
+	config.php during the installation process and should not by modified by hand.
 */
 
-// DATABASE ACCESS INFORMATION [DEFAULT example]
-// Modify these values to suit your local settings
-
-$w2Pconfig['dbtype'] = 'mysql'; // ONLY MySQL is supported at present
-$w2Pconfig['dbhost'] = 'localhost';
-$w2Pconfig['dbname'] = 'web2project'; // Change to match your web2Project Database Name
-$w2Pconfig['dbuser'] = 'w2p_user'; // Change to match your MySQL Username
-$w2Pconfig['dbpass'] = 'w2p_pass'; // Change to match your MySQL Password
+$w2Pconfig['dbtype'] = '[DBTYPE]'; // ONLY MySQL is supported at present
+$w2Pconfig['dbhost'] = '[DBHOST]';
+$w2Pconfig['dbname'] = '[DBNAME]'; // Change to match your web2Project Database Name
+$w2Pconfig['dbuser'] = '[DBUSER]'; // Change to match your MySQL Username
+$w2Pconfig['dbpass'] = '[DBPASS]'; // Change to match your MySQL Password
 $w2Pconfig['dbprefix'] = ''; // Change to match the prefix used for db table names, example: $w2Pconfig['dbprefix'] = 'w2p_prefix_';
 
 // set this value to true to use persistent database connections
 $w2Pconfig['dbpersist'] = false;
 
 /***************** Configuration for DEVELOPERS use only! ******/
-// Root directory is now automatically set to avoid
-// getting it wrong.
+// Root directory and base_url are automatically set to avoid
+// getting them wrong.  They are not the same.
 $w2Pconfig['root_dir'] = W2P_BASE_DIR;
-
-// Base Url is now automatically set to avoid
-// getting it wrong.
 $w2Pconfig['base_url'] = W2P_BASE_URL;
-?>
