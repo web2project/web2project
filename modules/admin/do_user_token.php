b46b0e54d4e4b823924ce0848e400078eb91a179
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-02-13 20:53:52 +0000

    added the calendar feed to the user view page; added token generation for the calendar feed; added the corresponding action handler for all the fun;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@278 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

---
+++ modules/admin/do_user_token.php
@@ -0,0 +1,12 @@
+<?php
+
+	$token = w2PgetParam($_POST, 'token', '');
+	$userId = intval(w2PgetParam($_POST, 'user_id', 0));
+
+	if ($userId > 0) {
+		CUser::generateUserToken($userId, $token);
+		$AppUI->redirect( 'm=admin&a=viewuser&user_id='.$userId );
+	} else {
+		$AppUI->redirect( 'm=admin' );
+	}
+?>
