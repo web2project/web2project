9690e0e1af2c314ebb65a9808bafa8416a43dfe6
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-02-23 05:23:39 +0000

    implemented the getDepartments, getUsers, and getContacts static methods; cleaned up the company/contacts view to use the new methods;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@323 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- modules/companies/vw_contacts.php
+++ modules/companies/vw_contacts.php
@@ -12,58 +12,36 @@ global $AppUI, $company_id, $obj;
 // assemble the sql statement
 require_once $AppUI->getModuleClass('contacts');
 require_once $AppUI->getModuleClass('departments');
-$department = &new CDepartment;
-$allowedDepartments = $department->getAllowedSQL($AppUI->user_id);
-$q = new DBQuery;
-$q->addQuery('a.*');
-$q->addQuery('dept_name');
-$q->addTable('contacts', 'a');
-$q->leftJoin('companies', 'b', 'a.contact_company = b.company_id');
-$q->leftJoin('departments', '', 'contact_department = dept_id');
-$q->addWhere('contact_company = ' . (int)$obj->company_id);
-$q->addWhere('
-	(contact_private=0
-		OR (contact_private=1 AND contact_owner=' . $AppUI->user_id . ')
-		OR contact_owner IS NULL OR contact_owner = 0
-	)');
-if (count($allowedDepartments)) {
-	$dpt_where = implode(' AND ', $allowedDepartments);
-	$q->addWhere('( (' . $dpt_where . ') OR contact_department = 0 )');
-}
-$q->addOrder('contact_first_name');
-$q->addOrder('contact_last_name');
 
-$s = '';
-if (!($rows = $q->loadList())) {
-	echo '<table width="100%" border="0" cellpadding="2" cellspacing="1" class="tbl">';
-	echo $AppUI->_('No data available') . '<br />' . $AppUI->getMsg();
-} else {
-?>
-<table width="100%" border="0" cellpadding="2" cellspacing="1" class="tbl">
-<tr>
-	<th><?php echo $AppUI->_('Name'); ?></td>
-	<th><?php echo $AppUI->_('e-mail'); ?></td>
-	<th><?php echo $AppUI->_('Phone'); ?></td>
-	<th><?php echo $AppUI->_('Department'); ?></td>
-</tr>
-<?php
-	foreach ($rows as $row) {
-		$contact = &new CContact;
-		$contact->bind($row);
-		$dept_detail = $contact->getDepartmentDetails();
+$contacts = CCompany::getContacts($AppUI, $obj->company_id);
 
-		$s .= '<tr><td>';
-		$s .= '<a href="./index.php?m=contacts&a=view&contact_id=' . $row['contact_id'] . '">' . $row['contact_first_name'] . ' ' . $row['contact_last_name'] . '</a>';
-		$s .= '<td><a href="mailto:' . $row['contact_email'] . '">' . $row['contact_email'] . '</a></td>';
-		$s .= '<td>' . $row['contact_phone'] . '</td>';
-		$s .= '<td>' . $dept_detail['dept_name'] . '</td>';
-		$s .= '</tr>';
+?><table width="100%" border="0" cellpadding="2" cellspacing="1" class="tbl"><?php
+if (count($contacts) > 0) {
+	?>
+	<tr>
+		<th><?php echo $AppUI->_('Name'); ?></th>
+		<th><?php echo $AppUI->_('e-mail'); ?></th>
+		<th><?php echo $AppUI->_('Phone'); ?></th>
+		<th><?php echo $AppUI->_('Department'); ?></th>
+	</tr>
+	<?php
+	foreach ($contacts as $contact_id => $contact_data) {
+		echo '<tr><td class="hilite">';
+		echo $contact_data['contact_first_name'] . ' ' . $contact_data['contact_last_name'];
+		echo '</td>';
+		echo '<td class="hilite"><a href="mailto:' . $contact_data['contact_email'] . '">' . $contact_data['contact_email'] . '</a></td>';
+		echo '<td class="hilite">' . $contact_data['contact_phone'] . '</td>';
+		echo '<td class="hilite">' . $contact_data['dept_name'] . '</td>';
+		echo '</tr>';
 	}
+} else {
+	?><tr><td colspan="5"><?php echo $AppUI->_('No data available') . '<br />' . $AppUI->getMsg(); ?></td></tr><?php
 }
-
-$s .= '<tr><td colspan="4" align="right" valign="top" style="background-color:#ffffff">';
-$s .= '<input type="button" class=button value="' . $AppUI->_('new contact') . '" onClick="javascript:window.location=\'./index.php?m=contacts&a=addedit&company_id=' . $company_id . '&company_name=' . $obj->company_name . '\'">';
-$s .= '</td></tr>';
-$s .= '</table>';
-echo $s;
 ?>
+
+	<tr>
+		<td colspan="4" align="right" valign="top" style="background-color:#ffffff">
+			<input type="button" class=button value="<?php echo $AppUI->_('new contact') ?>" onClick="javascript:window.location='./index.php?m=contacts&a=addedit&company_id=<?php echo $company_id; ?>&company_name=<?php echo $obj->company_name; ?>'">
+		</td>
+	</tr>
+</table>
