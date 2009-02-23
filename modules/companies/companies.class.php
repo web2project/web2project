3c16ba08227b9f68c6de632e59b30ad17ef8bb8f
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-02-23 03:53:00 +0000

    added a loadFull method similar to other classes; changed $obj to $company to be meaningful; removed the custom user query to use our standard w2pgetUsers function;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@319 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- modules/companies/companies.class.php
+++ modules/companies/companies.class.php
@@ -71,5 +71,18 @@ class CCompany extends CW2pObject {
 		// call the parent class method to assign the oid
 		return CW2pObject::canDelete($msg, $oid, $tables);
 	}
+	
+	public function loadFull($companyId) {
+		$q = new DBQuery;
+		$q->addTable('companies');
+		$q->addQuery('companies.*');
+		$q->addQuery('con.contact_first_name');
+		$q->addQuery('con.contact_last_name');
+		$q->leftJoin('users', 'u', 'u.user_id = companies.company_owner');
+		$q->leftJoin('contacts', 'con', 'u.user_contact = con.contact_id');
+		$q->addWhere('companies.company_id = ' . (int) $companyId);
+
+		$q->loadObject($this, true, false);
+	}
 }
 ?>
