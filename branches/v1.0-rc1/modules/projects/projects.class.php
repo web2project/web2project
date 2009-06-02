ddddd5302eea7ac744ba9e4c187ec35569d29e77
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-06-02 17:38:04 +0000

    resolved an issue where you could only view Gantt charts for Active Projects, not inactive; detailed here - http://twitter.com/sweatje/statuses/2003902012
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@426 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- branches/v1.0-rc1/modules/projects/projects.class.php
+++ branches/v1.0-rc1/modules/projects/projects.class.php
@@ -622,11 +622,13 @@ class CProject extends CW2pObject {
 		}
 		return '';
 	}
-	public function getAllowedProjects($userId) {
+	public function getAllowedProjects($userId, $activeOnly = true) {
 		$q = new DBQuery;
 		$q->addTable('projects', 'pr');
 		$q->addQuery('pr.project_id, project_color_identifier, project_name, project_start_date, project_end_date');
-		$q->addWhere('project_active = 1');
+		if ($activeOnly) {
+			$q->addWhere('project_active = 1');
+		}
 		$q->addGroup('pr.project_id');
 		$q->addOrder('project_name');
 		$this->setAllowedSQL($userId, $q, null, 'pr');
