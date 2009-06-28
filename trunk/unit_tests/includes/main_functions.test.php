b1a04f8d470d5d73eba604bf6b93b34148609d2d
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-06-28 15:33:22 +0000

    added some unit tests to cover main_function changes included in r458;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@460 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

---
+++ trunk/unit_tests/includes/main_functions.test.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Necessary global variables 
+ */
+global $db;
+global $ADODB_FETCH_MODE;
+global $w2p_performance_dbtime;
+global $w2p_performance_old_dbqueries;
+global $AppUI;
+
+require_once '../base.php';
+require_once W2P_BASE_DIR . '/includes/config.php';
+require_once W2P_BASE_DIR . '/includes/main_functions.php';
+require_once W2P_BASE_DIR . '/includes/db_adodb.php';
+require_once W2P_BASE_DIR . '/classes/ui.class.php';
+require_once W2P_BASE_DIR . '/classes/query.class.php';
+
+// Need this to test actions that require permissions.
+$AppUI  = new CAppUI;
+$_POST['login'] = 'login';
+$_REQUEST['login'] = 'sql';
+$AppUI->login('admin', 'passwd');
+
+require_once W2P_BASE_DIR . '/classes/permissions.class.php';
+require_once W2P_BASE_DIR . '/includes/session.php';
+require_once W2P_BASE_DIR . '/classes/CustomFields.class.php';
+require_once W2P_BASE_DIR . '/modules/companies/companies.class.php';
+require_once W2P_BASE_DIR . '/modules/projects/projects.class.php';
+require_once W2P_BASE_DIR . '/modules/departments/departments.class.php';
+require_once 'PHPUnit/Framework.php';
+/**
+ * DateTest Class.
+ * 
+ * Class to test the date include
+ * @author D. Keith Casey, Jr.
+ * @package web2project
+ * @subpackage unit_tests
+ */
+class Main_Functions_Test extends PHPUnit_Framework_TestCase 
+{
+	public function testW2PgetParam()
+	{
+		global $AppUI;
+		$params = array('m' => 'projects', 'a' => 'view', 'v' => '<script>alert</script>');
+		
+		//echo w2PgetParam($params, 'm');
+		$this->assertEquals('projects', w2PgetParam($params, 'm'));
+
+		$this->assertEquals('', w2PgetParam($params, 'NotGonnaBeThere'));
+
+		$this->assertEquals('Some Default', w2PgetParam($params, 'NotGonnaBeThere', 'Some Default'));
+		
+		$this->markTestIncomplete("Currently w2PgetParam redirects for tainted names.. what do we do there?");
+		
+		$this->markTestIncomplete("Currently w2PgetParam redirects for tainted values.. what do we do there?");
+	}
+}
