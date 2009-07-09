0bcf789e4bec06665841a17add2829dbf65d727d
pedroix <pedroix@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-07-09 11:00:26 +0000

    1) Just load the locale files if they exist instead of trying to read them and sending php error logs if they don't exist. 2) Fix the .inc files path structure so it matches the given instructions on core.php for custom modules. Before, paths like modules/custom_module/locales/??.inc (where ?? are the two letters of the locale) were right but were not matching the standard given on the instructions of locales/core.php and should be (and now are) modules/custom_module/locales/??/custom_module.inc This will also make it easier if a custom module is to be integrated into core. Just need to copy/paste the .inc file to /locales/??/custom_module.inc So custom module developers beware of this! 3) A nice example of this is the fact that projectdesigner module was built this way so we are removing the old locales folder from the projectdesigner module (and its .inc files) 4) And since it is a core module now, we go and place those .inc files in the right /locales/??/ folders
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@503 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/locales/core.php
+++ trunk/locales/core.php
@@ -5,24 +5,31 @@ if (!defined('W2P_BASE_DIR')) {
 
 ob_start();
 
-@readfile(W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/common.inc');
+
+if (W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/common.inc') {
+	readfile(W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/common.inc');
+}
 
 // language files for specific locales and specific modules (for external modules) should be
 // put in modules/[the-module]/locales/[the-locale]/[the-module].inc
 // this allows for module specific translations to be distributed with the module
 
-if (file_exists(W2P_BASE_DIR . '/modules/' . $m . '/locales/' . $AppUI->user_locale . '.inc')) {
-	@readfile(W2P_BASE_DIR . '/modules/' . $m . '/locales/' . $AppUI->user_locale . '.inc');
-} else {
-	@readfile(W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/' . $m . '.inc');
+if (file_exists(W2P_BASE_DIR . '/modules/' . $m . '/locales/' . $AppUI->user_locale . '/' . $m . '.inc')) {
+	readfile(W2P_BASE_DIR . '/modules/' . $m . '/locales/' . $AppUI->user_locale . '/' . $m . '.inc');
+} elseif (file_exists(W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/' . $m . '.inc')) {
+	readfile(W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/' . $m . '.inc');
 }
 
 switch ($m) {
 	case 'departments':
-		@readfile(W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/companies.inc');
+		if (file_exists(W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/companies.inc')) {
+			readfile(W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/companies.inc');
+		}
 		break;
 	case 'system':
-		@readfile(W2P_BASE_DIR . '/locales/' . $w2Pconfig['host_locale'] . '/styles.inc');
+		if (file_exists(W2P_BASE_DIR . '/locales/' . $w2Pconfig['host_locale'] . '/styles.inc')) {
+			readfile(W2P_BASE_DIR . '/locales/' . $w2Pconfig['host_locale'] . '/styles.inc');
+		}
 		break;
 }
 eval('$GLOBALS[\'translate\']=array(' . ob_get_contents() . "\n'0');");
