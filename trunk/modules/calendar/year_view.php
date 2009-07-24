b757c28075bbda5907bd312990011e500a54c2da
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-07-24 03:55:24 +0000

    cleaned up and eliminated a bunch of duplicate code for the year view;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@520 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/modules/calendar/year_view.php
+++ trunk/modules/calendar/year_view.php
@@ -17,6 +17,8 @@ w2PsetMicroTime();
 
 require_once ($AppUI->getModuleClass('companies'));
 require_once ($AppUI->getModuleClass('tasks'));
+require_once (w2PgetConfig('root_dir') . "/modules/calendar/links_tasks.php");
+require_once (w2PgetConfig('root_dir') . "/modules/calendar/links_events.php");
 
 // retrieve any state parameters
 if (isset($_REQUEST['company_id'])) {
@@ -35,7 +37,6 @@ $company = new CCompany();
 $companies = $company->getAllowedRecords($AppUI->user_id, 'company_id,company_name', 'company_name');
 $companies = arrayMerge(array('0' => $AppUI->_('All')), $companies);
 
-#echo '<pre>';print_r($events);echo '</pre>';
 // setup the title block
 $titleBlock = new CTitleBlock('Yearly Calendar', 'myevo-appointments.png', $m, "$m.$a");
 $titleBlock->addCell($AppUI->_('Company') . ':');
@@ -70,293 +71,91 @@ $next_year = (int)($next_year + 10000);
 
 ?>
 <table class="std" width="100%" cellspacing="0" cellpadding="0">
-<tr>
-	<td>
-		<table width="100%" cellspacing="0" cellpadding="4">
-		<tr>
-			<td colspan="20" valign="top">
-		    	<table border="0" cellspacing="1" cellpadding="2" width="100%" class="motitle">
-		        	<tr>
-		            	<td>
-		                	<a href="<?php echo '?m=calendar&a=year_view&date=' . $prev_year; ?>"><img src="<?php echo w2PfindImage('prev.gif'); ?>" width="16" height="16" alt="pre" title="pre" border="0"></a>
-		                </td>
-		                <th width="100%" align="center">
-		                	<?php echo htmlspecialchars($date->format('%Y')); ?>
-		                </th>
-		                <td>
-		                	<a href="<?php echo '?m=calendar&a=year_view&date=' . $next_year; ?>"><img src="<?php echo w2PfindImage('next.gif'); ?>" width="16" height="16" alt="next" title="next" border="0"></a>
-		                </td>
-		            </tr>
-		        </table>
-		    </td>
-		</tr>
-<?php
-$minical = new CMonthCalendar($date);
-$minical->setStyles('minititle', 'minical');
-$minical->showArrows = false;
-$minical->showWeek = true;
-$minical->clickMonth = true;
-$minical->setLinkFunctions('clickDay', 'clickWeek');
-// prepare time period for 1st minical'events'
-require_once (w2PgetConfig('root_dir') . "/modules/calendar/links_tasks.php");
-require_once (w2PgetConfig('root_dir') . "/modules/calendar/links_events.php");
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-
-$links = array();
-
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-
-echo '<table cellspacing="0" cellpadding="0" border="0" width="100%"><tr>';
-echo '<td valign="top" align="center" width="20%">&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-
-$date->addMonths(1);
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-$links = array();
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-$minical->setDate($date);
-echo '<td valign="top" align="center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-
-$date->addMonths(1);
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-$links = array();
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-$minical->setDate($date);
-echo '<td valign="top" align="center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-
-$date->addMonths(1);
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-$links = array();
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-$minical->setDate($date);
-echo '<td valign="top" align="center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-echo '<td valign="top" align="center" width="20%">&nbsp;</td>';
-echo '</tr></table>';
-
-$date->addMonths(1);
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-$links = array();
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-$minical->setDate($date);
-echo '<table cellspacing="0" cellpadding="0" border="0" width="100%"><tr>';
-echo '<td valign="top" align="center" width="20%">&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-
-$date->addMonths(1);
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-$links = array();
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-$minical->setDate($date);
-echo '<td valign="top" align="center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-
-$date->addMonths(1);
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-$links = array();
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-$minical->setDate($date);
-
-echo '<td valign="top" align="center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-
-$date->addMonths(1);
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-$links = array();
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-$minical->setDate($date);
-echo '<td valign="top" align="center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-echo '<td valign="top" align="center" width="20%">&nbsp;</td>';
-echo '</tr></table>';
-
-$date->addMonths(1);
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-$links = array();
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-$minical->setDate($date);
-echo '<table cellspacing="0" cellpadding="0" border="0" width="100%"><tr>';
-echo '<td valign="top" align="center" width="20%">&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-
-$date->addMonths(1);
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-$links = array();
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-$minical->setDate($date);
-
-echo '<td valign="top" align="center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-
-$date->addMonths(1);
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-$links = array();
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-$minical->setDate($date);
-echo '<td valign="top" align="center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-
-$date->addMonths(1);
-$first_time = new CDate($date);
-$first_time->setDay(1);
-$first_time->setTime(0, 0, 0);
-$last_time = new CDate($date);
-$last_time->setDay($date->getDaysInMonth());
-$last_time->setTime(23, 59, 59);
-$links = array();
-// assemble the links for the tasks
-// assemble the links for the events
-//Pedro A.
-getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
-getEventLinks($first_time, $last_time, $links, 20, true);
-$minical->setEvents($links);
-$minical->setDate($date);
-echo '<td valign="top" align="center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>';
-echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
-echo '<td valign="top" align="center" width="20%">&nbsp;</td>';
-echo '</tr></table>';
-?>
-	</td>
-</tr>
-<tr>
-	<td>
-		<table width="100%" class="minical">
-		<tr>
-		  	 <td nowrap="nowrap"><?php echo $AppUI->_('Key'); ?>:</td>
-		     <td>&nbsp;</td>
-		     <td style="border-style:solid;border-width:1px" class="day">&nbsp;&nbsp;</td>
-		     <td nowrap="nowrap"><?php echo $AppUI->_('Day'); ?></td>
-		     <td>&nbsp;</td>
-		     <td style="border-style:solid;border-width:1px" class="event">&nbsp;&nbsp;</td>
-		     <td nowrap="nowrap"><?php echo $AppUI->_('Event'); ?></td>
-		     <td>&nbsp;</td>
-		     <td style="border-style:solid;border-width:1px" class="task">&nbsp;&nbsp;</td>
-		     <td nowrap="nowrap"><?php echo $AppUI->_('Task'); ?></td>
-		     <td>&nbsp;</td>
-		     <td style="border-style:solid;border-width:1px" class="eventtask">&nbsp;&nbsp;</td>
-		     <td nowrap="nowrap"><?php echo $AppUI->_('Event'); ?>+<?php echo $AppUI->_('Task'); ?></td>
-		     <td>&nbsp;</td>
-		     <td style="border-style:solid;border-width:1px" class="weekend">&nbsp;&nbsp;</td>
-		     <td nowrap="nowrap"><?php echo $AppUI->_('Weekend'); ?></td>
-		     <td>&nbsp;</td>
-		     <td class="today">&nbsp;&nbsp;</td>
-		     <td nowrap="nowrap"><?php echo $AppUI->_('Today'); ?></td>
-		     <td>&nbsp;</td>
-		     <td width="40%">&nbsp;</td>
-		</tr>
-		</table>
-	</td>
-</tr>
+	<tr>
+		<td align="center">
+			<table width="100%" cellspacing="0" cellpadding="4">
+				<tr>
+					<td colspan="20" valign="top">
+				    	<table border="0" cellspacing="1" cellpadding="2" width="100%" class="motitle">
+				        	<tr>
+				            	<td>
+				                	<a href="<?php echo '?m=calendar&a=year_view&date=' . $prev_year; ?>"><img src="<?php echo w2PfindImage('prev.gif'); ?>" width="16" height="16" alt="pre" title="pre" border="0"></a>
+				                </td>
+				                <th width="100%" align="center">
+				                	<?php echo htmlspecialchars($date->format('%Y')); ?>
+				                </th>
+				                <td>
+				                	<a href="<?php echo '?m=calendar&a=year_view&date=' . $next_year; ?>"><img src="<?php echo w2PfindImage('next.gif'); ?>" width="16" height="16" alt="next" title="next" border="0"></a>
+				                </td>
+				            </tr>
+				        </table>
+				    </td>
+				</tr>
+			</table>
+			<?php
+				$minical = new CMonthCalendar($date);
+				$minical->setStyles('minititle', 'minical');
+				$minical->showArrows = false;
+				$minical->showWeek = true;
+				$minical->clickMonth = true;
+				$minical->setLinkFunctions('clickDay', 'clickWeek');
+				
+				for ($j = 0; $j < 3; $j++) {
+					echo '<table cellspacing="0" cellpadding="0" border="0" width="100%"><tr>';
+					echo '<td valign="top" align="center" width="20%">&nbsp;</td>';
+					for ($i = 0; $i <= 3; $i++) {
+						// prepare time period for minical'events'
+						$first_time = new CDate($date);
+						$first_time->setDay(1);
+						$first_time->setTime(0, 0, 0);
+						$last_time = new CDate($date);
+						$last_time->setDay($date->getDaysInMonth());
+						$last_time->setTime(23, 59, 59);
+						$links = array();
+						// assemble the links for the tasks
+						// assemble the links for the events
+						//Pedro A.
+						getTaskLinks($first_time, $last_time, $links, 20, $company_id, true);
+						getEventLinks($first_time, $last_time, $links, 20, true);
+						$minical->setEvents($links);
+						$minical->setDate($date);
+						echo '<td valign="top" align="center">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>';
+						echo '<td valign="top" align="center" width="200">' . $minical->show() . '</td>';
+						$date->addMonths(1);
+					}
+					echo '<td valign="top" align="center" width="20%">&nbsp;</td>';
+					echo '</tr></table>';
+				}
+			?>
+		</td>
+	</tr>
+	<tr>
+		<td align="center">
+			<table width="80%" class="minical">
+				<tr>
+				  	 <td nowrap="nowrap"><?php echo $AppUI->_('Key'); ?>:</td>
+				     <td>&nbsp;</td>
+				     <td style="border-style:solid;border-width:1px" class="day">&nbsp;&nbsp;</td>
+				     <td nowrap="nowrap"><?php echo $AppUI->_('Day'); ?></td>
+				     <td>&nbsp;</td>
+				     <td style="border-style:solid;border-width:1px" class="event">&nbsp;&nbsp;</td>
+				     <td nowrap="nowrap"><?php echo $AppUI->_('Event'); ?></td>
+				     <td>&nbsp;</td>
+				     <td style="border-style:solid;border-width:1px" class="task">&nbsp;&nbsp;</td>
+				     <td nowrap="nowrap"><?php echo $AppUI->_('Task'); ?></td>
+				     <td>&nbsp;</td>
+				     <td style="border-style:solid;border-width:1px" class="eventtask">&nbsp;&nbsp;</td>
+				     <td nowrap="nowrap"><?php echo $AppUI->_('Event'); ?>+<?php echo $AppUI->_('Task'); ?></td>
+				     <td>&nbsp;</td>
+				     <td style="border-style:solid;border-width:1px" class="weekend">&nbsp;&nbsp;</td>
+				     <td nowrap="nowrap"><?php echo $AppUI->_('Weekend'); ?></td>
+				     <td>&nbsp;</td>
+				     <td class="today">&nbsp;&nbsp;</td>
+				     <td nowrap="nowrap"><?php echo $AppUI->_('Today'); ?></td>
+				     <td>&nbsp;</td>
+				     <td width="40%">&nbsp;</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
 </table>
