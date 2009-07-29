2127ec81ebaf7479a108c9bbaafd865ec346088f
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-07-29 05:14:55 +0000

    set a default value for file_checkout in the creation script and in the upgrades;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@537 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

---
+++ trunk/install/sql/007_files_default_cleanup.mysql.sql
@@ -0,0 +1,5 @@
+
+-- This resolves issue #201 where the file_checkout field did not have a 
+--   default value set so file uploads were not posible.
+
+ALTER TABLE `files` CHANGE `file_checkout` `file_checkout` VARCHAR(16) NOT NULL DEFAULT '';
