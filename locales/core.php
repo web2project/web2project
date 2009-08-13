47f065d9687c92ba4bc3c1b2b3235e0415dd40bc
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-08-13 05:54:44 +0000

    combined in the tab translating code detailed in issue #112;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@557 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- locales/core.php
+++ locales/core.php
@@ -5,6 +5,21 @@ if (!defined('W2P_BASE_DIR')) {
 
 ob_start();
 
+if (isset($perms)) {
+  foreach ($AppUI->getActiveModules() as $dir => $module) {
+    if (!$perms->checkModule($dir, 'access')) {
+      continue;
+    }
+    $modules_tabs_crumbs = $AppUI->readFiles(W2P_BASE_DIR . '/modules/' . $dir . '/', '^' . $m . '_(tab|crumb).*\.php');
+    if (count($modules_tabs_crumbs) > 0) {
+      if (file_exists(W2P_BASE_DIR . '/modules/' . $dir . '/locales/' . $AppUI->user_locale . '/' . $dir . '.inc')) {
+        readfile(W2P_BASE_DIR . '/modules/' . $dir . '/locales/' . $AppUI->user_locale . '/' . $dir . '.inc');
+      } elseif (file_exists(W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/' . $dir . '.inc')) {
+        readfile(W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/' . $dir . '.inc');
+      }
+    }
+  }
+}
 
 if (W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/common.inc') {
 	readfile(W2P_BASE_DIR . '/locales/' . $AppUI->user_locale . '/common.inc');
