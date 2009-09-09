f8b73d0a0070cb486bdc312b2a9c4360df82156a
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-09-09 19:38:51 +0000

    updated the version number, prepping for release;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@616 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- includes/version.php
+++ includes/version.php
@@ -2,6 +2,6 @@
 
 // Set the version components separately.
 $w2p_version_major = 1;
-$w2p_version_minor = 0;
+$w2p_version_minor = 1;
 $w2p_version_patch = 0; // Can be set to null if not used
 $w2p_version_prepatch = null; // Set to null if a release version.
