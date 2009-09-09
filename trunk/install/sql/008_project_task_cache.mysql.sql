671ba49d0b7a03a3b4e12ba7706934b79e467716
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-09-09 07:35:50 +0000

    cleaned up some formatting; made sure table and field names have proper backticks; added a "if not exists" on the dp->w2p conversion process, this is especially necessary if they were already using the Project Designer Module or Pedro's temp table patch;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@611 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/install/sql/008_project_task_cache.mysql.sql
+++ trunk/install/sql/008_project_task_cache.mysql.sql
@@ -4,8 +4,11 @@
 --   calculated at run time.  The values for these fields are in the next 
 --   update.
 
-ALTER TABLE `projects` ADD `project_scheduled_hours` FLOAT NOT NULL DEFAULT '0' AFTER `project_actual_budget`;
+ALTER TABLE `projects` ADD `project_scheduled_hours` 
+	FLOAT NOT NULL DEFAULT '0' AFTER `project_actual_budget`;
 
-ALTER TABLE `projects` ADD `project_worked_hours` FLOAT NOT NULL DEFAULT '0' AFTER `project_scheduled_hours`;
+ALTER TABLE `projects` ADD `project_worked_hours` 
+	FLOAT NOT NULL DEFAULT '0' AFTER `project_scheduled_hours`;
 
-ALTER TABLE `projects` ADD `project_task_count` INT( 10 ) NOT NULL DEFAULT '0' AFTER `project_worked_hours` ;
+ALTER TABLE `projects` ADD `project_task_count` 
+	INT( 10 ) NOT NULL DEFAULT '0' AFTER `project_worked_hours` ;
--- trunk/install/sql/009_project_shortname_required.mysql.sql
+++ trunk/install/sql/009_project_shortname_required.mysql.sql
@@ -2,4 +2,5 @@
 -- This adds the necessary data to the database for making the
 -- shortname required for projects.
 
-INSERT INTO `sysvals` (`sysval_key_id`,`sysval_title`,`sysval_value`,`sysval_value_id`) VALUES ('1', 'ProjectRequiredFields', '<1', 'f.project_short_name.value.length');
+INSERT INTO `sysvals` (`sysval_key_id`,`sysval_title`,`sysval_value`,`sysval_value_id`) 
+	VALUES ('1', 'ProjectRequiredFields', '<1', 'f.project_short_name.value.length');
