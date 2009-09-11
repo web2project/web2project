5d69ed56387cc55f8586c3ce74c97bdd76b315dc
pedroix <pedroix@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-09-11 17:24:32 +0000

    Don't display PHP errors on the screen, this may inadvertently show errors with too much information useless to the common user and might frighten someone into a cardiac arrest.
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@634 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/base.php
+++ trunk/base.php
@@ -21,7 +21,7 @@ along with web2Project; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 if(defined('E_DEPRECATED')){ 
 	// since php 5.3
 	error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
