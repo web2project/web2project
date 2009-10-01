09b864e12f9a447b459dd703d30e3ce115b84f47
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-10-01 03:12:24 +0000

    made some tweaks to the anchor links to auto scroll to the proper entry; tweaked the borders to make the layout a bit clearer;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@678 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- modules/system/syskeys/index.php
+++ modules/system/syskeys/index.php
@@ -85,55 +85,55 @@ function delIt(id) {
 </script>
 
 <form name="sysValFrm" method="post" action="?m=system&u=syskeys&a=do_sysval_aed" accept-charset="utf-8">
-<input type="hidden" name="del" value="0" />
-<table border="0" cellpadding="2" cellspacing="1" width="100%" class="tbl">
-<tr>
-	<th>&nbsp;</th>
-	<th><?php echo $AppUI->_('Key Type'); ?></th>
-	<th><?php echo $AppUI->_('Title'); ?></th>
-	<th colspan="2"><?php echo $AppUI->_('Values'); ?></th>
-	<th>&nbsp;</th>
-</tr>
-<?php
-function showRow($id = '', $key = 0, $title = '', $value = '') {
-	global $canEdit, $sysval_id, $AppUI, $keys;
-	global $fixedSysVals;
-	$s = '';
-	if (($sysval_id == $title) && $canEdit) {
-		// edit form
-		$s = '<input type="hidden" name="sysval_id" value="' . $title . '" />';
-		$s .= '<tr><td>&nbsp;</td>';
-		$s .= '<td valign="top">' . arraySelect($keys, 'sysval_key_id', 'size="1" class="text"', $key) . '</td>';
-		$s .= '<td valign="top"><input type="text" name="sysval_title" value="' . w2PformSafe($title) . '" class="text" /></td>';
-		$s .= '<td valign="top"><textarea name="sysval_value" class="small" rows="5" cols="40">' . $value . '</textarea></td>';
-		$s .= '<td><input type="submit" value="' . $AppUI->_($id ? 'edit' : 'add') . '" class="button" /></td><td>&nbsp;</td>';
-	} else {
-		$s = '<tr><td width="12" valign="top">';
-		if ($canEdit) {
-			$s .= '<a href="?m=system&u=syskeys&sysval_id=' . $title . '" title="' . $AppUI->_('edit') . '">' . w2PshowImage('icons/stock_edit-16.png', 16, 16, '') . '</a></td>';
-		}
-		$s .= '<td valign="top">' . $keys[$key] . '</td>';
-		$s .= '<td valign="top">' . w2PformSafe($title) . '</td>';
-		$s .= '<td valign="top" colspan="2">' . $value . '</td>';
-		$s .= '<td valign="top" width="16">';
-		if ($canEdit && !in_array($title, $fixedSysVals)) {
-			$s .= '<a href="javascript:delIt(\'' . $title . '\')" title="' . $AppUI->_('delete') . '">' . w2PshowImage('icons/stock_delete-16.png', 16, 16, '') . '</a>';
-		}
-		$s .= '</td>';
-	}
-	$s .= '</tr>';
-	return $s;
-}
-
-// do the modules that are installed on the system
-$s = '';
-foreach ($values as $row) {
-	echo showRow($row['sysval_title'], $row['sysval_key_id'], $row['sysval_title'], $row['sysval_value']);
-}
-// add in the new key row:
-if (!$sysval_id) {
-	echo showRow();
-}
-?>
-</table>
+  <input type="hidden" name="del" value="0" />
+  <table border="0" cellpadding="2" cellspacing="0" width="100%" class="tbl">
+    <tr>
+    	<th>&nbsp;</th>
+    	<th><?php echo $AppUI->_('Key Type'); ?></th>
+    	<th><?php echo $AppUI->_('Title'); ?></th>
+    	<th colspan="2"><?php echo $AppUI->_('Values'); ?></th>
+    	<th>&nbsp;</th>
+    </tr>
+    <?php
+    foreach ($values as $row) {
+    	echo '<tr><td colspan="12""><a name="'.$row['sysval_title'].'"> </a><hr /></td></tr>';
+      echo showRow($row['sysval_title'], $row['sysval_key_id'], $row['sysval_title'], $row['sysval_value']);
+    }
+    // add in the new key row:
+    if (!$sysval_id) {
+    	echo showRow();
+    }
+    ?>
+  </table>
 </form>
+
+<?php
+
+function showRow($id = '', $key = 0, $title = '', $value = '') {
+  global $canEdit, $sysval_id, $AppUI, $keys;
+  global $fixedSysVals;
+  $s = '';
+  if (($sysval_id == $title) && $canEdit) {
+    // edit form
+    $s .= '<tr><td><input type="hidden" name="sysval_id" value="' . $title . '" />&nbsp;</td>';
+    $s .= '<td valign="top">' . arraySelect($keys, 'sysval_key_id', 'size="1" class="text"', $key) . '</td>';
+    $s .= '<td valign="top"><input type="text" name="sysval_title" value="' . w2PformSafe($title) . '" class="text" /></td>';
+    $s .= '<td valign="top"><textarea name="sysval_value" class="small" rows="5" cols="40">' . $value . '</textarea></td>';
+    $s .= '<td><input type="submit" value="' . $AppUI->_($id ? 'edit' : 'add') . '" class="button" /></td><td>&nbsp;</td>';
+  } else {
+    $s = '<tr><td width="12" valign="top">';
+    if ($canEdit) {
+      $s .= '<a href="?m=system&u=syskeys&sysval_id=' . $title . '#'.$title.'" title="' . $AppUI->_('edit') . '">' . w2PshowImage('icons/stock_edit-16.png', 16, 16, '') . '</a></td>';
+    }
+    $s .= '<td valign="top">' . $keys[$key] . '</td>';
+    $s .= '<td valign="top">' . w2PformSafe($title) . '</td>';
+    $s .= '<td valign="top" colspan="2">' . $value . '</td>';
+    $s .= '<td valign="top" width="16">';
+    if ($canEdit && !in_array($title, $fixedSysVals)) {
+      $s .= '<a href="javascript:delIt(\'' . $title . '\')" title="' . $AppUI->_('delete') . '">' . w2PshowImage('icons/stock_delete-16.png', 16, 16, '') . '</a>';
+    }
+    $s .= '</td>';
+  }
+  $s .= '</tr>';
+  return $s;
+}
