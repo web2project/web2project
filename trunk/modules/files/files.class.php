a5382bf758a5b77361f14c9a24d929444f91db68
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-10-09 06:07:15 +0000

    started some object cleanup to move the internal functionality (notification, indexing) back to the model; changed the file parser to only care about *unique* instances of the word... since we don't actually link within the file, knowing that it's word 132 as opposed to word 5 isn't useful, this also significantly shrinks our search index; changed the file indexing to support using the file list from file_index_ignore.php - solves part of http://bugs.web2project.net/view.php?id=268
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@698 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/modules/files/files.class.php
+++ trunk/modules/files/files.class.php
@@ -273,228 +273,245 @@ class CFile extends CW2pObject {
 	// parse file for indexing
 	public function indexStrings() {
 		global $AppUI, $w2Pconfig;
-		// get the parser application
-		$parser = $w2Pconfig['parser_' . $this->file_type];
-		if (!$parser)
-			$parser = $w2Pconfig['parser_default'];
-		if (!$parser)
-			return false;
-		// buffer the file
-		$this->_filepath = W2P_BASE_DIR . '/files/' . $this->file_project . '/' . $this->file_real_filename;
-		$fp = fopen($this->_filepath, 'rb');
-		$x = fread($fp, $this->file_size);
-		fclose($fp);
-		// parse it
-		$parser = $parser . ' ' . $this->_filepath;
-		$pos = strpos($parser, '/pdf');
-		if (false !== $pos) {
-			$x = `$parser -`;
-		} else {
-			$x = `$parser`;
-		}
-		// if nothing, return
-		if (strlen($x) < 1) {
-			return 0;
-		}
-		// remove punctuation and parse the strings
-		$x = str_replace(array('.', ',', '!', '@', '(', ')'), ' ', $x);
-		$warr = split('[[:space:]]', $x);
+    $nwords_indexed = 0;
 
-		$wordarr = array();
-		$nwords = count($warr);
-		for ($x = 0; $x < $nwords; $x++) {
-			$newword = $warr[$x];
-			if (!ereg('[[:punct:]]', $newword) && mb_strlen(mb_trim($newword)) > 2 && !ereg('[[:digit:]]', $newword)) {
-				$wordarr[] = array('word' => $newword, 'wordplace' => $x);
-			}
-		}
-		// filter out common strings
-		$ignore = array();
-		include W2P_BASE_DIR . '/modules/files/file_index_ignore.php';
-		foreach ($ignore as $w) {
-			unset($wordarr[$w]);
-		}
-		$nwords_indexed = count($wordarr);
-		// insert the strings into the table
-		while (list($key, $val) = each($wordarr)) {
-			$q = new DBQuery;
-			$q->addTable('files_index');
-
-			$q->addReplace('file_id', $this->file_id);
-			$q->addReplace('word', $wordarr[$key]['word']);
-			$q->addReplace('word_placement', $wordarr[$key]['wordplace']);
-			$q->exec();
-			$q->clear();
-		}
-
-		return (int)$nwords_indexed;
+    /* Workaround for indexing large files:
+    ** Based on the value defined in config data,
+    ** files with file_size greater than specified limit
+    ** are not indexed for searching.
+    ** Negative value :<=> no filesize limit
+    */
+    $index_max_file_size = w2PgetConfig('index_max_file_size', 0);
+    if ($index_max_file_size < 0 || $obj->file_size <= $index_max_file_size * 1024) {
+  		// get the parser application
+  		$parser = $w2Pconfig['parser_' . $this->file_type];
+  		if (!$parser) {
+  			$parser = $w2Pconfig['parser_default'];
+      }
+  		if (!$parser) {
+  			return false;
+      }
+  		// buffer the file
+  		$this->_filepath = W2P_BASE_DIR . '/files/' . $this->file_project . '/' . $this->file_real_filename;
+  		$fp = fopen($this->_filepath, 'rb');
+  		$x = fread($fp, $this->file_size);
+  		fclose($fp);
+  		// parse it
+  		$parser = $parser . ' ' . $this->_filepath;
+  		$pos = strpos($parser, '/pdf');
+  		if (false !== $pos) {
+  			$x = `$parser -`;
+  		} else {
+  			$x = `$parser`;
+  		}
+  		// if nothing, return
+  		if (strlen($x) < 1) {
+  			return 0;
+  		}
+  		// remove punctuation and parse the strings
+  		$x = str_replace(array('.', ',', '!', '@', '(', ')'), ' ', $x);
+  		$warr = split('[[:space:]]', $x);
+  
+  		$wordarr = array();
+  		$nwords = count($warr);
+  		for ($x = 0; $x < $nwords; $x++) {
+  			$newword = $warr[$x];
+  			if (!ereg('[[:punct:]]', $newword) && mb_strlen(mb_trim($newword)) > 2 && !ereg('[[:digit:]]', $newword)) {
+  				$wordarr[$newword] = $x;
+  			}
+  		}
+  		// filter out common strings
+  		$ignore = array();
+  		include W2P_BASE_DIR . '/modules/files/file_index_ignore.php';
+  		foreach ($ignore as $w) {
+  			unset($wordarr[$w]);
+  		}
+  		$nwords_indexed = count($wordarr);
+  		// insert the strings into the table
+  		while (list($key, $val) = each($wordarr)) {
+  			$q = new DBQuery;
+  			$q->addTable('files_index');  
+  			$q->addReplace('file_id', $this->file_id);
+  			$q->addReplace('word', $key);
+  			$q->addReplace('word_placement', $val);
+  			$q->exec();
+  			$q->clear();
+  		}      
+    }
+		return $nwords_indexed;
 	}
 
 	//function notifies about file changing
-	public function notify() {
-		global $AppUI, $w2Pconfig, $locale_char_set, $helpdesk_available;
-		// if helpdesk_item is available send notification to assigned users
-		if ($helpdesk_available && $this->file_helpdesk_item != 0) {
-			$this->_hditem = new CHelpDeskItem();
-			$this->_hditem->load($this->file_helpdesk_item);
+	public function notify($notify) {
+    global $AppUI, $w2Pconfig, $locale_char_set, $helpdesk_available;
 
-			$task_log = new CHDTaskLog();
-			$task_log_help_desk_id = $this->_hditem->item_id;
-			// send notifcation about new log entry
-			// 2 = TASK_LOG
-			$this->_hditem->notify(2, $task_log->task_log_id);
-
-		}
-		//if no project specified than we will not do anything
-		if ($this->file_project != 0) {
-			$this->_project = new CProject();
-			$this->_project->load($this->file_project);
-			$mail = new Mail;
-
-			if ($this->file_task == 0) { //notify all developers
-				$mail->Subject($this->_project->project_name . '::' . $this->file_name, $locale_char_set);
-			} else { //notify all assigned users
-				$this->_task = new CTask();
-				$this->_task->load($this->file_task);
-				$mail->Subject($this->_project->project_name . '::' . $this->_task->task_name . '::' . $this->file_name, $locale_char_set);
-			}
-
-			$body = $AppUI->_('Project') . ': ' . $this->_project->project_name;
-			$body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/index.php?m=projects&a=view&project_id=' . $this->_project->project_id;
-
-			if (intval($this->_task->task_id) != 0) {
-				$body .= "\n\n" . $AppUI->_('Task') . ':    ' . $this->_task->task_name;
-				$body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/index.php?m=tasks&a=view&task_id=' . $this->_task->task_id;
-				$body .= "\n" . $AppUI->_('Description') . ':' . "\n" . $this->_task->task_description;
-
-				//preparing users array
-				$q = new DBQuery;
-				$q->addTable('tasks', 't');
-				$q->addQuery('t.task_id, cc.contact_email as creator_email, cc.contact_first_name as
-						 creator_first_name, cc.contact_last_name as creator_last_name,
-						 oc.contact_email as owner_email, oc.contact_first_name as owner_first_name,
-						 oc.contact_last_name as owner_last_name, a.user_id as assignee_id, 
-						 ac.contact_email as assignee_email, ac.contact_first_name as
-						 assignee_first_name, ac.contact_last_name as assignee_last_name');
-				$q->addJoin('user_tasks', 'u', 'u.task_id = t.task_id');
-				$q->addJoin('users', 'o', 'o.user_id = t.task_owner');
-				$q->addJoin('contacts', 'oc', 'o.user_contact = oc.contact_id');
-				$q->addJoin('users', 'c', 'c.user_id = t.task_creator');
-				$q->addJoin('contacts', 'cc', 'c.user_contact = cc.contact_id');
-				$q->addJoin('users', 'a', 'a.user_id = u.user_id');
-				$q->addJoin('contacts', 'ac', 'a.user_contact = ac.contact_id');
-				$q->addWhere('t.task_id = ' . (int)$this->_task->task_id);
-				$this->_users = $q->loadList();
-			} else {
-				//find project owner and notify him about new or modified file
-				$q = new DBQuery;
-				$q->addTable('users', 'u');
-				$q->addTable('projects', 'p');
-				$q->addQuery('u.*');
-				$q->addWhere('p.project_owner = u.user_id');
-				$q->addWhere('p.project_id = ' . (int)$this->file_project);
-				$this->_users = $q->loadList();
-			}
-			$body .= "\n\nFile " . $this->file_name . ' was ' . $this->_message . ' by ' . $AppUI->user_first_name . ' ' . $AppUI->user_last_name;
-			if ($this->_message != 'deleted') {
-				$body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/fileviewer.php?file_id=' . $this->file_id;
-				$body .= "\n" . $AppUI->_('Description') . ':' . "\n" . $this->file_description;
-			}
-
-			//send mail
-			$mail->Body($body, isset($GLOBALS['locale_char_set']) ? $GLOBALS['locale_char_set'] : '');
-
-			if (intval($this->_task->task_id) != 0) {
-				foreach ($this->_users as $row) {
-					if ($row['assignee_id'] != $AppUI->user_id) {
-						if ($mail->ValidEmail($row['assignee_email'])) {
-							$mail->To($row['assignee_email'], true);
-							$mail->Send();
-						}
-					}
-				}
-			} else { //sending mail to project owner
-				foreach ($this->_users as $row) { //there should be only one row
-					if ($row['user_id'] != $AppUI->user_id) {
-						if ($mail->ValidEmail($row['user_email'])) {
-							$mail->To($row['user_email'], true);
-							$mail->Send();
-						}
-					}
-				}
-			}
-		}
+    if ($notify == '1') {
+  		// if helpdesk_item is available send notification to assigned users
+  		if ($helpdesk_available && $this->file_helpdesk_item != 0) {
+  			$this->_hditem = new CHelpDeskItem();
+  			$this->_hditem->load($this->file_helpdesk_item);
+  
+  			$task_log = new CHDTaskLog();
+  			$task_log_help_desk_id = $this->_hditem->item_id;
+  			// send notifcation about new log entry
+  			// 2 = TASK_LOG
+  			$this->_hditem->notify(2, $task_log->task_log_id);
+  
+  		}
+  		//if no project specified than we will not do anything
+  		if ($this->file_project != 0) {
+  			$this->_project = new CProject();
+  			$this->_project->load($this->file_project);
+  			$mail = new Mail;
+  
+  			if ($this->file_task == 0) { //notify all developers
+  				$mail->Subject($this->_project->project_name . '::' . $this->file_name, $locale_char_set);
+  			} else { //notify all assigned users
+  				$this->_task = new CTask();
+  				$this->_task->load($this->file_task);
+  				$mail->Subject($this->_project->project_name . '::' . $this->_task->task_name . '::' . $this->file_name, $locale_char_set);
+  			}
+  
+  			$body = $AppUI->_('Project') . ': ' . $this->_project->project_name;
+  			$body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/index.php?m=projects&a=view&project_id=' . $this->_project->project_id;
+  
+  			if (intval($this->_task->task_id) != 0) {
+  				$body .= "\n\n" . $AppUI->_('Task') . ':    ' . $this->_task->task_name;
+  				$body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/index.php?m=tasks&a=view&task_id=' . $this->_task->task_id;
+  				$body .= "\n" . $AppUI->_('Description') . ':' . "\n" . $this->_task->task_description;
+  
+  				//preparing users array
+  				$q = new DBQuery;
+  				$q->addTable('tasks', 't');
+  				$q->addQuery('t.task_id, cc.contact_email as creator_email, cc.contact_first_name as
+  						 creator_first_name, cc.contact_last_name as creator_last_name,
+  						 oc.contact_email as owner_email, oc.contact_first_name as owner_first_name,
+  						 oc.contact_last_name as owner_last_name, a.user_id as assignee_id, 
+  						 ac.contact_email as assignee_email, ac.contact_first_name as
+  						 assignee_first_name, ac.contact_last_name as assignee_last_name');
+  				$q->addJoin('user_tasks', 'u', 'u.task_id = t.task_id');
+  				$q->addJoin('users', 'o', 'o.user_id = t.task_owner');
+  				$q->addJoin('contacts', 'oc', 'o.user_contact = oc.contact_id');
+  				$q->addJoin('users', 'c', 'c.user_id = t.task_creator');
+  				$q->addJoin('contacts', 'cc', 'c.user_contact = cc.contact_id');
+  				$q->addJoin('users', 'a', 'a.user_id = u.user_id');
+  				$q->addJoin('contacts', 'ac', 'a.user_contact = ac.contact_id');
+  				$q->addWhere('t.task_id = ' . (int)$this->_task->task_id);
+  				$this->_users = $q->loadList();
+  			} else {
+  				//find project owner and notify him about new or modified file
+  				$q = new DBQuery;
+  				$q->addTable('users', 'u');
+  				$q->addTable('projects', 'p');
+  				$q->addQuery('u.*');
+  				$q->addWhere('p.project_owner = u.user_id');
+  				$q->addWhere('p.project_id = ' . (int)$this->file_project);
+  				$this->_users = $q->loadList();
+  			}
+  			$body .= "\n\nFile " . $this->file_name . ' was ' . $this->_message . ' by ' . $AppUI->user_first_name . ' ' . $AppUI->user_last_name;
+  			if ($this->_message != 'deleted') {
+  				$body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/fileviewer.php?file_id=' . $this->file_id;
+  				$body .= "\n" . $AppUI->_('Description') . ':' . "\n" . $this->file_description;
+  			}
+  
+  			//send mail
+  			$mail->Body($body, isset($GLOBALS['locale_char_set']) ? $GLOBALS['locale_char_set'] : '');
+  
+  			if (intval($this->_task->task_id) != 0) {
+  				foreach ($this->_users as $row) {
+  					if ($row['assignee_id'] != $AppUI->user_id) {
+  						if ($mail->ValidEmail($row['assignee_email'])) {
+  							$mail->To($row['assignee_email'], true);
+  							$mail->Send();
+  						}
+  					}
+  				}
+  			} else { //sending mail to project owner
+  				foreach ($this->_users as $row) { //there should be only one row
+  					if ($row['user_id'] != $AppUI->user_id) {
+  						if ($mail->ValidEmail($row['user_email'])) {
+  							$mail->To($row['user_email'], true);
+  							$mail->Send();
+  						}
+  					}
+  				}
+  			}
+  		}
+    }
 	} //notify
 
-	public function notifyContacts() {
+	public function notifyContacts($notifyContacts) {
 		global $AppUI, $w2Pconfig, $locale_char_set;
-		//if no project specified than we will not do anything
-		if ($this->file_project != 0) {
-			$this->_project = new CProject();
-			$this->_project->load($this->file_project);
-			$mail = new Mail;
 
-			if ($this->file_task == 0) { //notify all developers
-				$mail->Subject($AppUI->_('Project') . ': ' . $this->_project->project_name . '::' . $this->file_name, $locale_char_set);
-			} else { //notify all assigned users
-				$this->_task = new CTask();
-				$this->_task->load($this->file_task);
-				$mail->Subject($AppUI->_('Project') . ': ' . $this->_project->project_name . '::' . $this->_task->task_name . '::' . $this->file_name, $locale_char_set);
-			}
-
-			$body = $AppUI->_('Project') . ': ' . $this->_project->project_name;
-			$body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/index.php?m=projects&a=view&project_id=' . $this->_project->project_id;
-
-			if (intval($this->_task->task_id) != 0) {
-				$body .= "\n\n" . $AppUI->_('Task') . ':    ' . $this->_task->task_name;
-				$body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/index.php?m=tasks&a=view&task_id=' . $this->_task->task_id;
-				$body .= "\n" . $AppUI->_('Description') . ":\n" . $this->_task->task_description;
-
-				$q = new DBQuery;
-				$q->addTable('project_contacts', 'pc');
-				$q->addQuery('c.contact_email as contact_email, c.contact_first_name as contact_first_name, c.contact_last_name as contact_last_name');
-				$q->addJoin('contacts', 'c', 'c.contact_id = pc.contact_id');
-				$q->addWhere('pc.project_id = ' . (int)$this->_project->project_id);
-				$sql = '(' . $q->prepare() . ')';
-				$q->clear();
-				$sql .= ' UNION ';
-				$q->addTable('task_contacts', 'tc');
-				$q->addQuery('c.contact_email as contact_email, c.contact_first_name as contact_first_name, c.contact_last_name as contact_last_name');
-				$q->addJoin('contacts', 'c', 'c.contact_id = tc.contact_id');
-				$q->addWhere('tc.task_id = ' . (int)$this->_task->task_id);
-				$sql .= '(' . $q->prepare() . ')';
-				$q->clear();
-				$this->_users = $q->loadList();
-			} else {
-				$q = new DBQuery;
-				$q->addTable('project_contacts', 'pc');
-				$q->addQuery('pc.project_id, pc.contact_id');
-				$q->addQuery('c.contact_email as contact_email, c.contact_first_name as contact_first_name, c.contact_last_name as contact_last_name');
-				$q->addJoin('contacts', 'c', 'c.contact_id = pc.contact_id');
-				$q->addWhere('pc.project_id = ' . (int)$this->file_project);
-
-				$this->_users = $q->loadList();
-				$q->clear();
-			}
-
-			$body .= "\n\nFile " . $this->file_name . ' was ' . $this->_message . ' by ' . $AppUI->user_first_name . ' ' . $AppUI->user_last_name;
-			if ($this->_message != 'deleted') {
-				$body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/fileviewer.php?file_id=' . $this->file_id;
-				$body .= "\n" . $AppUI->_('Description') . ":\n" . $this->file_description;
-			}
-
-			//send mail
-			$mail->Body($body, isset($GLOBALS['locale_char_set']) ? $GLOBALS['locale_char_set'] : '');
-
-			foreach ($this->_users as $row) {
-
-				if ($mail->ValidEmail($row['contact_email'])) {
-					$mail->To($row['contact_email'], true);
-					$mail->Send();
-				}
-			}
-			return '';
-		}
+    if ($notifyContacts == '1') {
+      //if no project specified than we will not do anything
+      if ($this->file_project != 0) {
+        $this->_project = new CProject();
+        $this->_project->load($this->file_project);
+        $mail = new Mail;
+  
+        if ($this->file_task == 0) { //notify all developers
+          $mail->Subject($AppUI->_('Project') . ': ' . $this->_project->project_name . '::' . $this->file_name, $locale_char_set);
+        } else { //notify all assigned users
+          $this->_task = new CTask();
+          $this->_task->load($this->file_task);
+          $mail->Subject($AppUI->_('Project') . ': ' . $this->_project->project_name . '::' . $this->_task->task_name . '::' . $this->file_name, $locale_char_set);
+        }
+  
+        $body = $AppUI->_('Project') . ': ' . $this->_project->project_name;
+        $body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/index.php?m=projects&a=view&project_id=' . $this->_project->project_id;
+  
+        if (intval($this->_task->task_id) != 0) {
+          $body .= "\n\n" . $AppUI->_('Task') . ':    ' . $this->_task->task_name;
+          $body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/index.php?m=tasks&a=view&task_id=' . $this->_task->task_id;
+          $body .= "\n" . $AppUI->_('Description') . ":\n" . $this->_task->task_description;
+  
+          $q = new DBQuery;
+          $q->addTable('project_contacts', 'pc');
+          $q->addQuery('c.contact_email as contact_email, c.contact_first_name as contact_first_name, c.contact_last_name as contact_last_name');
+          $q->addJoin('contacts', 'c', 'c.contact_id = pc.contact_id');
+          $q->addWhere('pc.project_id = ' . (int)$this->_project->project_id);
+          $sql = '(' . $q->prepare() . ')';
+          $q->clear();
+          $sql .= ' UNION ';
+          $q->addTable('task_contacts', 'tc');
+          $q->addQuery('c.contact_email as contact_email, c.contact_first_name as contact_first_name, c.contact_last_name as contact_last_name');
+          $q->addJoin('contacts', 'c', 'c.contact_id = tc.contact_id');
+          $q->addWhere('tc.task_id = ' . (int)$this->_task->task_id);
+          $sql .= '(' . $q->prepare() . ')';
+          $q->clear();
+          $this->_users = $q->loadList();
+        } else {
+          $q = new DBQuery;
+          $q->addTable('project_contacts', 'pc');
+          $q->addQuery('pc.project_id, pc.contact_id');
+          $q->addQuery('c.contact_email as contact_email, c.contact_first_name as contact_first_name, c.contact_last_name as contact_last_name');
+          $q->addJoin('contacts', 'c', 'c.contact_id = pc.contact_id');
+          $q->addWhere('pc.project_id = ' . (int)$this->file_project);
+  
+          $this->_users = $q->loadList();
+          $q->clear();
+        }
+  
+        $body .= "\n\nFile " . $this->file_name . ' was ' . $this->_message . ' by ' . $AppUI->user_first_name . ' ' . $AppUI->user_last_name;
+        if ($this->_message != 'deleted') {
+          $body .= "\n" . $AppUI->_('URL') . ':     ' . W2P_BASE_URL . '/fileviewer.php?file_id=' . $this->file_id;
+          $body .= "\n" . $AppUI->_('Description') . ":\n" . $this->file_description;
+        }
+  
+        //send mail
+        $mail->Body($body, isset($GLOBALS['locale_char_set']) ? $GLOBALS['locale_char_set'] : '');
+  
+        foreach ($this->_users as $row) {
+  
+          if ($mail->ValidEmail($row['contact_email'])) {
+            $mail->To($row['contact_email'], true);
+            $mail->Send();
+          }
+        }
+        return '';
+      }
+    }
 	}
 
 	public function getOwner() {
