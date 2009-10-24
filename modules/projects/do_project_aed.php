263651b134a02e12f436edd135c49a5a74119835
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-10-24 06:23:11 +0000

    updated the Model to put the check() method within the store() call, so now an incorrect/incomplete object should not be save-able; refactored the controller to make proper use of the calls and simplified the logic quite a bit; updated the tests to take all of this into account;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@727 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- modules/projects/do_project_aed.php
+++ modules/projects/do_project_aed.php
@@ -12,105 +12,34 @@ if (!$obj->bind($_POST)) {
 }
 
 $action = ($del) ? 'deleted' : 'stored';
-
-$project_id = (int) w2PgetParam($_POST, 'project_id', 0);
-
-$isNotNew = $project_id;
-$perms = &$AppUI->acl();
-if ($del) {
-} elseif ($project_id) {
-	if (!$perms->checkModuleItem('projects', 'edit', $project_id)) {
-		$AppUI->redirect('m=public&a=access_denied');
-	}
-} else {
-	if (!$perms->checkModule('projects', 'add')) {
-		$AppUI->redirect('m=public&a=access_denied');
-	}
-}
+$result = ($del) ? $obj->delete($AppUI) : $obj->store($AppUI);
 
 $notify_owner = ($_POST['email_project_owner_box']) ? 1 : 0;
 $notify_contacts = ($_POST['email_project_contacts_box']) ? 1 : 0;
+$notfiyTrigger = ($del) ? 1 : $obj->project_id;
+$importTask_projectId = (int) w2PgetParam($_POST, 'import_tasks_from', '0');
 
-// convert dates to SQL format first
-if ($obj->project_start_date) {
-	$date = new CDate($obj->project_start_date);
-	$obj->project_start_date = $date->format(FMT_DATETIME_MYSQL);
+if (is_array($result)) {
+  $AppUI->setMsg($result, UI_MSG_ERROR);
+  $AppUI->redirect();
 }
-if ($obj->project_end_date) {
-	$date = new CDate($obj->project_end_date);
-	$date->setTime(23, 59, 59);
-	$obj->project_end_date = $date->format(FMT_DATETIME_MYSQL);
-}
-if ($obj->project_actual_end_date) {
-	$date = new CDate($obj->project_actual_end_date);
-	$obj->project_actual_end_date = $date->format(FMT_DATETIME_MYSQL);
-}
-
-// let's check if there are some assigned departments to project
-if (!w2PgetParam($_POST, 'project_departments', 0)) {
-	$obj->project_departments = implode(',', w2PgetParam($_POST, 'dept_ids', array()));
-}
-
-// prepare (and translate) the module name ready for the suffix
-if ($del) {
-  if (!$perms->checkModuleItem('projects', 'delete', $project_id)) {
-    $AppUI->redirect('m=public&a=access_denied');
+if ($result) {
+  if ($importTask_projectId) {
+    $obj->importTasks($importTask_projectId);
   }
-	if (($msg = $obj->delete($AppUI))) {
-		$AppUI->setMsg($msg, UI_MSG_ERROR);
-		$AppUI->redirect();
-	} else {
-		if ($notify_owner) {
-			if ($msg = $obj->notifyOwner(1)) {
-				$AppUI->setMsg($msg, UI_MSG_ERROR);
-			}
-		}
-		if ($notify_contacts) {
-			if ($msg = $obj->notifyContacts(1)) {
-				$AppUI->setMsg($msg, UI_MSG_ERROR);
-			}
-		}
-		$AppUI->setMsg('Project deleted', UI_MSG_ALERT);
-		$AppUI->redirect('m=projects');
-	}
+  if ($notify_owner) {
+    if ($msg = $obj->notifyOwner($notfiyTrigger)) {
+      $AppUI->setMsg($msg, UI_MSG_ERROR);
+    }
+  }
+  if ($notify_contacts) {
+    if ($msg = $obj->notifyContacts($notfiyTrigger)) {
+      $AppUI->setMsg($msg, UI_MSG_ERROR);
+    }
+  }
+
+  $AppUI->setMsg('Project '.$action, UI_MSG_OK, true);
+  $AppUI->redirect('m=projects');
 } else {
-	if (($msg = $obj->store())) {
-		$AppUI->setMsg($msg, UI_MSG_ERROR);
-	} else {
-		// check project parents and reset them to self if they do not exist
-		if (!$obj->project_parent) {
-			$obj->project_parent = $obj->project_id;
-			$obj->project_original_parent = $obj->project_id;
-		} else {
-			$parent_project = new CProject();
-			$parent_project->load($obj->project_parent);
-			$obj->project_original_parent = $parent_project->project_original_parent;
-		}
-
-		if (!$obj->project_original_parent) {
-			$obj->project_original_parent = $obj->project_id;
-		}
-
-		$obj->store();
-
-		if ($importTask_projectId = w2PgetParam($_POST, 'import_tasks_from', '0')) {
-			$obj->importTasks($importTask_projectId);
-		}
-
-		$custom_fields = new CustomFields($m, 'addedit', $obj->project_id, 'edit');
-		$custom_fields->bind($_POST);
-		$sql = $custom_fields->store($obj->project_id); // Store Custom Fields
-		if ($notify_owner) {
-			if ($msg = $obj->notifyOwner($project_id)) {
-				$AppUI->setMsg($msg, UI_MSG_ERROR);
-			}
-		}
-		if ($notify_contacts) {
-			if ($msg = $obj->notifyContacts($project_id)) {
-				$AppUI->setMsg($msg, UI_MSG_ERROR);
-			}
-		}
-		$AppUI->setMsg($project_id ? 'Project updated' : 'Project inserted', UI_MSG_OK);
-	}
-	$AppUI->redirect();
+  $AppUI->redirect('m=public&a=access_denied');
 }
