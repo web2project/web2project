94fcda1b81a8835d7a170c6dfeaa0ae730e8a37e
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-11-02 03:53:35 +0000

    fixed a dumb typo of my own fault;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@732 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/install/sql/mysql/013_file_indexing.mysql.sql
+++ trunk/install/sql/mysql/013_file_indexing.mysql.sql
@@ -1,7 +1,7 @@
 
 -- This handles a bit of cleanup to the database that should be in place.
 
-ALTER TABLE `gacl_axo` CHANGE `id` `id` INT( 11 ) NOT NULL AUTO_INCREMENT  
+ALTER TABLE `gacl_axo` CHANGE `id` `id` INT( 11 ) NOT NULL AUTO_INCREMENT;
 
 -- This adds a simple flag to each of the files to flag if a file has been indexed or not.
 
