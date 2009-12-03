2d37496fe68fb8088fd3634505226e3eb4439810
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-12-03 18:46:42 +0000

    added two files to allow for tracking of deprecated functions and those that need wrapping for backwards compatibility;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@830 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

---
+++ includes/backcompat_functions.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+* This file exists in order to identify individual functions which may not be
+*   available in all versions of PHP.  Therefore, we have to wrap the
+*   functionality in function_exists stuff and all that.  In the documentation
+*   for each function, you must describe:
+*
+*    * the specific version of PHP or extension the regular function requires.
+*
+* During Minor releases, this file will grow only to shrink as Major releases
+*   allow us to change minimum version for PHP compatibility.
+*/
+
