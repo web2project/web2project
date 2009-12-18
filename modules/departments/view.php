b04b214fd233e5cef84553ed99c80f85ffe7d916
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2009-12-18 07:05:21 +0000

    updated a few more comment/description fields in core to use this function for all Views; this covers part of #336 - http://bugs.web2project.net/view.php?id=336 ;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@863 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- modules/departments/view.php
+++ modules/departments/view.php
@@ -136,7 +136,7 @@ function delIt() {
 			<strong><?php echo $AppUI->_('Description'); ?></strong>
 			<table cellspacing="1" cellpadding="2" border="0" width="100%">
 			<tr>
-				<td class="hilite" width="100%"><?php echo mb_str_replace(chr(10), '<br />', w2p_textarea($department->dept_desc)); ?>&nbsp;</td>
+				<td class="hilite" width="100%"><?php echo w2p_textarea($department->dept_desc); ?>&nbsp;</td>
 			</tr>
 			</table>
 		</td>
