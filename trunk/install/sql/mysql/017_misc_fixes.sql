b16a5a125f39a813f6de7a8f361b5f382532017b
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-02-07 06:04:23 +0000

    applying various database fixes;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@950 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

---
+++ trunk/install/sql/mysql/017_misc_fixes.sql
@@ -0,0 +1,14 @@
+
+-- This updates the contact_fax and contact_aol to allow null and remove
+--   the bad (zero) values.
+
+ALTER TABLE `contacts` CHANGE `contact_aol` `contact_aol` VARCHAR( 30 )
+  CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+
+ALTER TABLE `contacts` CHANGE `contact_fax` `contact_fax` VARCHAR( 30 )
+  CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+
+UPDATE `contacts` SET `contact_aol` = NULL WHERE `contact_aol` = 0;
+UPDATE `contacts` SET `contact_fax` = NULL WHERE `contact_fax` = 0;
+
+UPDATE `modules` SET `mod_main_class` = 'CReport' WHERE `mod_directory` = 'reports';
