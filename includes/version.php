69166c098d4ac27741205e6f52c04e13a99641ae
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-04-01 06:22:59 +0000

    updated the version number. woot.
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@992 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- includes/version.php
+++ includes/version.php
@@ -4,4 +4,4 @@
 $w2p_version_major = 1;
 $w2p_version_minor = 3;
 $w2p_version_patch = 0; // Can be set to null if not used
-$w2p_version_prepatch = 'pre'; // Set to null if a release version.
+$w2p_version_prepatch = null; // Set to null if a release version.
