27c3cd5675afb4d0773a48c7c8b1f84eaf00df13
D. Keith Casey, Jr <keith@caseysoftware.com>
2010-04-12 00:53:37 -0400

    added the $percent variable as described here - http://bugs.web2project.net/view.php?id=404

--- modules/projectdesigner/vw_actions.php
+++ modules/projectdesigner/vw_actions.php
@@ -84,6 +84,7 @@ asort($sprojects);
 $sprojects = arrayMerge(array('' => '(' . $AppUI->_('Move to Project', UI_OUTPUT_RAW) . ')'), $sprojects);
 
 //lets addthe reference to percent
+$percent = array(0 => '0', 5 => '5', 10 => '10', 15 => '15', 20 => '20', 25 => '25', 30 => '30', 35 => '35', 40 => '40', 45 => '45', 50 => '50', 55 => '55', 60 => '60', 65 => '65', 70 => '70', 75 => '75', 80 => '80', 85 => '85', 90 => '90', 95 => '95', 100 => '100');
 $spercent = arrayMerge(array('' => '(Progress)'), $percent);
 ?>
             <form name="frm_bulk" method="POST" action="?m=projectdesigner&a=do_task_bulk_aed" accept-charset="utf-8">
