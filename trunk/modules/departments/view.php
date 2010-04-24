a85571fc31fd793b39b0fff507f5fc28a69bb03a
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-04-24 03:51:46 +0000

    deprecated the getDenyRead function in favor of canView()
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@1016 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/modules/departments/view.php
+++ trunk/modules/departments/view.php
@@ -9,7 +9,7 @@ $dept_id = isset($_GET['dept_id']) ? w2PgetParam($_GET, 'dept_id', 0) : (isset($
 $msg = '';
 $department = new CDepartment();
 // check permissions
-$canRead = !getDenyRead($m, $dept_id);
+$canRead = canView($m, $dept_id);
 $canEdit = !getDenyEdit($m, $dept_id);
 
 if (!$canRead) {
