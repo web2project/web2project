aa7132be36a64370717a86b343498f32029ebe2e
D. Keith Casey Jr <caseydk@nightcrawler.(none)>
2010-05-13 22:54:27 -0400

    cleaned up the store, check, and load functions; updated the addedit screens to be consistent with the page structures;

--- modules/forums/do_forum_aed.php
+++ modules/forums/do_forum_aed.php
@@ -3,46 +3,26 @@ if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
 
-$del = isset($_POST['del']) ? $_POST['del'] : 0;
-
-$isNotNew = $_POST['forum_id'];
-$perms = &$AppUI->acl();
-if ($del) {
-	if (!canDelete('forums')) {
-		$AppUI->redirect('m=public&a=access_denied');
-	}
-} elseif ($isNotNew) {
-	if (!canEdit('forums')) {
-		$AppUI->redirect('m=public&a=access_denied');
-	}
-} else {
-	if (!canAdd('forums')) {
-		$AppUI->redirect('m=public&a=access_denied');
-	}
-}
+$del = (int) w2PgetParam($_POST, 'del', 0);
+$isNotNew = (int) w2PgetParam($_POST, 'forum_id', 0);
 
 $obj = new CForum();
-
-if (($msg = $obj->bind($_POST))) {
-	$AppUI->setMsg($msg, UI_MSG_ERROR);
-	$AppUI->redirect();
+if (!$obj->bind($_POST)) {
+  $AppUI->setMsg($obj->getError(), UI_MSG_ERROR);
+  $AppUI->redirect();
 }
 
-// prepare (and translate) the module name ready for the suffix
-$AppUI->setMsg('Forum');
-if ($del) {
-	if (($msg = $obj->delete())) {
-		$AppUI->setMsg($msg, UI_MSG_ERROR);
-		$AppUI->redirect();
-	} else {
-		$AppUI->setMsg('deleted', UI_MSG_ALERT, true);
-		$AppUI->redirect('m=forums');
-	}
+$action = ($del) ? 'deleted' : 'stored';
+$result = ($del) ? $obj->delete($AppUI) : $obj->store($AppUI);
+
+if (is_array($result)) {
+  $AppUI->setMsg($result, UI_MSG_ERROR, true);
+  $AppUI->holdObject($obj);
+  $AppUI->redirect('m=forums&a=addedit');
+}
+if ($result) {
+  $AppUI->setMsg('Forums '.$action, UI_MSG_OK, true);
+  $AppUI->redirect('m=forums');
 } else {
-	if (($msg = $obj->store())) {
-		$AppUI->setMsg($msg, UI_MSG_ERROR);
-	} else {
-		$AppUI->setMsg($isNotNew ? 'updated' : 'added', UI_MSG_OK, true);
-	}
-	$AppUI->redirect();
+  $AppUI->redirect('m=public&a=access_denied');
 }
