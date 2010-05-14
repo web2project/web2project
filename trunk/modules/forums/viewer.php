d3af62414a942873f8f57cf4acff154d4d8bc6af
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-05-14 04:18:29 +0000

    added support to allow forums without projects; sync'd up the API; cleaned up the CForumMessage class to use to clean class structure; cleaned up the post controller;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@1079 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/modules/forums/viewer.php
+++ trunk/modules/forums/viewer.php
@@ -7,7 +7,7 @@ if (!defined('W2P_BASE_DIR')) {
 $forum_id = (int) w2PgetParam($_GET, 'forum_id', 0);
 
 $message_id = (int) w2PgetParam($_GET, 'message_id', 0);
-$post_message = isset($_GET['post_message']) ? w2PgetParam($_GET, 'post_message', 0) : 0;
+$post_message = (int) w2PgetParam($_GET, 'post_message', 0);
 $f = w2PgetParam($_POST, 'f', 0);
 
 // check permissions
@@ -27,16 +27,15 @@ $tf = $AppUI->getPref('TIMEFORMAT');
 
 $q = new DBQuery;
 $q->addTable('forums');
-$q->addTable('projects', 'p');
 $q->addTable('users', 'u');
 $q->addQuery('forum_id, forum_project,	forum_description, forum_owner, forum_name,
 	forum_create_date, forum_last_date, forum_message_count, forum_moderated,
 	user_username, contact_first_name, contact_last_name,
 	project_name, project_color_identifier');
 $q->addJoin('contacts', 'con', 'contact_id = user_contact', 'inner');
+$q->addJoin('projects', 'p', 'p.project_id = forum_project');
 $q->addWhere('user_id = forum_owner');
 $q->addWhere('forum_id = ' . (int)$forum_id);
-$q->addWhere('forum_project = project_id');
 $q->exec(ADODB_FETCH_ASSOC);
 $forum = $q->fetchRow();
 $forum_name = $forum['forum_name'];
@@ -51,32 +50,33 @@ $titleBlock->addCell(arraySelect($filters, 'f', 'size="1" class="text" onchange=
 $titleBlock->show();
 ?>
 <table width="100%" cellspacing="0" cellpadding="2" border="0" class="std">
-<tr>
-	<td height="20" colspan="3" style="border: outset #D1D1CD 1px;background-color:#<?php echo $forum['project_color_identifier']; ?>">
-		<font size="2" color="<?php echo bestColor($forum["project_color_identifier"]); ?>"><strong><?php echo $forum['forum_name']; ?></strong></font>
-	</td>
-</tr>
-<tr>
-	<td align="left" nowrap="nowrap"><?php echo $AppUI->_('Related Project'); ?>:</td>
-	<td nowrap="nowrap"><strong><?php echo $forum['project_name']; ?></strong></td>
-	<td valign="top" width="50%" rowspan="99">
-		<strong><?php echo $AppUI->_('Description'); ?>:</strong><br />
-    <?php echo w2p_textarea($forum['forum_description']); ?>
-	</td>
-</tr>
-<tr>
-	<td align="left"><?php echo $AppUI->_('Owner'); ?>:</td>
-	<td nowrap="nowrap"><?php
-echo $forum['contact_first_name'] . ' ' . $forum['contact_last_name'];
-if (intval($forum['forum_id']) <> 0) {
-	echo ' (' . $AppUI->_('moderated') . ') ';
-} ?>
-	</td>
-</tr>
-<tr>
-	<td align="left"><?php echo $AppUI->_('Created On'); ?>:</td>
-	<td nowrap="nowrap"><?php echo $start_date ? $start_date->format($df) : '-'; ?></td>
-</tr>
+    <tr>
+        <td height="20" colspan="3" style="border: outset #D1D1CD 1px;background-color:#<?php echo $forum['project_color_identifier']; ?>">
+            <font size="2" color="<?php echo bestColor($forum["project_color_identifier"]); ?>"><strong><?php echo $forum['forum_name']; ?></strong></font>
+        </td>
+    </tr>
+    <tr>
+        <td align="left" nowrap="nowrap"><?php echo $AppUI->_('Related Project'); ?>:</td>
+        <td nowrap="nowrap"><strong><?php echo ($forum['project_name']) ? $forum['project_name'] : 'No associated project'; ?></strong></td>
+        <td valign="top" width="50%" rowspan="99">
+            <strong><?php echo $AppUI->_('Description'); ?>:</strong><br />
+            <?php echo w2p_textarea($forum['forum_description']); ?>
+        </td>
+    </tr>
+    <tr>
+        <td align="left"><?php echo $AppUI->_('Owner'); ?>:</td>
+        <td nowrap="nowrap">
+            <?php
+            echo $forum['contact_first_name'] . ' ' . $forum['contact_last_name'];
+            if (intval($forum['forum_id']) <> 0) {
+                echo ' (' . $AppUI->_('moderated') . ') ';
+            } ?>
+        </td>
+    </tr>
+    <tr>
+        <td align="left"><?php echo $AppUI->_('Created On'); ?>:</td>
+        <td nowrap="nowrap"><?php echo $start_date ? $start_date->format($df) : '-'; ?></td>
+    </tr>
 </table>
 <?php
 if (function_exists('styleRenderBoxBottom')) {
