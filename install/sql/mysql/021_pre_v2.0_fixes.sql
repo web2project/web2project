462e8f66367b03bdff79aa40b469ee909b0311c7
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-05-31 04:45:19 +0000

    renamed and cleaned up the upgrade scripts to handle updates in the correct order;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1111 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- install/sql/mysql/021_pre_v2.0_fixes.sql
+++
@@ -1,8 +0,0 @@
-
--- This addition adds support for token tasks to represent subprojects. The
---   best part about this method is that you get dependencies and other
---   planning aspects for "free".
-
-ALTER TABLE `tasks` ADD `task_represents_project` INT( 10 ) NOT NULL ;
-ALTER TABLE `tasks` ADD INDEX ( `task_represents_project` ) ;
-
