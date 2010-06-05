eeb31e6e7ed6c6da03ed50041f29c4a1fe2a9301
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-06-05 06:16:54 +0000

    cleaned up some formatting; cleaning up the store/delete functions to use the core structure; wired the autoloader to handle the new class;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@1149 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/modules/forums/do_post_aed.php
+++ trunk/modules/forums/do_post_aed.php
@@ -10,21 +10,22 @@ $message_parent = (int) w2PgetParam($_POST, 'message_parent', 0);
 
 $obj = new CForumMessage();
 if (!$obj->bind($_POST)) {
-  $AppUI->setMsg($obj->getError(), UI_MSG_ERROR);
-  $AppUI->redirect();
+    $AppUI->setMsg($obj->getError(), UI_MSG_ERROR);
+    $AppUI->redirect();
 }
 
 $action = ($del) ? 'deleted' : 'stored';
 $result = ($del) ? $obj->delete($AppUI) : $obj->store($AppUI);
 
 if (is_array($result)) {
-  $AppUI->setMsg($result, UI_MSG_ERROR, true);
-  $AppUI->holdObject($obj);
-  $AppUI->redirect('m=forums&a=viewer&forum_id='.$message_forum.'&message_parent='.$message_parent.'&post_message=1');
+    $AppUI->setMsg($result, UI_MSG_ERROR, true);
+    $AppUI->holdObject($obj);
+    $AppUI->redirect('m=forums&a=viewer&forum_id='.$message_forum.'&message_parent='.$message_parent.'&post_message=1');
 }
 if ($result) {
-  $AppUI->setMsg('Message '.$action, UI_MSG_OK, true);
-  $AppUI->redirect('m=forums&a=viewer&forum_id='.$message_forum.'&message_parent='.$message_parent);
+    $AppUI->setMsg('Message '.$action, UI_MSG_OK, true);
+    $redirect = ($del) ? 'm=forums' : 'm=forums&a=viewer&forum_id='.$message_forum.'&message_parent='.$message_parent;
+    $AppUI->redirect($redirect);
 } else {
-  $AppUI->redirect('m=public&a=access_denied');
+    $AppUI->redirect('m=public&a=access_denied');
 }
--- trunk/modules/forums/forums.class.php
+++ trunk/modules/forums/forums.class.php
@@ -122,7 +122,6 @@ class CForum extends CW2pObject {
             if (($msg = parent::store())) {
                 return $msg;
             }
-            addHistory('forums', $this->forum_id, 'update', $this->forum_name, $this->forum_id);
             $stored = true;
         }
         if (0 == $this->forum_id && $perms->checkModuleItem('forums', 'add')) {
@@ -131,7 +130,6 @@ class CForum extends CW2pObject {
             if (($msg = parent::store())) {
                 return $msg;
             }
-            addHistory('forums', $this->forum_id, 'add', $this->forum_name, $this->forum_id);
             $stored = true;
         }
         return $stored;
@@ -139,15 +137,20 @@ class CForum extends CW2pObject {
 
 	public function delete(CAppUI $AppUI = null) {
         global $AppUI;
-
         $perms = $AppUI->acl();
-        $result = false;
 
         if ($perms->checkModuleItem('forums', 'delete', $this->project_id)) {
             $q = new DBQuery;
             $q->setDelete('forum_visits');
             $q->addWhere('visit_forum = ' . (int)$this->forum_id);
             $q->exec(); // No error if this fails, it is not important.
+
+            $q->clear();
+            $q->setDelete('forum_messages');
+            $q->addWhere('message_forum = ' . (int)$this->forum_id);
+            if (!$q->exec()) {
+                return db_error();
+            }
             $q->clear();
 
             $q->setDelete('forums');
@@ -156,17 +159,12 @@ class CForum extends CW2pObject {
                 $q->clear();
                 return db_error();
             }
-            $q->clear();
-            $q->setDelete('forum_messages');
-            $q->addWhere('message_forum = ' . (int)$this->forum_id);
-            if (!$q->exec()) {
-                $result = db_error();
-            } else {
-                addHistory('forums', $this->forum_id, 'delete', $this->forum_name);
-                $result = null;
+            if ($msg = parent::delete()) {
+                return $msg;
             }
+            return true;
         }
-		return $result;
+		return false;
 	}
 
 	public function getAllowedRecords($uid, $fields = '*', $orderby = '', $index = null, $extra = null) {
@@ -192,214 +190,5 @@ class CForum extends CW2pObject {
 		}
 		return parent::getAllowedRecords($uid, $fields, $orderby, $index, $extra);
 
-	}
-}
-
-class CForumMessage extends CW2pObject {
-	public $message_id = null;
-	public $message_forum = null;
-	public $message_parent = null;
-	public $message_author = null;
-	public $message_editor = null;
-	public $message_title = null;
-	public $message_date = null;
-	public $message_body = null;
-	public $message_published = null;
-
-	public function __construct() {
-		parent::__construct('forum_messages', 'message_id');
-	}
-
-	public function check() {
-        $errorArray = array();
-        $baseErrorMsg = get_class($this) . '::store-check failed - ';
-
-        if (0 == (int) $this->message_forum) {
-            $errorArray['message_forum'] = $baseErrorMsg . 'forum is not set';
-        }
-        if (0 == (int) $this->message_author) {
-            $errorArray['message_author'] = $baseErrorMsg . 'message author is not set';
-        }
-        if ('' == trim($this->message_title)) {
-            $errorArray['message_title'] = $baseErrorMsg . 'message title is not set';
-        }
-        if ('' == trim($this->message_body)) {
-            $errorArray['message_body'] = $baseErrorMsg . 'message body is not set';
-        }
-
-        return $errorArray;
-	}
-
-	public function store(CAppUI $AppUI = null) {
-        global $AppUI;
-
-        $perms = $AppUI->acl();
-        $stored = false;
-
-        $errorMsgArray = $this->check();
-
-        if (count($errorMsgArray) > 0) {
-            return $errorMsgArray;
-        }
-
-        if ($this->message_id && $perms->checkModuleItem('forums', 'edit', $this->forum_id)) {
-            $q = new DBQuery;
-            $q->setDelete('forum_visits');
-            $q->addWhere('visit_message = ' . (int)$this->message_id);
-			$q->exec();
-
-            if (($msg = parent::store())) {
-                return $msg;
-            }
-            addHistory('forum_messages', $this->message_id, 'update', $this->message_title, $this->message_id);
-            $stored = true;
-        }
-        if (0 == $this->message_id && $perms->checkModuleItem('forums', 'add')) {
-            $q = new DBQuery;
-            $this->message_date = $q->dbfnNow();
-            if (($msg = parent::store())) {
-                return $msg;
-            }
-            addHistory('forum_messages', $this->message_id, 'add', $this->message_title, $this->message_id);
-
-			$q->addTable('forum_messages');
-			$q->addQuery('count(message_id), MAX(message_date)');
-			$q->addWhere('message_forum = ' . (int)$this->message_forum);
-            $reply = $q->fetchRow();
-
-			//update forum descriptor
-			$forum = new CForum();
-            $forum->load($AppUI, $this->message_forum);
-			$forum->forum_message_count = $reply[0];
-			$forum->forum_last_date = $reply[1];
-			$forum->forum_last_id = $this->message_id;
-			$forum->store($AppUI);
-
-			$this->sendWatchMail(false);
-            $stored = true;
-        }
-        return $stored;
-	}
-
-	public function delete(CAppUI $AppUI = null) {
-        global $AppUI;
-
-        $perms = $AppUI->acl();
-        $result = false;
-
-        if ($perms->checkModuleItem('forums', 'delete', $this->project_id)) {
-            $q = new DBQuery;
-            $q->setDelete('forum_visits');
-            $q->addWhere('visit_message = ' . (int)$this->message_id);
-            $q->exec(); // No error if this fails, it is not important.
-            $q->clear();
-
-            $q->addTable('forum_messages');
-            $q->addQuery('message_forum');
-            $q->addWhere('message_id = ' . (int)$this->message_id);
-            $forumId = $q->loadResult();
-            $q->clear();
-
-            $q->setDelete('forum_messages');
-            $q->addWhere('message_id = ' . (int)$this->message_id);
-            if (!$q->exec()) {
-                $result = db_error();
-            } else {
-                $result = null;
-            }
-            $q->clear();
-
-            $q->addTable('forum_messages');
-            $q->addQuery('COUNT(message_id)');
-            $q->addWhere('message_forum = ' . (int)$forumId);
-            $messageCount = $q->loadResult();
-            $q->clear();
-
-            $q->addTable('forums');
-            $q->addUpdate('forum_message_count', $messageCount);
-            $q->addWhere('forum_id = ' . (int)$forumId);
-            $q->exec();
-        }
-		return $result;
-	}
-
-	public function sendWatchMail($debug = false) {
-		global $AppUI, $debug, $w2Pconfig;
-		$subj_prefix = $AppUI->_('forumEmailSubj', UI_OUTPUT_RAW);
-		$body_msg = $AppUI->_('forumEmailBody', UI_OUTPUT_RAW);
-
-		// Get the message from details.
-		$q = new DBQuery;
-		$q->addTable('users', 'u');
-		$q->addQuery('contact_email, contact_first_name, contact_last_name');
-		$q->addJoin('contacts', 'con', 'contact_id = user_contact', 'inner');
-		$q->addWhere('user_id = ' . (int)$this->message_author);
-		$res = $q->exec();
-		if ($row = $q->fetchRow()) {
-			$message_from = $row['contact_first_name'] . ' ' . $row['contact_last_name'] . '<' . $row['contact_email'] . '>';
-		} else {
-			$message_from = 'Unknown user';
-		}
-		// Get the forum name;
-		$q->clear();
-		$q->addTable('forums');
-		$q->addQuery('forum_name');
-		$q->addWhere('forum_id = \'' . $this->message_forum . '\'');
-		$res = $q->exec();
-		if ($row = $q->fetchRow()) {
-			$forum_name = $row['forum_name'];
-		} else {
-			$forum_name = 'Unknown';
-		}
-
-		// SQL-Query to check if the message should be delivered to all users (forced)
-		// In positive case there will be a (0,0,0) row in the forum_watch table
-		$q->clear();
-		$q->addTable('forum_watch');
-		$q->addQuery('*');
-		$q->addWhere('watch_user = 0 AND watch_forum = 0 AND watch_topic = 0');
-		$resAll = $q->exec();
-		$AllCount = db_num_rows($resAll);
-
-		$q->clear();
-		$q->addTable('users');
-		$q->addQuery('DISTINCT contact_email, user_id, contact_first_name, contact_last_name');
-		$q->leftJoin('contacts', 'con', 'contact_id = user_contact');
-
-		if ($AllCount < 1) {
-		//message is only delivered to users that checked the forum watch
-			$q->addTable('forum_watch');
-			$q->addWhere('user_id = watch_user AND (watch_forum = ' . (int)$this->message_forum . ' OR watch_topic = ' . (int)$this->message_parent . ')');
-		}
-
-		if (!($res = $q->exec(ADODB_FETCH_ASSOC))) {
-			$q->clear();
-			return;
-		}
-		if (db_num_rows($res) < 1) {
-			return;
-		}
-
-		$mail = new Mail;
-		$mail->Subject($subj_prefix . ' ' . $this->message_title, isset($GLOBALS['locale_char_set']) ? $GLOBALS['locale_char_set'] : '');
-
-		$body = $body_msg;
-
-		$body .= "\n\n" . $AppUI->_('Forum', UI_OUTPUT_RAW) . ': ' . $forum_name;
-		$body .= "\n" . $AppUI->_('Subject', UI_OUTPUT_RAW) . ': ' . $this->message_title;
-		$body .= "\n" . $AppUI->_('Message From', UI_OUTPUT_RAW) . ': ' . $message_from;
-		$body .= "\n\n" . W2P_BASE_URL . '/index.php?m=forums&a=viewer&forum_id=' . $this->message_forum;
-		$body .= "\n\n" . $this->message_body;
-
-		$mail->Body($body, isset($GLOBALS['locale_char_set']) ? $GLOBALS['locale_char_set'] : '');
-
-		while ($row = $q->fetchRow()) {
-			if ($mail->ValidEmail($row['contact_email'])) {
-				$mail->To($row['contact_email'], true);
-				$mail->Send();
-			}
-		}
-		$q->clear();
-		return;
 	}
 }
