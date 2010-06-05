09b88c58ff7358109424cf7a2380aa52a0d0e6c2
D. Keith Casey Jr <caseydk@nightcrawler.(none)>
2010-06-05 00:15:48 -0400

    updated the hook_search to not use the contact_email, etc fields;

--- modules/projects/projects.class.php
+++ modules/projects/projects.class.php
@@ -964,8 +964,8 @@ class CProject extends CW2pObject {
     $search['table_link'] = 'index.php?m=projects&a=view&project_id='; // first part of link
     $search['table_title'] = 'Projects';
     $search['table_orderby'] = 'project_name';
-    $search['search_fields'] = array('p.project_id', 'p.project_name', 'p.project_short_name', 'p.project_location', 'p.project_description', 'p.project_url', 'p.project_demo_url', 'con.contact_last_name', 'con.contact_first_name', 'con.contact_email', 'con.contact_title', 'con.contact_email2', 'con.contact_phone', 'con.contact_phone2', 'con.contact_address1', 'con.contact_notes');
-    $search['display_fields'] = array('p.project_id', 'p.project_name', 'p.project_short_name', 'p.project_location', 'p.project_description', 'p.project_url', 'p.project_demo_url', 'con.contact_last_name', 'con.contact_first_name', 'con.contact_email', 'con.contact_title', 'con.contact_email2', 'con.contact_phone', 'con.contact_phone2', 'con.contact_address1', 'con.contact_notes');
+    $search['search_fields'] = array('p.project_id', 'p.project_name', 'p.project_short_name', 'p.project_location', 'p.project_description', 'p.project_url', 'p.project_demo_url', 'con.contact_last_name', 'con.contact_first_name', 'con.contact_title', 'con.contact_address1', 'con.contact_notes');
+    $search['display_fields'] = array('p.project_id', 'p.project_name', 'p.project_short_name', 'p.project_location', 'p.project_description', 'p.project_url', 'p.project_demo_url', 'con.contact_last_name', 'con.contact_first_name', 'con.contact_title', 'con.contact_address1', 'con.contact_notes');
     $search['table_joins'] = array(array('table' => 'project_contacts', 'alias' => 'pc', 'join' => 'p.project_id = pc.project_id'), array('table' => 'contacts', 'alias' => 'con', 'join' => 'pc.contact_id = con.contact_id'));
 
     return $search;
