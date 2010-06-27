d488d5c0090f4e5bb852ffff2f45f764aa5cb474
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-06-27 19:35:37 +0000

    tweaks to allow for non-assigned/unowned tasks;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@1208 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/modules/tasks/tasks.class.php
+++ trunk/modules/tasks/tasks.class.php
@@ -301,9 +301,9 @@ class CTask extends CW2pObject {
 
         $q = new DBQuery;
         $q->addTable('tasks');
-        $q->addJoin('users', 'u1', 'u1.user_id = task_owner', 'inner');
-        $q->addJoin('contacts', 'ct', 'ct.contact_id = u1.user_contact', 'inner');
-        $q->addJoin('projects', 'p', 'p.project_id = task_project', 'inner');
+        $q->leftJoin('users', 'u1', 'u1.user_id = task_owner', 'outer');
+        $q->leftJoin('contacts', 'ct', 'ct.contact_id = u1.user_contact', 'outer');
+        $q->leftJoin('projects', 'p', 'p.project_id = task_project', 'outer');
         $q->addWhere('task_id = ' . (int)$taskId);
         $q->addQuery('tasks.*');
         $q->addQuery('project_name, project_color_identifier');
