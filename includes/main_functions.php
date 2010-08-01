71c0d2f4d7d5acf6c1cff671d788701c2b922ab6
D. Keith Casey Jr <keith@blueparabola.com>
2010-07-31 19:49:11 -0500

    tracked down the ambiguous reference for the contact_id; resolves http://bugs.web2project.net/view.php?id=516

--- includes/main_functions.php
+++ includes/main_functions.php
@@ -340,7 +340,7 @@ function w2PgetUsersList($stub = null, $where = null, $orderby = 'contact_first_
 	$q->addTable('users');
 	$q->addQuery('DISTINCT(user_id), user_username, contact_last_name, contact_first_name,
 		 company_name, contact_company, dept_id, dept_name, CONCAT(contact_first_name,\' \',contact_last_name) contact_name, user_type');
-	$q->addJoin('contacts', 'con', 'contact_id = user_contact', 'inner');
+	$q->addJoin('contacts', 'con', 'con.contact_id = user_contact', 'inner');
     $q->leftJoin('contacts_methods', 'cm', 'cm.contact_id = con.contact_id');
     $q->addWhere("cm.method_name = 'email_primary'");
     $q->addQuery('cm.method_value AS contact_email');
