0d4f6645e75ebf1ddca11cf59b9f5e659e96b8de
D. Keith Casey Jr <keith@blueparabola.com>
2010-08-03 02:05:31 -0500

    prepping for the next release

--- includes/version.php
+++ includes/version.php
@@ -3,5 +3,5 @@
 // Set the version components separately.
 $w2p_version_major = 2;
 $w2p_version_minor = 0;
-$w2p_version_patch = 0; // Can be set to null if not used
-$w2p_version_prepatch = null; // Set to null if a release version.
+$w2p_version_patch = 1; // Can be set to null if not used
+$w2p_version_prepatch = pre; // Set to null if a release version.
