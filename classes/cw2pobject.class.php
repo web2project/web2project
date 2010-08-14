17d1ccfeab19c8cf68e01266f1893ab220c87e0f
pedroix <pedroix@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-08-14 22:42:58 +0000

    Perms getAllowedItems and getDeniedItems were being called with a table name as first parameter instead of a module name. That would make things being naturally denied for all modules where the module name is different of the table name.
    So I made the CW2pObject call the permissions getAllowedItems and getDeniedItems with a module name, as it should, making sure that when the table and the module name match the call is still done correctly.
    
    Modules where the table name and the module name are different must use the third parameter (the module directory name) on their __construct() for the permissions to work correctly. 
    Because if not, the permissions will be checked against something that is not defined on the gacl tables, and therefore... denied.
    
    Example:
    
    	public function __construct()
    	{
    		parent::__construct('notes', 'note_id', 'notebook');
    	}
    
    In this case the table is 'notes' but the permissions that will be looked up on the gacl tables is for the 'notebook' module.
    If the third parameter was not set, then the permissions engine would look for 'notes' on the gacl_axo table, and since it is not there, the permissions engine would deny any action.
    
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1294 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- classes/cw2pobject.class.php
+++ classes/cw2pobject.class.php
@@ -1,12 +1,14 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+<?php /* $Id: BaseObject.class.php 1261 2010-07-30 05:41:59Z caseydk $ $URL: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk/classes/w2p/Core/BaseObject.class.php $ */
+/*
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+*/
 
-class CW2pObject extends w2p_Core_BaseObject {
-	public function __construct($table, $key) {
-		parent::__construct($table, $key);
-        //trigger_error("CW2pObject has been deprecated in v2.0 and will be removed in v3.0. Please use w2p_Core_Object instead.", E_USER_NOTICE );
+class CW2pObject extends w2p_Core_BaseObject
+{
+	public function __construct($table, $key, $module = '')
+	{
+		parent::__construct($table, $key, $module);
+		//trigger_error("CW2pObject has been deprecated in v2.0 and will be removed in v3.0. Please use w2p_Core_Object instead.", E_USER_NOTICE );
 	}
 }
