6b111efcf78c0aa1769ecca352d9b9d39f3df928
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-08-25 07:05:38 +0000

    tweaked the password handling to hide the password(s) by default; also since that would have saved over the password on every edit, added a 'modified' sentinal (think: isDirty); resolves http://bugs.web2project.net/view.php?id=555
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1321 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- modules/system/do_systemconfig_aed.php
+++ modules/system/do_systemconfig_aed.php
@@ -20,19 +20,31 @@ $rs = $q->loadResult();
 $q->clear();
 
 foreach ($_POST['w2Pcfg'] as $name => $value) {
-	$obj->config_name = $name;
+    $obj->config_name = $name;
 	$obj->config_value = $value;
 
 	// grab the appropriate id for the object in order to ensure
 	// that the db is updated well (config_name must be unique)
 	$obj->config_id = $_POST['w2PcfgId'][$name];
+    $update = false;
 
-	// prepare (and translate) the module name ready for the suffix
-	$AppUI->setMsg('System Configuration');
-	if (($msg = $obj->store($AppUI))) {
-		$AppUI->setMsg($msg, UI_MSG_ERROR);
-	} else {
-		$AppUI->setMsg('updated', UI_MSG_OK, true);
-	}
+    // This is really kludgy, but it works.. suggestions?
+    if (strpos($name, '_pass') !== false) {
+        if (1 == $_POST[$name.'_mod']) {
+            $update = true;
+        }
+    } else {
+        $update = true;
+    }
+
+    if ($update) {
+        // prepare (and translate) the module name ready for the suffix
+        $AppUI->setMsg('System Configuration');
+        if (($msg = $obj->store($AppUI))) {
+            $AppUI->setMsg($msg, UI_MSG_ERROR);
+        } else {
+            $AppUI->setMsg('updated', UI_MSG_OK, true);
+        }
+    }
 }
 $AppUI->redirect();
