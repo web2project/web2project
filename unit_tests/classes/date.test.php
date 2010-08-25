91c898daf35a70e24cb7587a5b27e5783b719d94
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-08-25 07:25:52 +0000

    Adding more tests for CDate constructor
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1322 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- unit_tests/classes/date.test.php
+++ unit_tests/classes/date.test.php
@@ -24,6 +24,7 @@
  * Necessary global variables
  */
 global $AppUI;
+global $_DATE_TIMEZONE_DATA;
 
 require_once '../base.php';
 require_once W2P_BASE_DIR . '/includes/main_functions.php';
@@ -54,8 +55,8 @@ class Date_Test extends PHPUnit_Framework_TestCase
      */
     public function testConstructorNoDateTimeNoTz()
     {
-        $date = new CDate();
-        $datetime = new DateTime();
+        $date       = new CDate();
+        $datetime   = new DateTime();
 
         $this->assertType('CDate',                  $date);
         $this->assertEquals($datetime->format('Y'), $date->year);
@@ -71,6 +72,104 @@ class Date_Test extends PHPUnit_Framework_TestCase
         $this->assertEquals('BST',                  $date->tz['dstshortname']);
         $this->assertEquals('Europe/London',        $date->tz['id']);
         $this->assertTrue($date->tz['hasdst']);
+    }
+
+    /**
+     * Tests constructor with a datetime, but no timezone
+     */
+    public function testConstructorDateTimeNoTz()
+    {
+        $date       = new CDate('2010-08-07 11:00:00');
+        $datetime   = new DateTime('2010-08-07 11:00:00');
+
+        $this->assertType('CDate',                  $date);
+        $this->assertEquals($datetime->format('Y'), $date->year);
+        $this->assertEquals($datetime->format('m'), $date->month);
+        $this->assertEquals($datetime->format('d'), $date->day);
+        $this->assertEquals($datetime->format('H'), $date->hour);
+        $this->assertEquals($datetime->format('i'), $date->minute);
+        $this->assertEquals($datetime->format('s'), $date->second);
+        $this->assertEquals(0,                      $date->tz['offset']);
+        $this->assertEquals('Greenwich Mean Time',  $date->tz['longname']);
+        $this->assertEquals('GMT',                  $date->tz['shortname']);
+        $this->assertEquals('British Summer Time',  $date->tz['dstlongname']);
+        $this->assertEquals('BST',                  $date->tz['dstshortname']);
+        $this->assertEquals('Europe/London',        $date->tz['id']);
+        $this->assertTrue($date->tz['hasdst']);
+    }
+
+    /**
+     * Tests constructor with a datetime and timezone
+     */
+    public function testConstructorDateTimeTz()
+    {
+        $date       = new CDate('2010-08-07 11:00:00', 'America/Halifax');
+        $datetime   = new DateTime('2010-08-07 11:00:00', new DateTimeZone('America/Halifax'));
+
+        $this->assertType('CDate',                      $date);
+        $this->assertEquals($datetime->format('Y'),     $date->year);
+        $this->assertEquals($datetime->format('m'),     $date->month);
+        $this->assertEquals($datetime->format('d'),     $date->day);
+        $this->assertEquals($datetime->format('H'),     $date->hour);
+        $this->assertEquals($datetime->format('i'),     $date->minute);
+        $this->assertEquals($datetime->format('s'),     $date->second);
+        $this->assertEquals(-14400000,                  $date->tz['offset']);
+        $this->assertEquals('Atlantic Standard Time',   $date->tz['longname']);
+        $this->assertEquals('AST',                      $date->tz['shortname']);
+        $this->assertEquals('Atlantic Daylight Time',   $date->tz['dstlongname']);
+        $this->assertEquals('ADT',                      $date->tz['dstshortname']);
+        $this->assertEquals('America/Halifax',          $date->tz['id']);
+        $this->assertTrue($date->tz['hasdst']);
+    }
+
+    /**
+     * Tests constructor with an invalid datetime
+     */
+    public function testConstructorInvalidDateTime()
+    {
+        $date = new CDate('2010-35-35 28:65:85');
+
+        $this->assertType('CDate',                  $date);
+        $this->assertEquals(2010,                   $date->year);
+        $this->assertEquals(35,                     $date->month);
+        $this->assertEquals(35,                     $date->day);
+        $this->assertEquals(28,                     $date->hour);
+        $this->assertEquals(65,                     $date->minute);
+        $this->assertEquals(85,                     $date->second);
+        $this->assertEquals(0,                      $date->tz['offset']);
+        $this->assertEquals('Greenwich Mean Time',  $date->tz['longname']);
+        $this->assertEquals('GMT',                  $date->tz['shortname']);
+        $this->assertEquals('British Summer Time',  $date->tz['dstlongname']);
+        $this->assertEquals('BST',                  $date->tz['dstshortname']);
+        $this->assertEquals('Europe/London',        $date->tz['id']);
+        $this->assertEquals('2010-35-35 28:65:85',  $date->getDate()); // WTF?
+        $this->assertTrue($date->tz['hasdst']);
+    }
+
+    /**
+     * Tests constructor with an invalid timezone
+     *
+     * expectedException PHPUnit_Framework_Error
+     */
+    public function testConstructorInvalidTimezone()
+    {
+        $date = new CDate('2010-08-07 22:10:27', 'Halifax');
+        $datetime = new DateTime('2010-08-07 22:10:27');
+
+        $this->assertType('CDate',                  $date);
+        $this->assertEquals($datetime->format('Y'), $date->year);
+        $this->assertEquals($datetime->format('m'), $date->month);
+        $this->assertEquals($datetime->format('d'), $date->day);
+        $this->assertEquals($datetime->format('H'), $date->hour);
+        $this->assertEquals($datetime->format('i'), $date->minute);
+        $this->assertEquals($datetime->format('s'), $date->second);
+        $this->assertEquals('Halifax',              $date->tz['id']);
+        $this->assertFalse(isset($data->tz['offset']));
+        $this->assertFalse(isset($data->tz['longname']));
+        $this->assertFalse(isset($data->tz['shortname']));
+        $this->assertFalse(isset($data->tz['dstlongname']));
+        $this->assertFalse(isset($data->tz['dstshortname']));
+        $this->assertFalse(isset($data->tz['hasdst']));
     }
 
     /**
