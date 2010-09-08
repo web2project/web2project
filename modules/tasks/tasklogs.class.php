2b3ff583be4a247edac12364996ef8d49feb4825
pedroix <pedroix@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-09-08 17:19:31 +0000

    Fixed and issue where tasklogs with empty work hours or work hours = 0 were being rejected. http://forums.web2project.net/viewtopic.php?t=2536
    Ended up adding the possibility for the users to enter time in a human readable format (ex: 2:15)
    It will translate internally to decimal format automatically.
    If still it is not recognized, it will store 0 worked hours. 
    
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1344 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- modules/tasks/tasklogs.class.php
+++ modules/tasks/tasklogs.class.php
@@ -29,274 +29,282 @@
 class CTaskLog extends CW2pObject
 {
 
-    /**
-     * The id of the task log
-     *
-     * @var int
-     * @access public
-     */
+	/**
+	 * The id of the task log
+	 *
+	 * @var int
+	 * @access public
+	 */
 	public $task_log_id = null;
 
-    /**
-     * The id of the task this task log belongs to
-     *
-     * @var int
-     * @access public
-     */
+	/**
+	 * The id of the task this task log belongs to
+	 *
+	 * @var int
+	 * @access public
+	 */
 	public $task_log_task = null;
 
-    /**
-     * The name of the task log
-     *
-     * @var string
-     * @access public
-     */
+	/**
+	 * The name of the task log
+	 *
+	 * @var string
+	 * @access public
+	 */
 	public $task_log_name = null;
 
-    /**
-     * Description of the task log
-     *
-     * @var string
-     * @access public
-     */
+	/**
+	 * Description of the task log
+	 *
+	 * @var string
+	 * @access public
+	 */
 	public $task_log_description = null;
 
-    /**
-     * The id of user that created the task log
-     *
-     * @var int
-     * @access public
-     */
+	/**
+	 * The id of user that created the task log
+	 *
+	 * @var int
+	 * @access public
+	 */
 	public $task_log_creator = null;
 
-    /**
-     * The number of hours worked for the task log
-     *
-     * @var float
-     * @access public
-     */
+	/**
+	 * The number of hours worked for the task log
+	 *
+	 * @var float
+	 * @access public
+	 */
 	public $task_log_hours = null;
 
-    /**
-     * Date of the task log, stored as a date in the db
-     *
-     * @var string
-     * @access public
-     */
+	/**
+	 * Date of the task log, stored as a date in the db
+	 *
+	 * @var string
+	 * @access public
+	 */
 	public $task_log_date = null;
 
-    /**
-     * Cost code for the task log
-     * Integer value, can be defined by user
-     *
-     * @var int
-     * @access public
-     */
+	/**
+	 * Cost code for the task log
+	 * Integer value, can be defined by user
+	 *
+	 * @var int
+	 * @access public
+	 */
 	public $task_log_costcode = null;
 
-    /**
-     * Indicates if there was a problem while working on the task
-     *
-     * @var bool
-     * @access public
-     */
+	/**
+	 * Indicates if there was a problem while working on the task
+	 *
+	 * @var bool
+	 * @access public
+	 */
 	public $task_log_problem = null;
 
-    /**
-     * The id of reference for the task log
-     * default values are 0 - Not Defined, 1 - Email, 2 - Helpdesk, 3 - Phone Call,
-     * 4 - Fax however they can be altered by user
-     *
-     * @var int
-     * @access public
-     */
+	/**
+	 * The id of reference for the task log
+	 * default values are 0 - Not Defined, 1 - Email, 2 - Helpdesk, 3 - Phone Call,
+	 * 4 - Fax however they can be altered by user
+	 *
+	 * @var int
+	 * @access public
+	 */
 	public $task_log_reference = null;
 
-    /**
-     * Url related to the work done in this task log
-     *
-     * @var string
-     * @access public
-     */
+	/**
+	 * Url related to the work done in this task log
+	 *
+	 * @var string
+	 * @access public
+	 */
 	public $task_log_related_url = null;
 
-    /**
-     * Datetime stamp of when task log was created
-     * Is handled by database layer
-     *
-     * @var string
-     * @access public
-     */
+	/**
+	 * Datetime stamp of when task log was created
+	 * Is handled by database layer
+	 *
+	 * @var string
+	 * @access public
+	 */
 	public $task_log_created = null;
 
-    /**
-     * Datetime stamp of when task log was laste updated
-     * Is handled by database layer
-     *
-     * @var string
-     * @access public
-     */
+	/**
+	 * Datetime stamp of when task log was laste updated
+	 * Is handled by database layer
+	 *
+	 * @var string
+	 * @access public
+	 */
 	public $task_log_updated = null;
 
-    /**
-     * Constructor for class
-     *
-     * @return void
-     *
-     * @access public
-     */
+	/**
+	 * Constructor for class
+	 *
+	 * @return void
+	 *
+	 * @access public
+	 */
 	public function __construct()
-    {
-	  parent::__construct('task_log', 'task_log_id');
+	{
+		parent::__construct('task_log', 'task_log_id');
 
 		// ensure changes to checkboxes are honoured
 		$this->task_log_problem = intval($this->task_log_problem);
 	}
 
-    /**
-     * Stores the current task log in the database updating the task_log_updated
-     * field appropriately. Then updates total hours worked cache on task.
-     *
-     * @return void
-     *
-     * @access public
-     */
+	/**
+	 * Stores the current task log in the database updating the task_log_updated
+	 * field appropriately. Then updates total hours worked cache on task.
+	 *
+	 * @return void
+	 *
+	 * @access public
+	 */
 	public function store(CAppUI $AppUI = null)
-    {
-        global $AppUI;
-        $perms = $AppUI->acl();
+	{
+		global $AppUI;
+		$perms = $AppUI->acl();
 
-        $errorMsgArray = $this->check();
+		$errorMsgArray = $this->check();
 
-        if (count($errorMsgArray) > 0) {
-            return $errorMsgArray;
-        }
+		if (count($errorMsgArray) > 0) {
+			return $errorMsgArray;
+		}
 
-        $q = new DBQuery();
-        $this->task_log_updated = $q->dbfnNowWithTZ();
+		$q = new DBQuery();
+		$this->task_log_updated = $q->dbfnNowWithTZ();
 
-        if ($this->task_log_date) {
-            $date = new CDate($this->task_log_date);
-            $this->task_log_date = $date->format(FMT_DATETIME_MYSQL);
-        }
-        $dot = strpos($this->task_log_hours, ':');
-        if ($dot > 0) {
-            $log_duration_minutes = sprintf('%.3f', substr($this->task_log_hours, $dot + 1) / 60.0);
-            $this->task_log_hours = floor($this->task_log_hours) + $log_duration_minutes;
-        }
-        $this->task_log_hours = round($this->task_log_hours, 3);
-        $this->task_log_costcode = cleanText($this->task_log_costcode);
+		if ($this->task_log_date) {
+			$date = new CDate($this->task_log_date);
+			$this->task_log_date = $date->format(FMT_DATETIME_MYSQL);
+		}
+		$dot = strpos($this->task_log_hours, ':');
+		if ($dot > 0) {
+			$log_duration_minutes = sprintf('%.3f', substr($this->task_log_hours, $dot + 1) / 60.0);
+			$this->task_log_hours = floor($this->task_log_hours) + $log_duration_minutes;
+		}
+		$this->task_log_hours = round($this->task_log_hours, 3);
+		$this->task_log_costcode = cleanText($this->task_log_costcode);
 
-        if ($this->task_log_id && $perms->checkModuleItem('task_log', 'edit', $this->task_log_id)) {
-            if (($msg = parent::store())) {
-                return $msg;
-            }
-            $stored = true;
-            $this->updateHoursWorked($this->task_log_task);
-        }
-        if (0 == $this->task_log_id && $perms->checkModuleItem('task_log', 'add')) {
-            $this->task_log_created = $q->dbfnNowWithTZ();
-            if (($msg = parent::store())) {
-                return $msg;
-            }
-            $stored = true;
-            $this->updateHoursWorked($this->task_log_task);
-        }
+		if ($this->task_log_id && $perms->checkModuleItem('task_log', 'edit', $this->task_log_id)) {
+			if (($msg = parent::store())) {
+				return $msg;
+			}
+			$stored = true;
+			$this->updateHoursWorked($this->task_log_task);
+		}
+		if (0 == $this->task_log_id && $perms->checkModuleItem('task_log', 'add')) {
+			$this->task_log_created = $q->dbfnNowWithTZ();
+			if (($msg = parent::store())) {
+				return $msg;
+			}
+			$stored = true;
+			$this->updateHoursWorked($this->task_log_task);
+		}
 
-        return $stored;
+		return $stored;
 	}
 
-    /**
-     * Deletes the current task log from the database. Then updated total hours
-     * worked cache on task.
-     *
-     * @return void
-     *
-     * @access public
-     */
+	/**
+	 * Deletes the current task log from the database. Then updated total hours
+	 * worked cache on task.
+	 *
+	 * @return void
+	 *
+	 * @access public
+	 */
 	public function delete(CAppUI $AppUI = null)
-    {
-        global $AppUI;
-        $perms = $AppUI->acl();
+	{
+		global $AppUI;
+		$perms = $AppUI->acl();
 
-        $this->load($this->task_log_id);
-        //$task_log_task = $this->task_log_task;
+		$this->load($this->task_log_id);
+		//$task_log_task = $this->task_log_task;
 
-        if ($perms->checkModuleItem('task_log', 'delete', $this->task_log_id)) {
-            if ($msg = parent::delete()) {
-                return $msg;
-            }
-            $this->updateHoursWorked($this->task_log_task);
-            return true;
-        }
-        return false;
+		if ($perms->checkModuleItem('task_log', 'delete', $this->task_log_id)) {
+			if ($msg = parent::delete()) {
+				return $msg;
+			}
+			$this->updateHoursWorked($this->task_log_task);
+			return true;
+		}
+		return false;
 	}
 
-    /**
-     * Updates the total hours worked cache on task.
-     *
-     * @param int $task_log_task that task id of task this task log is for
-     *
-     * @return void
-     *
-     * @access public
-     */
+	/**
+	 * Updates the total hours worked cache on task.
+	 *
+	 * @param int $task_log_task that task id of task this task log is for
+	 *
+	 * @return void
+	 *
+	 * @access public
+	 */
 	private function updateHoursWorked($task_log_task)
-    {
+	{
 		$q = new DBQuery();
 		$q->addQuery('SUM(task_log_hours)');
 		$q->addTable('task_log');
-		$q->addWhere('task_log_task = ' . (int) $task_log_task);
+		$q->addWhere('task_log_task = ' . (int)$task_log_task);
 		$totalHours = $q->loadResult();
 
 		CTask::updateHoursWorked($task_log_task, $totalHours);
 	}
 
-    /**
-     * Trims all vars of this object of type string, except the task_log_description.
-     *
-     * @return void
-     *
-     * @access public
-     */
+	/**
+	 * Trims all vars of this object of type string, except the task_log_description.
+	 *
+	 * @return void
+	 *
+	 * @access public
+	 */
 	public function w2PTrimAll()
-    {
+	{
 		$spacedDescription = $this->task_log_description;
 		parent::w2PTrimAll();
 		$this->task_log_description = $spacedDescription;
 	}
 
 	/**
-     * Checks the class for validity
-     *
-     * @return null
-     */
+	 * Checks the class for validity
+	 *
+	 * @return null
+	 */
 	public function check()
-    {
-        // ensure the integrity of some variables
-        $errorArray = array();
-        $baseErrorMsg = get_class($this) . '::store-check failed - ';
+	{
+		// ensure the integrity of some variables
+		$errorArray = array();
+		$baseErrorMsg = get_class($this) . '::store-check failed - ';
 
-        if (!((float) $this->task_log_hours)) {
-            $errorArray['task_log_hours'] = $baseErrorMsg . 'task_log_hours is not a number';
-        }
+		if (!((float)$this->task_log_hours)) {
+			// before evaluating a non-float work hour as 0 lets try to check if user is trying
+			// to enter in hour:minute format and convert it to decimal. If that is not the format
+			// then we consider that there was no time worked at all (i.e. 0 time worked)
+			$log_time_hour = $log_time_minute = 0;
+			list($log_time_hour, $log_time_minute) = explode(':', $this->task_log_hours);
+			$this->task_log_hours = ((int)$log_time_hour) + (((int)$log_time_minute) / 60);
+			if (!((float)$this->task_log_hours)) {
+				$this->task_log_hours = 0;
+			}
+		}
 
-        return $errorArray;
+		return $errorArray;
 	}
 
-    /**
-     * Determines whether the currently logged in user can delete this task log.
-     *
-     * @global AppUI $AppUI global user permissions
-     *
-     * @param string by ref $msg error msg to be populated on failure
-     * @param int optional $oid key to check
-     * @param array $joins optional list of tables to join on
-     *
-     * @return bool
-     */
+	/**
+	 * Determines whether the currently logged in user can delete this task log.
+	 *
+	 * @global AppUI $AppUI global user permissions
+	 *
+	 * @param string by ref $msg error msg to be populated on failure
+	 * @param int optional $oid key to check
+	 * @param array $joins optional list of tables to join on
+	 *
+	 * @return bool
+	 */
 	public function canDelete(&$msg, $oid = null, $joins = null)
-    {
+	{
 		global $AppUI;
 		$q = new DBQuery;
 
@@ -347,20 +355,20 @@ class CTaskLog extends CW2pObject
 		return true;
 	}
 
-    /**
-     * Get a list of task logs the current user is allowed to access
-     *
-     * @global AppUI $AppUI global user permissions
-     * @param int $uid user id to test
-     * @param string $fields optional fields to be returned by the query, default is all
-     * @param string $orderby optional sort order for the query
-     * @param int $index optional name of field to index the returned array
-     * @param array $extra optional array of additional sql parameters (from and where supported)
-     *
-     * @return array
-     */
+	/**
+	 * Get a list of task logs the current user is allowed to access
+	 *
+	 * @global AppUI $AppUI global user permissions
+	 * @param int $uid user id to test
+	 * @param string $fields optional fields to be returned by the query, default is all
+	 * @param string $orderby optional sort order for the query
+	 * @param int $index optional name of field to index the returned array
+	 * @param array $extra optional array of additional sql parameters (from and where supported)
+	 *
+	 * @return array
+	 */
 	public function getAllowedRecords($uid, $fields = '*', $orderby = '', $index = null, $extra = null)
-    {
+	{
 		global $AppUI;
 		$oTsk = new CTask();
 
