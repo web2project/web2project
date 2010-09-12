47961e617e3e1db0e4fca3c354349ee5ab8d5e95
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-09-12 01:07:39 +0000

    Adding test for CTask->getAssignedUsers
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1377 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- unit_tests/modules/tasks.test.php
+++ unit_tests/modules/tasks.test.php
@@ -2230,11 +2230,6 @@ class Tasks_Test extends PHPUnit_Extensions_Database_TestCase
         $this->assertEquals(42,                                 $assigned_users[1][11]);
         $this->assertEquals('Person',                           $assigned_users[1][12]);
 
-        $assigned_users = $this->obj->getAssignedUsers(200);
-
-        $this->assertEquals(0, count($assigned_users));
-    }
-
     /**
      * Test getting a list of dependencies
      */
@@ -3791,3 +3786,7 @@ class Tasks_Test extends PHPUnit_Extensions_Database_TestCase
         $this->assertTablesEqual($xml_file_dataset->getTable('tasks'), $xml_db_dataset->getTable('tasks'));
     }
 }
+=======
+}
+?>
+>>>>>>> Adding test for CTask->getAssignedUsers:unit_tests/modules/tasks.test.php
