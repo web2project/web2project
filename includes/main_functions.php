3aadefa97d97bc15fb30f5ea344b064b8c8c00e9
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-09-12 01:16:16 +0000

    Moving CTaskLog class to its own file. Formatting to match PEAR coding standard. Adjusting autoload accordingly.
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1430 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- includes/main_functions.php
+++ includes/main_functions.php
@@ -1390,4 +1390,28 @@ function w2p_textarea($content)
   }
 
   return $result;
+<<<<<<< HEAD:includes/main_functions.php
 }
+=======
+}
+
+function w2p_format_datetime($baseTZ, $userTZ, $format = '', $datetime = 0)
+{
+    date_default_timezone_set('America/New_York');
+
+    $rawDatetime = ($datetime) ? $datetime : time();
+    $baseTime = new DateTimeZone($baseTZ);
+    $userTime = new DateTimeZone($userTZ);
+    $x = new DateTime();
+
+    //convert to GMT
+    $baseOffset = $baseTime->getOffset($x);
+    $newDatetime = $rawDatetime - $baseOffset;
+
+    //convert to user TZ
+    $userOffset = $userTime->getOffset($x);
+    $newDatetime += $userOffset;
+
+    return date($format, $newDatetime);
+}
+>>>>>>> Moving CTaskLog class to its own file. Formatting to match PEAR coding standard. Adjusting autoload accordingly.:includes/main_functions.php
