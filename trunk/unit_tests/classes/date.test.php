c938bbe013c9a2ebe859ce99a455ee146d03038e
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-09-12 03:53:40 +0000

    Adding test for CDate->next_working_day
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project@1443 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- trunk/unit_tests/classes/date.test.php
+++ trunk/unit_tests/classes/date.test.php
@@ -788,6 +788,33 @@ class Date_Test extends PHPUnit_Framework_TestCase
 
         $this->assertEquals('pm', $date->getAMPM());
     }
+
+    /**
+     * Tests next_working_day when not a working day, and not perserving
+     * hours
+     */
+    public function testNextWorkingDayNotWorkingDayNoPerserveHours()
+    {
+        global $w2Pconfig;
+
+        // Save old working days, day start and end
+        $old_working_days               = $w2Pconfig['cal_working_days'];
+        $old_cal_day_start              = $w2Pconfig['cal_day_start'];
+        $old_cal_day_end                = $w2Pconfig['cal_day_end'];
+        $w2Pconfig['cal_working_days']  = '1,2,3,4,5';
+        $w2Pconfig['cal_day_start']     = 9;
+        $w2Pconfig['cal_day_end']       = 17;
+
+        $date = new CDate('2010-08-07 00:00:00');
+        $date->next_working_day();
+
+        $this->assertEquals('2010-08-09 09:00:00', $date->getDate(DATE_FORMAT_ISO));
+
+        // Restore old working days, day start and end
+        $w2Pconfig['cal_working_days']  = $old_working_days;
+        $w2Pconfig['cal_day_start']     = $old_cal_day_start;
+        $w2Pconfig['cal_day_end']       = $old_cal_day_end;
+    }
     /**
      * Tests converting between timezones
      */
