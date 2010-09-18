29f0b2e11440a18ce32cbcb2924bc099dee6d4a7
trevormorse <trevor.morse@gmail.com>
2010-09-19 01:52:26 +0800

    Updating calcFinish. Previously it was adding hours when it should have been adding days. It was using a addHours function that did not exist, it now uses addDays which is correct.

--- classes/w2p/Utilities/Date.class.php
+++ classes/w2p/Utilities/Date.class.php
@@ -483,7 +483,7 @@ class w2p_Utilities_Date extends Date {
 			}
 			$inc -= $hoursToAddToFirstDay;
 			$hoursToAddToLastDay = $inc % $workHours;
-			$fullWorkingDays = floor(($inc - $hoursToAddToLastDay) / $workHours);
+            $fullWorkingDays = floor(($inc - $hoursToAddToLastDay) / $workHours);
 
 			if ($hoursToAddToLastDay <= 0 && !($hoursToAddToFirstDay == $workHours)) {
 				$f->setHour($f->getHour() + $hoursToAddToFirstDay);
@@ -505,7 +505,7 @@ class w2p_Utilities_Date extends Date {
 			$g = false;
 			for ($i = 0, $i_cmp = ceil($fullWorkingDays); $i < $i_cmp; $i++) {
 				if (!$g) {
-					$f->addHours(1);
+					$f->addDays(1);
 				}
 				$g = false;
 				// calculate overriden non-working days
