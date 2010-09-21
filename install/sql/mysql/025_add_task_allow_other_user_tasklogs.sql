830881f5c84c4cf48ed0013909abc7e5e460eef7
D. Keith Casey Jr <keith@blueparabola.com>
2010-09-21 17:53:58 -0500

    updating for the v2.1 release;

--- install/sql/mysql/025_add_task_allow_other_user_tasklogs.sql
+++ install/sql/mysql/025_add_task_allow_other_user_tasklogs.sql
@@ -4,3 +4,6 @@ ALTER TABLE `tasks` ADD COLUMN `task_allow_other_user_tasklogs` int(1) NOT NULL 
 
 -- This will add a field that indicates who created/updated the task log
 ALTER TABLE `task_log` ADD COLUMN `task_log_record_creator` int(10) unsigned NOT NULL;
+
+INSERT INTO `w2pversion` (`code_revision`, `code_version`, `db_version`, `last_db_update`, `last_code_update`)
+	VALUES (1465, '2.1.0', 25, now(), now());
