2ebf5bb42d0c5a2f175597c345166279955c6358
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2010-12-19 06:37:38 +0000

    added a delete to make sure the values actually get set properly;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1545 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- install/sql/mysql/028_system_configs.sql
+++ install/sql/mysql/028_system_configs.sql
@@ -1,8 +1,10 @@
 -- This sets a few default values for pagination and admin email. These
 --   resolve a pair of requests/issues for v2.2
 
+DELETE FROM `config` WHERE `config_name` = 'page_size';
 INSERT INTO `config` (`config_name`, `config_value`, `config_group`, `config_type`)
     VALUES ('page_size', '50', 'admin_system', 'text');
 
+DELETE FROM `config` WHERE `config_name` = 'admin_email';
 INSERT INTO `config` (`config_name`, `config_value`, `config_group`, `config_type`)
     VALUES ('admin_email', 'admin@web2project.net', 'admin_system', 'text');
