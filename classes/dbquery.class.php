33342a7d8ac1063980ef9c7a78692acf8e4aa1a6
caseysoftware <keith@blueparabola.com>
2010-12-28 01:03:08 -0600

    corrected class name

--- classes/dbquery.class.php
+++ classes/dbquery.class.php
@@ -5,6 +5,6 @@ class DBQuery extends w2p_Database_Query
     public function __construct($prefix = null, $query_db = null)
     {
         parent::__construct($prefix, $query_db);
-        trigger_error("DBQuery has been deprecated in v2.0 and will be removed by v4.0. Please use w2p_Database_DBQuery instead.", E_USER_NOTICE );
+        trigger_error("DBQuery has been deprecated in v2.0 and will be removed by v4.0. Please use w2p_Database_Query instead.", E_USER_NOTICE );
     }
 }
