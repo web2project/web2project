faf76981ef73d41171ef76e8167bb2454ded1a60
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2011-01-07 06:08:10 +0000

    removed the dead class; added the upgrade path for dotproject 2.1.5
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1573 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- classes/w2p/Core/UpgradeManager.class.php
+++ classes/w2p/Core/UpgradeManager.class.php
@@ -133,6 +133,7 @@ class w2p_Core_UpgradeManager {
             case '2.1.2':
             case '2.1.3':
             case '2.1.4':
+            case '2.1.5':
                 $errorMessages = $this->_applySQLUpdates('dp_to_w2p1.sql', $dbConn);
                 $allErrors = array_merge($allErrors, $errorMessages);
 
