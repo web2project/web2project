6a4fd6777fe13b96c1ada4fa4b17b7038ea38e03
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2011-01-11 07:09:04 +0000

    converted controllers to use the new w2p_Controller_Base;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1579 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- modules/companies/do_company_aed.php
+++ modules/companies/do_company_aed.php
@@ -3,25 +3,11 @@ if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
 
-$del = (int) w2PgetParam($_POST, 'del', 0);
+$delete = (int) w2PgetParam($_POST, 'del', 0);
 
-$obj = new CCompany();
-if (!$obj->bind($_POST)) {
-    $AppUI->setMsg($obj->getError(), UI_MSG_ERROR);
-    $AppUI->redirect();
-}
+$controller = new w2p_Controllers_Base(
+                    new CCompany(), $delete, 'Company', 'm=companies', 'm=companies&a=addedit'
+                  );
 
-$action = ($del) ? 'deleted' : 'stored';
-$result = ($del) ? $obj->delete($AppUI) : $obj->store($AppUI);
-
-if (is_array($result)) {
-    $AppUI->setMsg($result, UI_MSG_ERROR, true);
-    $AppUI->holdObject($obj);
-    $AppUI->redirect('m=companies&a=addedit');
-}
-if ($result) {
-    $AppUI->setMsg('Company '.$action, UI_MSG_OK, true);
-    $AppUI->redirect('m=companies');
-} else {
-    $AppUI->redirect('m=public&a=access_denied');
-}
+$AppUI = $controller->process($AppUI, $_POST);
+$AppUI->redirect($controller->resultPath);
--- modules/forums/do_post_aed.php
+++ modules/forums/do_post_aed.php
@@ -3,29 +3,19 @@ if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
 
-$del = (int) w2PgetParam($_POST, 'del', 0);
-$isNotNew = (int) w2PgetParam($_POST, 'message_id', 0);
+$delete = (int) w2PgetParam($_POST, 'del', 0);
+
 $message_forum = (int) w2PgetParam($_POST, 'message_forum', 0);
 $message_parent = (int) w2PgetParam($_POST, 'message_parent', 0);
+$successPath = ($delete) ? 'm=forums' : 'm=forums&a=viewer&forum_id='.
+        $message_forum.'&message_parent='.$message_parent;
+$errorPath = 'm=forums&a=viewer&forum_id='.$message_forum.'&message_parent='.
+        $message_parent.'&post_message=1';
 
-$obj = new CForumMessage();
-if (!$obj->bind($_POST)) {
-    $AppUI->setMsg($obj->getError(), UI_MSG_ERROR);
-    $AppUI->redirect();
-}
 
-$action = ($del) ? 'deleted' : 'stored';
-$result = ($del) ? $obj->delete($AppUI) : $obj->store($AppUI);
+$controller = new w2p_Controllers_Base(
+                    new CForumMessage(), $delete, 'Message', $successPath, $errorPath
+                  );
 
-if (is_array($result)) {
-    $AppUI->setMsg($result, UI_MSG_ERROR, true);
-    $AppUI->holdObject($obj);
-    $AppUI->redirect('m=forums&a=viewer&forum_id='.$message_forum.'&message_parent='.$message_parent.'&post_message=1');
-}
-if ($result) {
-    $AppUI->setMsg('Message '.$action, UI_MSG_OK, true);
-    $redirect = ($del) ? 'm=forums' : 'm=forums&a=viewer&forum_id='.$message_forum.'&message_parent='.$message_parent;
-    $AppUI->redirect($redirect);
-} else {
-    $AppUI->redirect('m=public&a=access_denied');
-}
+$AppUI = $controller->process($AppUI, $_POST);
+$AppUI->redirect($controller->resultPath);
