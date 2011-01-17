770ea725da385f4dcc9ccfc8ba62dcb6570fd8ee
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2011-01-17 07:26:52 +0000

    turning on deprecation warnings;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1594 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- classes/mail.class.php
+++ classes/mail.class.php
@@ -5,6 +5,6 @@ class Mail extends w2p_Utilities_Mail
     function __construct()
     {
         parent::__construct();
-        //trigger_error("Mail has been deprecated in v2.0 and will be removed in v3.0. Please use w2p_Utilities_Mail instead.", E_USER_NOTICE );
+        trigger_error("Mail has been deprecated in v2.0 and will be removed by v4.0. Please use w2p_Utilities_Mail instead.", E_USER_NOTICE );
     }
 }
