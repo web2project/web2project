f1b570f58375048c1f017b574306bc33c395d8e0
caseysoftware <keith@blueparabola.com>
2011-02-12 15:26:31 -0600

    This file contains the primary controler for Xajax inside web2Project.
    It should contain web2Project server side overall modules available xajax support functions usually for DOM manipulation.
    Client side overall modules available xajax functions should be at /js/base.js

---
+++ classes/ajax.class.php
@@ -0,0 +1,14 @@
+<?php /* $Id: ajax.class.php 38 2008-02-11 11:38:51Z pedroix $ $URL: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk/classes/ajax.class.php $ */
+include_once $AppUI->getLibraryClass('xajax/xajax_core/xajax.inc');
+
+class w2PajaxResponse extends xajaxResponse {
+
+	public function addCreateOptions($sSelectId, $options) {
+		if (sizeof($options) > 0) {
+			foreach ($options as $key => $option) {
+				$this->script("addOption('" . $sSelectId . "','" . $key . "','" . $option . "');");
+			}
+		}
+	}
+
+}
