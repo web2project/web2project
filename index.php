27bb3db8efb3c6ebf4944cd49e58421b14c821e8
pedroix <pedroix@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2011-02-14 03:36:50 +0000

    Modified the looks of the ajax loading message. Added a new loader gif.
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1634 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- index.php
+++ index.php
@@ -443,15 +443,15 @@ if (!$suppressHeaders) {
 	}
 	echo '
 		<!--AJAX loading messagebox -->
-		<div id="loadingMessage" style="alpha(opacity=100);opacity:1;position: fixed; left: 50%; top: 0;display: none;">
-		<table width="80" cellpadding="3" cellspacing="3" border="0">
+		<div id="loadingMessage" style="alpha(opacity=100);opacity:1;position: fixed; left: 0px; top: 1px;display: none;">
+		<table width="80" cellpadding="1" cellspacing="1" border="0">
 		<tr>
+			<td>';
+	echo w2PshowImage('ajax-loader.gif', '', '', 'web2Project', 'Server Connection Running, Please wait...');
+	echo '
+			</td>
 			<td>
 				<b>' . $AppUI->_('Loading') . '</b>
-			</td>
-			<td>';
-	echo w2PshowImage('progress.gif', '10', '10', 'spinner', 'Loading...');
-	echo '
 			</td>
 		</tr>
 		</table>
