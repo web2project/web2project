6995bfe92fbe799fadece3954b4637296c9d5af5
caseysoftware <keith@blueparabola.com>
2011-02-15 22:10:16 -0600

    Author: pedroix Added the Italian locale http://forums.web2project.net/viewtopic.php?t=3277

---
+++ locales/it/locales.php
@@ -0,0 +1,5 @@
+<?php /* $Id: locales.php 939 2010-01-23 06:11:13Z caseydk $ $URL: https://web2project.svn.sourceforge.net/svnroot/web2project/tags/version1.3/locales/it/locales.php $ */
+//$locale_char_set = 'iso-8859-1';
+$locale_char_set = 'utf-8';
+// 0 = sunday, 1 = monday
+define('LOCALE_FIRST_DAY', 1);
