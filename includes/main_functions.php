49c2408236a02650d1a70f77ba943bf5df7abdba
D. Keith Casey Jr <keith@blueparabola.com>
2011-02-20 23:16:20 -0600

    updated the config getter to set default values if they're missing

--- includes/main_functions.php
+++ includes/main_functions.php
@@ -292,16 +292,17 @@ function w2PcontextHelp($title, $link = '') {
  */
 function w2PgetConfig($key, $default = null) {
 	global $w2Pconfig, $AppUI;
+
 	if (isset($w2Pconfig[$key])) {
-            return $w2Pconfig[$key];
+		return $w2Pconfig[$key];
 	} else {
-//            if (!is_null($default)) {
-//                $obj = new w2p_Core_Config();
-//                $obj->config_name = $key;
-//                $obj->config_value = $default;
-//                $obj->store($AppUI);
-//            }
-            return $default;
+		if (!is_null($default)) {
+			$obj = new w2p_Core_Config();
+			$obj->config_name = $key;
+			$obj->config_value = $default;
+			$obj->store($AppUI);
+		}
+		return $default;
 	}
 }
 
