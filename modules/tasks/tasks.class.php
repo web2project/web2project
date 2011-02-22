25defbab89e39acd1945aad2d691e33bad019275
D. Keith Casey Jr <keith@blueparabola.com>
2011-02-22 00:17:56 -0600

    updated the database structure to make sure the right task data is stored;

--- modules/tasks/tasks.class.php
+++ modules/tasks/tasks.class.php
@@ -831,6 +831,7 @@ class CTask extends w2p_Core_BaseObject {
 		$q = new w2p_Database_Query;
 		$q->addQuery('task_id, MAX(task_end_date) as last_date');
 		$q->addTable('tasks');
+		$q->addWhere('task_dynamic <> 1');
 		$q->addWhere('task_project = ' . (int)$project_id);
 		$q->addGroup('task_project');
 
