d01ff02259f756a4bb71474760455becc91afa0f
pedroix <pedroix@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2011-02-22 17:18:43 +0000

    Fix user_email issue preventing projects from notifying project owners, and any other code below project owner notification on do_project_aed http://forums.web2project.net/viewtopic.php?t=3336
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1690 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- modules/admin/admin.class.php
+++ modules/admin/admin.class.php
@@ -288,6 +288,7 @@ class CUser extends w2p_Core_BaseObject {
 		$q = new w2p_Database_Query();
 		$q->addTable('users', 'u');
 		$q->addQuery('u.*');
+		$q->addQuery('con.contact_email AS user_email');
 		$q->addQuery('uf.feed_token');
 		$q->addQuery('con.*, company_id, company_name, dept_name, dept_id');
 		$q->addJoin('contacts', 'con', 'user_contact = contact_id', 'inner');
