ddc2fdb71dd1a5db4d84d91aa80d085bc07dc9ec
Robert Basic <robertbasic.com@gmail.com>
2011-02-28 21:12:49 +0100

    cleaning up the code a bit. found some html errors, fixed those too

--- modules/forums/view_pdf.php
+++ modules/forums/view_pdf.php
@@ -1,6 +1,6 @@
 <?php /* $Id: view_pdf.php 1517 2010-12-05 08:07:54Z caseydk $ $URL: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk/modules/forums/view_pdf.php $ */
 if (!defined('W2P_BASE_DIR')) {
-	die('You should not call this file directly.');
+    die('You should not call this file directly.');
 }
 $AppUI->savePlace();
 
@@ -18,7 +18,7 @@ $q->addWhere('f.forum_id = ' . (int) $forum_id);
 $forum = $q->loadHash();
 
 if (!$perms->checkModuleItem('forums', 'view', $message_id)) {
-	$AppUI->redirect('m=public&a=access_denied');
+    $AppUI->redirect('m=public&a=access_denied');
 }
 
 $q = new w2p_Database_Query;
@@ -30,7 +30,7 @@ $q->addJoin('users', 'u', 'message_author = u.user_id', 'inner');
 $q->addJoin('contacts', 'con', 'contact_id = user_contact', 'inner');
 $q->addWhere('forum_id = message_forum AND (message_id = ' . (int)$message_id . ' OR message_parent = ' . (int)$message_id . ')');
 if (w2PgetConfig('forum_descendent_order') || w2PgetParam($_REQUEST, 'sort', 0)) {
-	$q->addOrder('message_date ' . $sort);
+    $q->addOrder('message_date ' . $sort);
 }
 
 $messages = $q->loadList();
@@ -44,21 +44,22 @@ $pdfhead = array('Date', 'User', 'Message');
 $new_messages = array();
 
 foreach ($messages as $row) {
-	// Find the parent message - the topic.
-	if ($row['message_id'] == $message_id)
-		$topic = $row['message_title'];
+    // Find the parent message - the topic.
+    if ($row['message_id'] == $message_id) {
+        $topic = $row['message_title'];
+    }
 
-	$q = new w2p_Database_Query;
-	$q->addTable('forum_messages');
-	$q->addTable('users');
-	$q->addQuery('contact_first_name, contact_last_name, contact_email, user_username');
-	$q->addJoin('contacts', 'con', 'contact_id = user_contact', 'inner');
-	$q->addWhere('users.user_id = ' . (int)$row['message_editor']);
-	$editor = $q->loadList();
+    $q = new w2p_Database_Query;
+    $q->addTable('forum_messages');
+    $q->addTable('users');
+    $q->addQuery('contact_first_name, contact_last_name, contact_email, user_username');
+    $q->addJoin('contacts', 'con', 'contact_id = user_contact', 'inner');
+    $q->addWhere('users.user_id = ' . (int) $row['message_editor']);
+    $editor = $q->loadList();
 
-	$date = intval($row['message_date']) ? new w2p_Utilities_Date($row['message_date']) : null;
+    $date = intval($row['message_date']) ? new w2p_Utilities_Date($row['message_date']) : null;
 
-	$pdfdata[] = array($row['message_date'], $row['contact_first_name'] . ' ' . $row['contact_last_name'], '<b>' . $row['message_title'] . '</b><br>' . $row['message_body']);
+    $pdfdata[] = array($row['message_date'], $row['contact_first_name'] . ' ' . $row['contact_last_name'], '<b>' . $row['message_title'] . '</b><br>' . $row['message_body']);
 }
 
 $pdf = new w2p_Output_PDF_Reports('P', 'mm', 'A4', true, 'UTF-8');
--- modules/reports/reports/upcoming.php
+++ modules/reports/reports/upcoming.php
@@ -1,22 +1,22 @@
 <?php /* $Id$ $URL$ */
 if (!defined('W2P_BASE_DIR')) {
-	die('You should not access this file directly.');
+    die('You should not access this file directly.');
 }
 
 // Output the PDF
 // make the PDF file
 if ($project_id != 0) {
-	$q = new w2p_Database_Query;
-	$q->addTable('projects');
-	$q->addQuery('project_name');
-	$q->addWhere('project_id=' . (int)$project_id);
-	$pname = 'Project: ' . $q->loadResult();
+    $q = new w2p_Database_Query;
+    $q->addTable('projects');
+    $q->addQuery('project_name');
+    $q->addWhere('project_id=' . (int) $project_id);
+    $pname = 'Project: ' . $q->loadResult();
 } else {
-	$pname = $AppUI->_('All Projects');
+    $pname = $AppUI->_('All Projects');
 }
 if ($err = db_error()) {
-	$AppUI->setMsg($err, UI_MSG_ERROR);
-	$AppUI->redirect();
+    $AppUI->setMsg($err, UI_MSG_ERROR);
+    $AppUI->redirect();
 }
 
 $pdf = new w2p_Output_PDF_Reports('L', 'mm', 'A4', true, 'UTF-8');
@@ -47,7 +47,7 @@ $pdf->Cell(0, 0, $AppUI->_('Tasks Due to be Completed By') . ' ' . $next_week->f
 $hasResources = $AppUI->isActiveModule('resources');
 $perms = &$AppUI->acl();
 if ($hasResources) {
-	$hasResources = canView('resources');
+    $hasResources = canView('resources');
 }
 // Build the data to go into the table.
 $pdfdata = array();
@@ -56,7 +56,7 @@ $columns[] = '<b>' . $AppUI->_('Task Name') . '</b>';
 $columns[] = '<b>' . $AppUI->_('Owner') . '</b>';
 $columns[] = '<b>' . $AppUI->_('Assigned Users') . '</b>';
 if ($hasResources) {
-	$columns[] = '<b>' . $AppUI->_('Assigned Resources') . '</b>';
+    $columns[] = '<b>' . $AppUI->_('Assigned Resources') . '</b>';
 }
 $columns[] = '<b>' . $AppUI->_('Finish Date') . '</b>';
 
@@ -72,10 +72,10 @@ $q->addJoin('contacts', 'ct', 'ct.contact_id = b.user_contact', 'inner');
 $q->addWhere('task_percent_complete < 100');
 $q->addWhere('pr.project_active = 1');
 if (($template_status = w2PgetConfig('template_projects_status_id')) != '') {
-	$q->addWhere('pr.project_status <> ' . (int)$template_status);
+    $q->addWhere('pr.project_status <> ' . (int) $template_status);
 }
 if ($project_id != 0) {
-	$q->addWhere('task_project = ' . (int)$project_id);
+    $q->addWhere('task_project = ' . (int) $project_id);
 }
 $q->addWhere('task_end_date BETWEEN \'' . $date->format(FMT_DATETIME_MYSQL) . '\' AND \'' . $next_week->format(FMT_DATETIME_MYSQL) . '\'');
 $proj = new CProject();
@@ -86,68 +86,69 @@ $obj->setAllowedSQL($AppUI->user_id, $q);
 $tasks = $q->loadHashList('task_id');
 
 if ($err = db_error()) {
-	$AppUI->setMsg($err, UI_MSG_ERROR);
-	$AppUI->redirect();
+    $AppUI->setMsg($err, UI_MSG_ERROR);
+    $AppUI->redirect();
 }
 // Now grab the resources allocated to the tasks.
 $task_list = array_keys($tasks);
 $assigned_users = array();
 // Build the array
 foreach ($task_list as $tid) {
-	$assigned_users[$tid] = array();
+    $assigned_users[$tid] = array();
 }
 
 if (count($tasks)) {
-	$q->clear();
-	$q->addQuery('a.task_id, a.perc_assignment, b.*, c.*');
-	$q->addTable('user_tasks', 'a');
-	$q->addJoin('users', 'b', 'a.user_id = b.user_id', 'inner');
-	$q->addJoin('contacts', 'c', 'b.user_contact = c.contact_id', 'inner');
-	$q->addWhere('a.task_id IN (' . implode(',', $task_list) . ')');
-	$res = $q->exec();
-	if (!$res) {
-		$AppUI->setMsg(db_error(), UI_MSG_ERROR);
-		$q->clear();
-		$AppUI->redirect();
-	}
-	while ($row = db_fetch_assoc($res)) {
-		$assigned_users[$row['task_id']][$row['user_id']] = $row['contact_first_name'] . ' ' . $row['contact_last_name'] . ' [' . $row['perc_assignment'] . '%]';
-	}
-	$q->clear();
+    $q->clear();
+    $q->addQuery('a.task_id, a.perc_assignment, b.*, c.*');
+    $q->addTable('user_tasks', 'a');
+    $q->addJoin('users', 'b', 'a.user_id = b.user_id', 'inner');
+    $q->addJoin('contacts', 'c', 'b.user_contact = c.contact_id', 'inner');
+    $q->addWhere('a.task_id IN (' . implode(',', $task_list) . ')');
+    $res = $q->exec();
+    if (!$res) {
+        $AppUI->setMsg(db_error(), UI_MSG_ERROR);
+        $q->clear();
+        $AppUI->redirect();
+    }
+    while ($row = db_fetch_assoc($res)) {
+        $assigned_users[$row['task_id']][$row['user_id']] = $row['contact_first_name'] . ' ' . $row['contact_last_name'] . ' [' . $row['perc_assignment'] . '%]';
+    }
+    $q->clear();
 }
 
 $resources = array();
 if ($hasResources && count($tasks)) {
-	foreach ($task_list as $tid) {
-		$resources[$tid] = array();
-	}
-	$q->clear();
-	$q->addQuery('a.*, b.resource_name');
-	$q->addTable('resource_tasks', 'a');
-	$q->addJoin('resources', 'b', 'a.resource_id = b.resource_id', 'inner');
-	$q->addWhere('a.task_id IN (' . implode(',', $task_list) . ')');
-	$res = $q->exec();
-	if (!$res) {
-		$AppUI->setMsg(db_error(), UI_MSG_ERROR);
-		$q->clear();
-		$AppUI->redirect();
-	}
-	while ($row = db_fetch_assoc($res)) {
-		$resources[$row['task_id']][$row['resource_id']] = $row['resource_name'] . ' [' . $row['percent_allocated'] . '%]';
-	}
-	$q->clear();
+    foreach ($task_list as $tid) {
+        $resources[$tid] = array();
+    }
+    $q->clear();
+    $q->addQuery('a.*, b.resource_name');
+    $q->addTable('resource_tasks', 'a');
+    $q->addJoin('resources', 'b', 'a.resource_id = b.resource_id', 'inner');
+    $q->addWhere('a.task_id IN (' . implode(',', $task_list) . ')');
+    $res = $q->exec();
+    if (!$res) {
+        $AppUI->setMsg(db_error(), UI_MSG_ERROR);
+        $q->clear();
+        $AppUI->redirect();
+    }
+    while ($row = db_fetch_assoc($res)) {
+        $resources[$row['task_id']][$row['resource_id']] = $row['resource_name'] . ' [' . $row['percent_allocated'] . '%]';
+    }
+    $q->clear();
 }
 
 // Build the data columns
 foreach ($tasks as $task_id => $detail) {
-	$row = &$pdfdata[];
-	$row[] = $detail['task_name'];
-	$row[] = $detail['user_username'];
-	$row[] = implode("<br>", $assigned_users[$task_id]);
-	if ($hasResources)
-		$row[] = implode("<br>", $resources[$task_id]);
-	$end_date = new w2p_Utilities_Date($detail['task_end_date']);
-	$row[] = $end_date->format($df);
+    $row = &$pdfdata[];
+    $row[] = $detail['task_name'];
+    $row[] = $detail['user_username'];
+    $row[] = implode("<br>", $assigned_users[$task_id]);
+    if ($hasResources) {
+        $row[] = implode("<br>", $resources[$task_id]);
+    }
+    $end_date = new w2p_Utilities_Date($detail['task_end_date']);
+    $row[] = $end_date->format($df);
 }
 
 $pdf->addHtmlTable($pdfdata, $columns);
