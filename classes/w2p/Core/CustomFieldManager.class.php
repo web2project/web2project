4f094aaa3e381c2b0aa70f46f8d63a98c486b97f
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2011-04-30 21:39:37 +0000

    created the core landing page of the custom field editor
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1836 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

--- classes/w2p/Core/CustomFieldManager.class.php
+++ classes/w2p/Core/CustomFieldManager.class.php
@@ -2,6 +2,22 @@
 
 class w2p_Core_CustomFieldManager {
 
+    protected $html_types = array();
+
+    public function __construct(w2p_Core_CAppUI $AppUI) {
+        $this->html_types = array('textinput' => $AppUI->_('Text Input'),
+                    'textarea' => $AppUI->_('Text Area'),
+                    'checkbox' => $AppUI->_('Checkbox'),
+                    'select' => $AppUI->_('Select List'),
+                    'label' => $AppUI->_('Label'),
+                    'separator' => $AppUI->_('Separator'),
+                    'href' => $AppUI->_('Weblink'));
+    }
+
+    public function getType($name) {
+        return $this->html_types[$name];
+    }
+
     public function getModuleList() {
         $q = new w2p_Database_Query;
         $q->addTable('modules');
--- modules/system/customfields/index.php
+++ modules/system/customfields/index.php
@@ -4,23 +4,23 @@ if (!defined('W2P_BASE_DIR')) {
 }
 
 // check permissions
-$perms = &$AppUI->acl();
+$perms = $AppUI->acl();
 if (!canEdit('system')) {
 	$AppUI->redirect('m=public&a=access_denied');
 }
 
-$titleBlock = new CTitleBlock('Custom field editor', 'customfields.png', 'admin', 'admin.custom_field_editor');
+$titleBlock = new CTitleBlock('Custom field editor', 'customfields.png', $m, "$m.$a");
 $titleBlock->addCrumb('?m=system', 'system admin');
 $titleBlock->show();
 
-$manager = new w2p_Core_CustomFieldManager();
+$manager = new w2p_Core_CustomFieldManager($AppUI);
 $modules = $manager->getModuleList();
 
 ?>
 <table width="100%" border="0" cellpadding="2" cellspacing="1" class="tbl">
     <?php foreach ($modules as $module) { ?>
     <tr>
-        <td>
+        <td colspan="10">
             <h2>
                 <?php echo w2PtoolTip($AppUI->_($module['mod_name']), $AppUI->_('Click this icon to Add a new Custom Field to this Module.'), true); ?>
                     <a href="?m=system&a=custom_field_addedit&module=<?php echo $module['mod_id']; ?>">
@@ -30,5 +30,36 @@ $modules = $manager->getModuleList();
             </h2>
         </td>
     </tr>
-    <?php }?>
+    <?php
+    $custom_fields = $manager->getStructure($module['mod_name']);
+
+    $s = '';
+	if (count($custom_fields)) {
+		$s .= '<th width="10"></th>';
+        $s .= '<th>' . $AppUI->_('Name') . '</th>';
+		$s .= '<th>' . $AppUI->_('Description') . '</th>';
+		$s .= '<th>' . $AppUI->_('Type') . '</th>';
+		$s .= '<th>' . $AppUI->_('Published') . '</th>';
+		$s .= '<th>' . $AppUI->_('Order') . '</th>';
+        $s .= '<th width="5"></th>';
+        foreach ($custom_fields as $field) {
+            $s .= '<tr><td class="hilite">';
+            $s .= w2PtoolTip('', $AppUI->_('Click this icon to Edit this Custom Field.'), true);
+            $s .= '<a href="?m=system&u=customfields&a=addedit&module=' . $module['mod_id'] . '&field_id=' . $field['field_id'] . '"><img src="' . w2PfindImage('icons/stock_edit-16.png') . '" align="center" width="16" height="16" border="0" alt=""></a>';
+            $s .= w2PendTip();
+            $s .= '<td>'.$field['field_name'].'</td>';
+            $s .= '<td>'.$field['field_description'].'</td>';
+            $s .= '<td>'.$manager->getType($field['field_htmltype']).'</td>';
+            $s .= '<td>'.($field['field_published'] ? $AppUI->_('Yes') : $AppUI->_('No')).'</td>';
+            $s .= '<td>'.$field['field_order'].'</td>';
+            $s .= '<td>';
+            $s .= w2PtoolTip('', $AppUI->_('Click this icon to Delete this Custom Field.'), true);
+            $s .= '<a href="?m=system&u=customfields&a=addedit&field_id=' . $field['field_id'] . '&delete=1"><img src="' . w2PfindImage('icons/stock_delete-16.png') . '" align="center" width="16" height="16" border="0" alt=""></a> ';
+            $s .= w2PendTip();
+            $s .= '</td></tr>';
+        }
+        echo $s;
+	}
+
+    }?>
 </table>
