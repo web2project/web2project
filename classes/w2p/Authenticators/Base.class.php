c32d2b1bb2d6a4fae81257af269ab7d035326bcb
caseydk <caseydk@cef3e9b6-683d-0410-b475-c2bd3a4d0f1e>
2011-05-10 05:59:31 +0000

    collecting the Authenticator functionality to a single place;
    git-svn-id: https://web2project.svn.sourceforge.net/svnroot/web2project/trunk@1910 cef3e9b6-683d-0410-b475-c2bd3a4d0f1e

---
+++ classes/w2p/Authenticators/Base.class.php
@@ -0,0 +1,19 @@
+<?php /* $Id$ $URL$ */
+
+/**
+ *	@package web2project
+ *	@subpackage core
+ *	@version $Revision$
+ */
+
+/**
+ *	w2p_Authenticator_Base Abstract Class.
+ *
+ *	Parent class for all authenticators
+ *	@author Keith Casey <caseydk@users.sourceforge.net>
+ *	@abstract
+ */
+abstract class w2p_Authenticators_Base
+{
+    
+}
---
+++ classes/w2p/Authenticators/Factory.class.php
@@ -0,0 +1,19 @@
+<?php /* $Id$ $URL$ */
+
+/**
+ *	@package web2project
+ *	@subpackage core
+ *	@version $Revision$
+ */
+
+/**
+ *	w2p_Authenticator_Factory Class.
+ *
+ *	Parent class to all database table derived objects
+ *	@author Andrew Eddie <eddieajau@users.sourceforge.net>
+ *	@abstract
+ */
+class w2p_Authenticators_Factory
+{
+    // TODO: need a factory to figure out which auth method to use
+}
