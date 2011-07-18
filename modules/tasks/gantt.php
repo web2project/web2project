b211b17c0d2a4ae40b3c4a9da50461bd44c85065
D Keith Casey Jr <keith@blueparabola.com>
2011-07-17 23:29:54 -0500

    cleaned up some of the formatting from santos;

--- modules/tasks/gantt.php
+++ modules/tasks/gantt.php
@@ -139,7 +139,6 @@ $task = new CTask();
 $task->setAllowedSQL($AppUI->user_id, $q);
 $proTasks = $q->loadHashList('task_id');
 
-
 $orrarr[] = array('task_id'=>0, 'order_up'=>0, 'order'=>'');
 $end_max = '0000-00-00 00:00:00';
 $start_min = date('Y-m-d H:i:s');
@@ -264,28 +263,26 @@ $gantt->setDateRange($start_date, $end_date);
 $gantt_arr = array();
 reset($projects);
 $displayed = array();
-foreach ($projects as $p) {	
+foreach ($projects as $p) {
     $tnums = count($p['tasks']);
 
 	for ($i = 0; $i < $tnums; $i++) {
-		//	web2project_gii
-		$t = $p['tasks'][$i]; 
-		if ($caller == 'todo') {
-			if ($showDynTasks) {  		    
-				if ($t['task_parent'] == $t['task_id']) {
-					showgtask($t);										
-					findgchild($p['tasks'], $t['task_id']);
-				}
-			} else {
-				showgtask($t);	
-			}
-		} else {
-			if ($t['task_parent'] == $t['task_id']) {
-					showgtask($t);										
-					findgchild($p['tasks'], $t['task_id']);
-			}
+		$t = $p['tasks'][$i];
+        if ($caller == 'todo') {
+            if ($showDynTasks) {
+                if ($t['task_parent'] == $t['task_id']) {
+                    showgtask($t);
+                    findgchild($p['tasks'], $t['task_id']);
+                }
+            } else {
+                showgtask($t);
+            }
+        } else {
+            if ($t['task_parent'] == $t['task_id']) {
+                showgtask($t);
+                findgchild($p['tasks'], $t['task_id']);
+            }
 		}
-		//	/web2project_gii
 	}
 }
 $gantt->loadTaskArray($gantt_arr);
