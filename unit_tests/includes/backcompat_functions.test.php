75c83b3d6d64088cdb77727858f8064e5558b8e3
D Keith Casey Jr <keith@caseysoftware.com>
2011-08-28 03:54:46 -0500

    building out our own date_diff function to handle pre PHP 5.3 installs..

---
+++ unit_tests/includes/backcompat_functions.test.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Necessary global variables 
+ */
+global $db;
+global $ADODB_FETCH_MODE;
+global $w2p_performance_dbtime;
+global $w2p_performance_old_dbqueries;
+global $AppUI;
+
+require_once '../base.php';
+require_once W2P_BASE_DIR . '/includes/config.php';
+require_once W2P_BASE_DIR . '/includes/main_functions.php';
+require_once W2P_BASE_DIR . '/includes/db_adodb.php';
+
+require_once 'PHPUnit/Framework.php';
+
+/**
+ * BackCompat_Functions_Test Class.
+ * 
+ * Class to test the backcompat_functions include
+ * @author D. Keith Casey, Jr. <caseydk@users.sourceforge.net>
+ * @package web2project
+ * @subpackage unit_tests
+ */
+class BackCompat_Functions_Test extends PHPUnit_Framework_TestCase {
+    
+    
+    public function test_date_diff() {
+
+        $today  = new DateTime('now');
+        $plus7_days = new DateTime('+7 days');
+        $minus1_day = new DateTime('-1 day');
+        $year2010 = new DateTime('2010-01-01');
+        $year2030  = new DateTime('2030-01-01');
+        $plus5_weeks = new DateTime('+5 weeks');
+
+        $this->assertEquals('0',    date_diff2($today, $today));
+        $this->assertEquals('7',    date_diff2($today, $plus7_days, '%d'));
+        $this->assertEquals('-8',   date_diff2($plus7_days, $minus1_day, '%d'));
+        $this->assertEquals('-28',  date_diff2($plus5_weeks, $plus7_days, '%w'));
+        $this->assertEquals('7400', date_diff2($year2010, $year2030, '%d')); // don't forget leap days
+        $this->assertEquals('20',   date_diff2($year2010, $year2030, '%y'));
+        $this->assertEquals('-20',  date_diff2($year2030, $year2010, '%y'));
+    }
+    
+}
