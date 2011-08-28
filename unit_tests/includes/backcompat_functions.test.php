c1f69cb27ae578aa8f11f3476dbfab18fcc85f8f
D Keith Casey Jr <keith@caseysoftware.com>
2011-08-28 14:10:25 -0500

    killed off the date_diff function because it's just too complex to fake in any reasonable amount of time or effort;

--- unit_tests/includes/backcompat_functions.test.php
+++ unit_tests/includes/backcompat_functions.test.php
@@ -25,47 +25,7 @@ require_once 'PHPUnit/Framework.php';
  */
 class BackCompat_Functions_Test extends PHPUnit_Framework_TestCase {
 
-    public function test_date_diff() {
-        $today       = new DateTime('now');
-        $plus7_days  = new DateTime('+7 days');
-        $minus1_day  = new DateTime('-1 day');
-        $year2010    = new DateTime('2010-01-01');
-        $year2030    = new DateTime('2030-01-01');
-        $plus5_weeks = new DateTime('+5 weeks');
-
-        $this->assertEquals(0,      date_diff2($today,      $today));
-        $this->assertEquals(86400,  date_diff2($minus1_day, $today,      'S'));
-        $this->assertEquals(-10080, date_diff2($plus7_days, $today,      'I'));
-        $this->assertEquals(168,    date_diff2($today,      $plus7_days, 'H'));
-        $this->assertEquals(7,      date_diff2($today,      $plus7_days, 'D'));
-        $this->assertEquals(1,      date_diff2($today,      $plus7_days, 'W'));
-        $this->assertEquals(-8,     date_diff2($plus7_days, $minus1_day, 'D'));
-        $this->assertEquals(7305,   date_diff2($year2010,   $year2030,   'D')); // don't forget leap days!
-        $this->assertEquals(20 ,    date_diff2($year2010,   $year2030,   'Y'));
-        $this->assertEquals(-20,    date_diff2($year2030,   $year2010,   'Y'));
-    }
-
-    public function test_DateInterval_constructor() {
-
-        $interval = new DateInterval2('P2Y4DT6H8M');
-        $this->assertEquals(
-                array('y' => 2, 'm' => 0, 'd' => 4, 'h' => 6, 'i' => 8, 
-                    's' => 0, 'invert' => 0, 'days' => 0), get_object_vars($interval));
-
-        $interval = new DateInterval2('P3WT6H8M');
-        $this->assertEquals(
-                array('y' => 0, 'm' => 0, 'd' => 21, 'h' => 6, 'i' => 8, 
-                    's' => 0, 'invert' => 0, 'days' => 0), get_object_vars($interval));
-
-        $interval = new DateInterval2('P1Y1D');
-        $this->assertEquals(
-                array('y' => 1, 'm' => 0, 'd' => 1, 'h' => 0, 'i' => 0, 
-                    's' => 0, 'invert' => 0, 'days' => 0), get_object_vars($interval));
-
-        $interval = new DateInterval2('P1Y1M37DT500M');
-        $this->assertEquals(
-                array('y' => 1, 'm' => 1, 'd' => 37, 'h' => 0, 'i' => 500, 
-                    's' => 0, 'invert' => 0, 'days' => 0), get_object_vars($interval));
-        //TODO: test days & invert
+    public function placeholder() {
+        $this->assertEquals(0, 0);
     }
 }
