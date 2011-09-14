01c3694c552f3c979e6eb64c088f9f70bfd3c9ab
D Keith Casey Jr <keith@blueparabola.com>
2011-09-14 10:38:04 -0500

    more class cleanup

---
+++ classes/w2p/Core/Preferences.class.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *	@package web2project
+ *	@subpackage core
+ *	@version $Revision$
+ */
+
+/**
+ *	w2p_Core_Preferences Class.
+
+ */
+
+class w2p_Core_Preferences {
+	public $pref_user = null;
+	public $pref_name = null;
+	public $pref_value = null;
+
+	public function __construct() {
+		// empty constructor
+	}
+
+	public function bind($hash) {
+		if (!is_array($hash)) {
+			return 'CPreferences::bind failed';
+		} else {
+			$q = new w2p_Database_Query;
+			$q->bindHashToObject($hash, $this);
+			$q->clear();
+			return null;
+		}
+	}
+
+	public function check() {
+		// TODO MORE
+		return null; // object is ok
+	}
+
+	public function store() {
+		$msg = $this->check();
+		if ($msg) {
+			return 'CPreference::store-check failed ' . $msg;
+		}
+		if (($msg = $this->delete())) {
+			return 'CPreference::store-delete failed ' . $msg;
+		}
+		$q = new w2p_Database_Query;
+		if (!($ret = $q->insertObject('user_preferences', $this))) {
+			$q->clear();
+			return 'CPreference::store failed ' . db_error();
+		} else {
+			$q->clear();
+			return null;
+		}
+	}
+
+	public function delete() {
+		$q = new w2p_Database_Query;
+		$q->setDelete('user_preferences');
+		$q->addWhere('pref_user = ' . (int)$this->pref_user);
+		$q->addWhere('pref_name = \'' . $this->pref_name . '\'');
+		if (!$q->exec()) {
+			$q->clear();
+			return db_error();
+		} else {
+			$q->clear();
+			return null;
+		}
+	}
+}
