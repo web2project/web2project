19c21997abf9ad5e299662f38fa98452e7eb2362
D Keith Casey Jr <keith@blueparabola.com>
2011-11-26 23:35:12 -0600

    tweaked the module entry for smartsearch to eliminate some deprecation notices;

--- install/sql/mysql/036_pre_v3.0_fixes.sql
+++ install/sql/mysql/036_pre_v3.0_fixes.sql
@@ -9,3 +9,7 @@ ALTER TABLE  `custom_fields_values` CHANGE  `value_charvalue`  `value_charvalue`
 ALTER TABLE  `custom_fields_values` DROP INDEX  `value_charvalue`;
 
 ALTER TABLE  `custom_fields_values` ADD INDEX (  `value_charvalue` );
+
+-- This is just a tweak to get rid of the deprecation notice in the Hooks subststem
+
+UPDATE modules SET mod_main_class = 'CSmartSearch' WHERE mod_directory = 'smartsearch';
