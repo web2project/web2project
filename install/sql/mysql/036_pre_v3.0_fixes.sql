77ca8c45f9ea99bcf6206e3a095db78e4ee4f42d
D Keith Casey Jr <keith@caseysoftware.com>
2011-12-03 00:19:53 -0600

    cleaning up the database modifications to not have all the duplicate garbage;

--- install/sql/mysql/036_pre_v3.0_fixes.sql
+++ install/sql/mysql/036_pre_v3.0_fixes.sql
@@ -13,3 +13,63 @@ ALTER TABLE  `custom_fields_values` ADD INDEX (  `value_charvalue` );
 -- This is just a tweak to get rid of the deprecation notice in the Hooks subststem
 
 UPDATE modules SET mod_main_class = 'CSmartSearch' WHERE mod_directory = 'smartsearch';
+
+
+--  This begins the database changes for the new budgeting system.
+
+ALTER TABLE `billingcode` ADD `billingcode_company` INT( 10 ) NOT NULL DEFAULT '0';
+ALTER TABLE `billingcode` CHANGE  `company_id`  `company_id` BIGINT( 20 ) NOT NULL DEFAULT  '0' COMMENT  'deprecated';
+UPDATE `billingcode` SET `billingcode_company` = `company_id`;
+
+ALTER TABLE `billingcode` ADD `billingcode_category` VARCHAR( 50 ) NOT NULL DEFAULT '';
+
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Not Specified', '0');
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Labor', '1');
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Travel', '2');
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Licensing', '3');
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Software', '4');
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Administrative', '5');
+
+CREATE TABLE IF NOT EXISTS `budgets` (
+  `budget_id` int(10) NOT NULL AUTO_INCREMENT,
+  `budget_company` int(10) NOT NULL DEFAULT '0',
+  `budget_dept` int(10) NOT NULL DEFAULT '0',
+  `budget_start_date` date DEFAULT NULL,
+  `budget_end_date` date DEFAULT NULL,
+  `budget_amount` decimal(10,2) NOT NULL,
+  `budget_category` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`budget_id`),
+  KEY `budget_start_date` (`budget_start_date`),
+  KEY `budget_end_date` (`budget_end_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `budgets_assigned` (
+  `budget_id` int(10) NOT NULL AUTO_INCREMENT,
+  `budget_project` int(10) NOT NULL,
+  `budget_task` int(10) NOT NULL,
+  `budget_category` varchar(50) NOT NULL DEFAULT '',
+  `budget_amount` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`budget_id`),
+  KEY `budget_project` (`budget_project`),
+  KEY `budget_task` (`budget_task`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Not Specified', '0');
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Labor', '3');
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Travel', '2');
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Licensing', '1');
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Software', '1');
+INSERT INTO `sysvals` (`sysval_key_id`, `sysval_title`, `sysval_value`, `sysval_value_id`) 
+    VALUES(1, 'BudgetCategory', 'Administrative', '1');
