0645084acf3a585853b05c176991bb63bf319f8e
D Keith Casey Jr <keith@blueparabola.com>
2011-12-18 01:33:12 -0600

    deprecated the last of the db-specific functionality.. it's no longer needed! department tests run in 2x of the original time but we have more tests;

--- unit_tests/modules/departments.test.php
+++ unit_tests/modules/departments.test.php
@@ -58,45 +58,25 @@ require_once W2P_BASE_DIR . '/includes/session.php';
  * @copyright   2007-2010 The web2Project Development Team <w2p-developers@web2project.net>
  * @link        http://www.web2project.net
  */
-class Departments_Test extends PHPUnit_Extensions_Database_TestCase
+class Departments_Test extends PHPUnit_Framework_TestCase
 {
     protected $backupGlobals = FALSE;
     protected $obj = null;
     protected $post_data = array();
-
-    /**
-     * Return database connection for tests
-     */
-    protected function getConnection()
-    {
-        $pdo = new PDO(w2PgetConfig('dbtype') . ':host=' .
-                       w2PgetConfig('dbhost') . ';dbname=' .
-                       w2PgetConfig('dbname'),
-                       w2PgetConfig('dbuser'), w2PgetConfig('dbpass'));
-        return $this->createDefaultDBConnection($pdo, w2PgetConfig('dbname'));
-    }
-
-    /**
-     * Set up default dataset for testing
-     */
-    protected function getDataSet()
-    {
-      return $this->createXMLDataSet($this->getDataSetPath().'departmentsSeed.xml');
-    }
-    protected function getDataSetPath()
-    {
-      return dirname(dirname(__FILE__)).'/db_files/departments/';
-    }
+    protected $mockDB = null;
 
     protected function setUp()
     {
       parent::setUp();
 
       $this->obj = new CDepartment();
+      $this->mockDB = new w2p_Database_Mock();
+      $this->obj->overrideDatabase($this->mockDB);
+
       $this->post_data = array(
           'dosql'             => 'do_dept_aed',
           'dept_id'           => 0,
-          'dept_parent'       => 1,
+          'dept_parent'       => 0,
           'dept_company'      => 1,
           'dept_name'         => 'My Department',
           'dept_phone'        => '815-555-1212',
@@ -115,10 +95,6 @@ class Departments_Test extends PHPUnit_Extensions_Database_TestCase
       );
     }
 
-    protected function tearDown()
-    {
-      $this->getDataSet();
-    }
     /**
      * Tests the Attributes of a new Departments object.
      */
@@ -243,97 +219,91 @@ class Departments_Test extends PHPUnit_Extensions_Database_TestCase
         $this->AssertEquals(0, $this->obj->dept_id);
     }
 
-//    /**
-//     * Tests the proper creation of a dept
-//     */
-//    public function testCreateDepartment()
-//    {
-//      global $AppUI;
-//
-//      $this->obj->bind($this->post_data);
-//      $result = $this->obj->store($AppUI);
-//
-//      $this->assertTrue($result);
-//      $this->assertEquals('web2project homepage',   $this->obj->dept_name);
-//      $this->assertEquals(0,                        $this->obj->dept_project);
-//      $this->assertEquals(0,                        $this->obj->dept_task);
-//      $this->assertEquals('http://web2project.net', $this->obj->dept_url);
-//      $this->assertEquals(0,                        $this->obj->dept_parent);
-//      $this->assertEquals('This is web2project',    $this->obj->dept_description);
-//      $this->assertEquals(1,                        $this->obj->dept_owner);
-//      $this->assertEquals('',                       $this->obj->dept_icon);
-//      $this->assertEquals(0,                        $this->obj->dept_category);
-//      $this->assertNotEquals(0,                     $this->obj->dept_id);
-//    }
-//
-//    /**
-//     * Tests loading the Department Object
-//     */
-//    public function testLoad()
-//    {
-//      global $AppUI;
-//
-//      $this->obj->bind($this->post_data);
-//      $result = $this->obj->store($AppUI);
-//      $this->assertTrue($result);
-//
-//      $dept = new CDepartment();
-//      $dept->load($this->obj->dept_id);
-//
-//      $this->assertEquals($this->obj->dept_name,              $dept->dept_name);
-//      $this->assertEquals($this->obj->dept_project,           $dept->dept_project);
-//      $this->assertEquals($this->obj->dept_task,              $dept->dept_task);
-//      $this->assertEquals($this->obj->dept_url,               $dept->dept_url);
-//      $this->assertEquals($this->obj->dept_parent,            $dept->dept_parent);
-//      $this->assertEquals($this->obj->dept_description,       $dept->dept_description);
-//      $this->assertEquals($this->obj->dept_owner,             $dept->dept_owner);
-//      $this->assertEquals('obj/',                             $dept->dept_icon);
-//      $this->assertEquals($this->obj->dept_category,          $dept->dept_category);
-//      $this->assertEquals($this->obj->dept_id,                $dept->dept_id);
-//    }
-//
-//    /**
-//     * Tests the update of a dept
-//     */
-//    public function testUpdateDepartment()
-//    {
-//      global $AppUI;
-//
-//      $this->obj->bind($this->post_data);
-//      $result = $this->obj->store($AppUI);
-//      $this->assertTrue($result);
-//      $original_id = $this->obj->dept_id;
-//
-//      $this->obj->dept_name = 'web2project Forums';
-//      $this->obj->dept_url = 'http://forums.web2project.net';
-//      $result = $this->obj->store($AppUI);
-//      $this->assertTrue($result);
-//      $new_id = $this->obj->dept_id;
-//
-//      $this->assertEquals($original_id,                    $new_id);
-//      $this->assertEquals('web2project Forums',            $this->obj->dept_name);
-//      $this->assertEquals('http://forums.web2project.net', $this->obj->dept_url);
-//      $this->assertEquals('This is web2project',           $this->obj->dept_description);
-//    }
-//
-//    /**
-//     * Tests the delete of a dept
-//     */
-//    public function testDeleteDepartment()
-//    {
-//      global $AppUI;
-//
-//      $this->obj->bind($this->post_data);
-//      $result = $this->obj->store($AppUI);
-//      $this->assertTrue($result);
-//      $original_id = $this->obj->dept_id;
-//
-//      $result = $this->obj->delete($AppUI);
-//      $this->assertTrue($result);
-//
-//      $dept = new CDepartment();
-//      $dept->load($original_id);
-//      $this->assertEquals('',              $dept->dept_name);
-//      $this->assertEquals('',              $dept->dept_url);
-//    }
+    /**
+     * Tests loading the Department Object
+     */
+    public function testLoad()
+    {
+        global $AppUI;
+
+        $this->obj->bind($this->post_data);
+        $result = $this->obj->store($AppUI);
+        $this->assertTrue($result);
+
+        $item = new CDepartment();
+        $item->overrideDatabase($this->mockDB);
+        $this->post_data['dept_id'] = $this->obj->dept_id;
+        $this->mockDB->stageHash($this->post_data);
+        $item->load($this->obj->dept_id);
+
+        $this->assertEquals($this->obj->dept_name,      $item->dept_name);
+        $this->assertEquals($this->obj->dept_company,   $item->dept_company);
+        $this->assertEquals($this->obj->dept_parent,    $item->dept_parent);
+        $this->assertEquals($this->obj->dept_owner,     $item->dept_owner);
+        $this->assertEquals($this->obj->dept_type,      $item->dept_type);
+        $this->assertEquals($this->obj->dept_id,        $item->dept_id);
+    }
+
+    /**
+     * Tests the proper creation of a dept
+     */
+    public function testCreateDepartment()
+    {
+      global $AppUI;
+
+      $this->obj->bind($this->post_data);
+      $result = $this->obj->store($AppUI);
+
+      $this->assertTrue($result);
+      $this->assertEquals('My Department',  $this->obj->dept_name);
+      $this->assertEquals(1,                $this->obj->dept_company);
+      $this->assertEquals(0,                $this->obj->dept_parent);
+      $this->assertEquals(1,                $this->obj->dept_owner);
+      $this->assertEquals(1,                $this->obj->dept_type);
+      $this->assertNotEquals(0,             $this->obj->dept_id);
+    }
+
+    /**
+     * Tests the update of a dept
+     */
+    public function testUpdateDepartment()
+    {
+        global $AppUI;
+
+        $this->obj->bind($this->post_data);
+        $result = $this->obj->store($AppUI);
+        $this->assertTrue($result);
+        $original_id = $this->obj->dept_id;
+
+        $this->obj->dept_name = 'Change the department name';
+        $result = $this->obj->store($AppUI);
+
+        $this->assertTrue($result);
+        $new_id = $this->obj->dept_id;
+
+        $this->assertEquals($original_id,                    $new_id);
+        $this->assertEquals('Change the department name',    $this->obj->dept_name);
+    }
+
+    /**
+     * Tests the delete of a dept
+     */
+    public function testDelete()
+    {
+        global $AppUI;
+
+        $this->obj->bind($this->post_data);
+        $result = $this->obj->store($AppUI);
+        $this->assertTrue($result);
+        $original_id = $this->obj->dept_id;
+        $result = $this->obj->delete($AppUI);
+
+        $item = new CDepartment();
+        $item->overrideDatabase($this->mockDB);
+        $this->mockDB->stageHash(array('dept_name' => '', 'dept_owner' => ''));
+        $item->load($original_id);
+
+        $this->assertEquals('',              $item->dept_name);
+        $this->assertEquals('',              $item->dept_owner);
+    }
 }
