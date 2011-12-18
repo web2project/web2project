a1e7594cc9ee33c35a144218887e285534487b1f
D Keith Casey Jr <keith@blueparabola.com>
2011-12-17 22:51:01 -0600

    wired up the simple loadResult method for single-column data access; wiring up the tests to support said Mock Database object

--- classes/w2p/Database/Mock.class.php
+++ classes/w2p/Database/Mock.class.php
@@ -14,20 +14,32 @@
 
 class w2p_Database_Mock extends w2p_Database_Query {
 
-    protected $data = array();
+    protected $hash = array();
+    protected $result = '';
 
     public function __construct($prefix = null, $query_db = null) {
         parent::__construct($prefix, $query_db);
     }
 
-    public function stageData($stageData) {
-        $this->data[] = $stageData;
+    public function stageHash(array $array) {
+        $this->hash[] = $array;
     }
-
     public function loadHash() {
-        return array_shift($this->data);
+        return array_shift($this->hash);
     }
 
+    public function stageResult($value) {
+        $this->result = $value;
+    }
+    public function loadResult() {
+        return $this->result;
+    }
+
+    public function loadObject(&$object, $bindAll = false, $strip = true) {
+        $hash = $this->loadHash();
+
+        $this->bindHashToObject($hash, $object, null, $strip, $bindAll);
+    }
 
     public function exec() {
         return true;
---
+++ unit_tests/classes/database/mock.test.php
@@ -0,0 +1,69 @@
+<?php /* $Id$ $URL$ */
+
+/**
+ *	@package    web2project
+ *	@subpackage unit_tests
+ *	@version    $Revision$
+ *  @license	Clear BSD
+ *  @author     Keith casey
+ */
+
+class w2p_Database_Mock_Test extends PHPUnit_Framework_TestCase {
+
+    /**
+     * An AppUI object for validation
+     *
+     * @param CAppUI
+     * @access private
+     */
+    private $mockDB;
+
+    /**
+     * Create an AppUI before running tests
+     */
+    protected function setUp()
+    {
+        $this->mockDB = new w2p_Database_Mock();
+    }
+
+    public function testLoadHash() {
+        $hash1 = array('key1' => 'value1', 'key2' => 'value2');
+        $hash2 = array('key3' => 'value3', 'key4' => 'value4');
+
+        $this->mockDB->stageHash($hash1);
+        $this->mockDB->stageHash($hash2);
+
+        $this->assertSame($hash1, $this->mockDB->loadHash());
+        $this->assertSame($hash2, $this->mockDB->loadHash());
+        $this->assertNull($this->mockDB->loadHash());
+    }
+
+    public function testLoadResult() {
+        $this->mockDB->stageResult('value1');
+        $this->assertEquals('value1', $this->mockDB->loadResult());
+
+        $this->mockDB->stageResult('value2');
+        $this->assertEquals('value2', $this->mockDB->loadResult());
+
+        $this->mockDB->stageResult('value3');
+        $this->assertNotEquals('value2', $this->mockDB->loadResult());
+    }
+
+
+//    public function loadObject(&$object, $bindAll = false, $strip = true) {
+//        $hash = $this->loadHash();
+//
+//        $this->bindHashToObject($hash, $object, null, $strip, $bindAll);
+//    }
+//
+//    public function insertObject($table, &$object, $keyName = null, $verbose = false) {
+//
+//        parent::insertObject($table, $object, $keyName, $verbose);
+//        $object->{$keyName} = 1;
+//    }
+//
+//    public function updateObject($table, &$object, $keyName, $updateNulls = true) {
+//
+//        parent::updateObject($table, $object, $keyName, $updateNulls);
+//    }
+}
