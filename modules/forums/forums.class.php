7deb086ac04f15d401ac4c2d06f1cd39fd815c3a
Robert Basic <robertbasic.com@gmail.com>
2012-02-18 21:50:33 +0100

    fixing notices

--- modules/forums/forums.class.php
+++ modules/forums/forums.class.php
@@ -8,7 +8,7 @@ $bbparser = new HTML_BBCodeParser();
 
 $filters = array('- Filters -');
 
-if ($a == 'viewer') {
+if (isset($a) && $a == 'viewer') {
     array_push($filters, 'My Watched', 'Last 30 days');
 } else {
     array_push($filters, 'My Forums', 'My Watched', 'My Projects', 'My Company', 'Inactive Projects');
