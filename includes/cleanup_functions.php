a65b7eee453bebb61af9cff0551673fc36f5bcec
D Keith Casey Jr <keith@caseysoftware.com>
2012-04-22 00:40:53 -0400

    correcting the task datetimes to be timezone aware

--- includes/cleanup_functions.php
+++ includes/cleanup_functions.php
@@ -513,9 +513,9 @@ function showtask(&$arr, $level = 0, $is_opened = true, $today_view = false, $hi
 		$s .= '<td align="center">-</td>';
 	}
 	// duration or milestone
-    $s .= $htmlHelper->createCell('task_start_date', $arr['task_start_date']);
+    $s .= $htmlHelper->createCell('task_start_datetime', $arr['task_start_date']);
     $s .= $htmlHelper->createCell('task_duration', $arr['task_duration'] . ' ' . mb_substr($AppUI->_($durnTypes[$arr['task_duration_type']]), 0, 1));
-    $s .= $htmlHelper->createCell('task_end_date', $arr['task_end_date']);
+    $s .= $htmlHelper->createCell('task_end_datetime', $arr['task_end_date']);
 	if ($today_view) {
         $s .= $htmlHelper->createCell('task_due_in', $arr['task_due_in']);
 	} elseif ($history_active) {
