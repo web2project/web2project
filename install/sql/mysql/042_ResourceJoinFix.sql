fa1066afb04d68092ce3d3e59286ff31c967513e
D Keith Casey Jr <keith@blueparabola.com>
2012-06-03 02:02:28 -0500

    removing the database tweak in favor of the upcoming resources branch under dev now;

--- install/sql/mysql/042_ResourceJoinFix.sql
+++
@@ -1,4 +0,0 @@
--- Added by Korkonius
--- Short fix to enable the other_resources view to function with all resources
-INSERT INTO `resource_types`(`resource_type_id`,`resource_type_name`,`resource_type_note`) VALUES(0, 'All resources', 'All resources');
-UPDATE `resource_types` SET `resource_type_id` = 0 WHERE `resource_type_id` = LAST_INSERT_ID();
