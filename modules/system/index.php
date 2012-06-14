ac1dbaf663b92478f4cea55a4030426a51ea3833
D Keith Casey Jr <keith@caseysoftware.com>
2012-06-14 11:58:45 -0500

    finished basic layout in css;

--- modules/system/index.php
+++ modules/system/index.php
@@ -13,118 +13,114 @@ $titleBlock = new w2p_Theme_TitleBlock('System Administration', '48_my_computer.
 $titleBlock->show();
 
 ?>
-<table class="std" width="100%" border="0" cellpadding="0" cellspacing="5">
-    <tr>
-        <td colspan="20">
-            <div style="float: left; width: 45%;">
-                <div style="float: left; margin: 10px;">
-                    <?php echo w2PshowImage('control-center.png', 42, 42, ''); ?>
-                </div>
-                <div style="float: left;">
-                    <span class="subtitle"><?php echo $AppUI->_('System Status'); ?></span><br />
-                    <a href="?m=system&a=systemconfig"><?php echo $AppUI->_('System Configuration'); ?></a>
-                    <?php
-                    $system = new CSystem();
-                    if ('admin@web2project.net' == w2PgetConfig('admin_email', 'admin@web2project.net')) {
-                        ?>
-                        <a href="?m=system&a=systemconfig#admin_email"><?php echo $AppUI->_('Set the Admin Email Address'); ?></a> -
-                        <span class="error"><?php echo $AppUI->_('Please set an email address for your Web2project Administrator.'); ?></span>
-                        <?php
-                    }
-                    echo '<br />';
-                    if ($system->upgradeRequired()) {
-                      ?>
-                      <a href="?m=system&u=upgrade"><?php echo $AppUI->_('Apply System Updates'); ?></a> -
-                      <span class="error"><?php echo $AppUI->_('Your upgrade is not complete. Please apply the updates immediately.'); ?></span>
-                      <?php
-                    } else {
-                        echo $AppUI->_('All installed update scripts have been executed.');
-                    }
-                    echo '<br />';
-                    $tzName = w2PgetConfig('system_timezone');
-                    if(strlen($tzName) == 0) {
-                        $tzName = ini_get('date.timezone');
-                    }
-                    if (strlen($tzName) > 0) {
-                        $time = new DateTimeZone($tzName);
-                        $x = new DateTime();
-                        $offset = $time->getOffset($x);
-                        $offset = ($offset >= 0) ? '+'.$offset/3600 : $offset/3600;
-                        echo $AppUI->_('Your system has a default timezone of GMT'.$offset.'.');
-                    } else {
-                      ?>
-                      <a href="?m=system&a=systemconfig#system_timezone"><?php echo $AppUI->_('Select a Timezone'); ?></a> -
-                      <span class="error"><?php echo $AppUI->_('You do not have a default server timezone selected. Please select one immediately.'); ?></span>
-                      <?php
-                    }
-                    echo '<br />';
-                    $availableVersion = w2PgetConfig('available_version', '');
-                    if (version_compare($AppUI->getVersion(), $availableVersion, '<')) {
-                        ?>
-                        <a href="http://sourceforge.net/projects/web2project/" target="_new"><?php echo $AppUI->_('Upgrade Available!'); ?></a> -
-                        <span class="error"><?php echo $AppUI->_('Your system should be upgraded to v'.$availableVersion.'.  Please upgrade at your earliest convenience.'); ?></span>
-                        <?php
-                    } else {
-                        echo $AppUI->_('Your system is the latest version available.');
-                    }
-                    ?>
-                </div>
-            </div>
-            <div style="float: right; width: 45%;">
-                <div style="float: left; margin: 10px;">
-                    <?php echo w2PshowImage('myevo-weather.png', 42, 42, ''); ?>
-                </div>
-                <div style="float: left;">
-                    <span class="subtitle"><?php echo $AppUI->_('Preferences'); ?></span><br />
-                    <a href="?m=system&a=addeditpref"><?php echo $AppUI->_('Default User Preferences'); ?></a><br />
-                    <a href="?m=system&u=syskeys&a=keys"><?php echo $AppUI->_('System Lookup Keys'); ?></a><br />
-                    <a href="?m=system&u=syskeys"><?php echo $AppUI->_('System Lookup Values'); ?></a><br />
-                    <a href="?m=system&a=custom_field_editor"><?php echo $AppUI->_('Custom Field Editor'); ?></a><br />
-                </div>
-            </div>
+<div class="std" style="overflow:hidden; height:1%; width: 100%;">
+    <div style="float: left; width: 45%;">
+        <div style="float: left; margin: 10px;">
+            <?php echo w2PshowImage('control-center.png', 42, 42, ''); ?>
+        </div>
+        <div style="float: left;">
+            <span class="subtitle"><?php echo $AppUI->_('System Status'); ?></span><br />
+            <a href="?m=system&a=systemconfig"><?php echo $AppUI->_('System Configuration'); ?></a>
+            <?php
+            $system = new CSystem();
+            if ('admin@web2project.net' == w2PgetConfig('admin_email', 'admin@web2project.net')) {
+                ?>
+                <a href="?m=system&a=systemconfig#admin_email"><?php echo $AppUI->_('Set the Admin Email Address'); ?></a> -
+                <span class="error"><?php echo $AppUI->_('Please set an email address for your Web2project Administrator.'); ?></span>
+                <?php
+            }
+            echo '<br />';
+            if ($system->upgradeRequired()) {
+              ?>
+              <a href="?m=system&u=upgrade"><?php echo $AppUI->_('Apply System Updates'); ?></a> -
+              <span class="error"><?php echo $AppUI->_('Your upgrade is not complete. Please apply the updates immediately.'); ?></span>
+              <?php
+            } else {
+                echo $AppUI->_('All installed update scripts have been executed.');
+            }
+            echo '<br />';
+            $tzName = w2PgetConfig('system_timezone');
+            if(strlen($tzName) == 0) {
+                $tzName = ini_get('date.timezone');
+            }
+            if (strlen($tzName) > 0) {
+                $time = new DateTimeZone($tzName);
+                $x = new DateTime();
+                $offset = $time->getOffset($x);
+                $offset = ($offset >= 0) ? '+'.$offset/3600 : $offset/3600;
+                echo $AppUI->_('Your system has a default timezone of GMT'.$offset.'.');
+            } else {
+              ?>
+              <a href="?m=system&a=systemconfig#system_timezone"><?php echo $AppUI->_('Select a Timezone'); ?></a> -
+              <span class="error"><?php echo $AppUI->_('You do not have a default server timezone selected. Please select one immediately.'); ?></span>
+              <?php
+            }
+            echo '<br />';
+            $availableVersion = w2PgetConfig('available_version', '');
+            if (version_compare($AppUI->getVersion(), $availableVersion, '<')) {
+                ?>
+                <a href="http://sourceforge.net/projects/web2project/" target="_new"><?php echo $AppUI->_('Upgrade Available!'); ?></a> -
+                <span class="error"><?php echo $AppUI->_('Your system should be upgraded to v'.$availableVersion.'.  Please upgrade at your earliest convenience.'); ?></span>
+                <?php
+            } else {
+                echo $AppUI->_('Your system is the latest version available.');
+            }
+            ?>
+        </div>
+    </div>
+    <div style="float: right; width: 45%;">
+        <div style="float: left; margin: 10px;">
+            <?php echo w2PshowImage('myevo-weather.png', 42, 42, ''); ?>
+        </div>
+        <div style="float: left;">
+            <span class="subtitle"><?php echo $AppUI->_('Preferences'); ?></span><br />
+            <a href="?m=system&a=addeditpref"><?php echo $AppUI->_('Default User Preferences'); ?></a><br />
+            <a href="?m=system&u=syskeys&a=keys"><?php echo $AppUI->_('System Lookup Keys'); ?></a><br />
+            <a href="?m=system&u=syskeys"><?php echo $AppUI->_('System Lookup Values'); ?></a><br />
+            <a href="?m=system&a=custom_field_editor"><?php echo $AppUI->_('Custom Field Editor'); ?></a><br />
+        </div>
+    </div>
 
-            <div style="float: left; width: 45%; margin-top: 20px;">
-                <div style="float: left; margin: 10px;">
-                    <?php echo w2PshowImage('power-management.png', 42, 42, ''); ?>
-                </div>
-                <div style="float: left;">
-                    <span class="subtitle"><?php echo $AppUI->_('Modules'); ?></span><br />
-                    <a href="?m=system&u=modules"><?php echo $AppUI->_('View Modules'); ?></a>
-                </div>
-            </div>
-            <div style="float: right; width: 45%; margin-top: 20px;">
-                <div style="float: left; margin: 10px;">
-                    <?php echo w2PshowImage('rdf2.png', 42, 42, ''); ?>
-                </div>
-                <div style="float: left;">
-                    <span class="subtitle"><?php echo $AppUI->_('Language Support'); ?></span><br />
-                    <a href="?m=system&a=translate"><?php echo $AppUI->_('Translation Management'); ?></a>
-                </div>
-            </div>
+    <div style="float: left; width: 45%; margin-top: 20px;">
+        <div style="float: left; margin: 10px;">
+            <?php echo w2PshowImage('power-management.png', 42, 42, ''); ?>
+        </div>
+        <div style="float: left;">
+            <span class="subtitle"><?php echo $AppUI->_('Modules'); ?></span><br />
+            <a href="?m=system&u=modules"><?php echo $AppUI->_('View Modules'); ?></a>
+        </div>
+    </div>
+    <div style="float: right; width: 45%; margin-top: 20px;">
+        <div style="float: left; margin: 10px;">
+            <?php echo w2PshowImage('rdf2.png', 42, 42, ''); ?>
+        </div>
+        <div style="float: left;">
+            <span class="subtitle"><?php echo $AppUI->_('Language Support'); ?></span><br />
+            <a href="?m=system&a=translate"><?php echo $AppUI->_('Translation Management'); ?></a>
+        </div>
+    </div>
 
-            <div style="float: left; width: 45%; margin-top: 20px;">
-                <div style="float: left; margin: 10px;">
-                    <?php echo w2PshowImage('main-settings.png', 42, 42, ''); ?>
-                </div>
-                <div style="float: left;">
-                    <span class="subtitle"><?php echo $AppUI->_('Administration'); ?></span><br />
-                    <a href="?m=system&u=roles"><?php echo $AppUI->_('User Roles'); ?></a><br />
-                    <a href="?m=system&a=acls_view"><?php echo $AppUI->_('Users Permissions Information'); ?></a><br />
-                    <a href="?m=system&a=contacts_ldap"><?php echo $AppUI->_('Import Contacts'); ?></a><br />
-                    <a href="?m=system&a=phpinfo&suppressHeaders=1" target="_blank"><?php echo $AppUI->_('PHP Info'); ?></a>
-                </div>
-            </div>
-            <div style="float: right; width: 45%; margin-top: 20px;">
-                <div style="float: left; margin: 10px;">
-                    <?php echo w2PshowImage('control-center.png', 42, 42, ''); ?>
-                </div>
-                <div style="float: left;">
-                    <span class="subtitle"><?php echo $AppUI->_('Budgeting Setup'); ?></span><br />
-                    <a href="?m=system&a=billingcode"><?php echo $AppUI->_('Billing Code Table'); ?></a><br />
-                    <a href="?m=system&a=budgeting"><?php echo $AppUI->_('Setup Budgets'); ?></a><br />
-                    <a href="?m=system&a=budgeting_allocated"><?php echo $AppUI->_('Budget Allocated'); ?></a>
-                </div>
-            </div>
-        </td>
-    </tr>
-</table>
+    <div style="float: left; width: 45%; margin-top: 20px;">
+        <div style="float: left; margin: 10px;">
+            <?php echo w2PshowImage('main-settings.png', 42, 42, ''); ?>
+        </div>
+        <div style="float: left;">
+            <span class="subtitle"><?php echo $AppUI->_('Administration'); ?></span><br />
+            <a href="?m=system&u=roles"><?php echo $AppUI->_('User Roles'); ?></a><br />
+            <a href="?m=system&a=acls_view"><?php echo $AppUI->_('Users Permissions Information'); ?></a><br />
+            <a href="?m=system&a=contacts_ldap"><?php echo $AppUI->_('Import Contacts'); ?></a><br />
+            <a href="?m=system&a=phpinfo&suppressHeaders=1" target="_blank"><?php echo $AppUI->_('PHP Info'); ?></a>
+        </div>
+    </div>
+    <div style="float: right; width: 45%; margin-top: 20px;">
+        <div style="float: left; margin: 10px;">
+            <?php echo w2PshowImage('control-center.png', 42, 42, ''); ?>
+        </div>
+        <div style="float: left;">
+            <span class="subtitle"><?php echo $AppUI->_('Budgeting Setup'); ?></span><br />
+            <a href="?m=system&a=billingcode"><?php echo $AppUI->_('Billing Code Table'); ?></a><br />
+            <a href="?m=system&a=budgeting"><?php echo $AppUI->_('Setup Budgets'); ?></a><br />
+            <a href="?m=system&a=budgeting_allocated"><?php echo $AppUI->_('Budget Allocated'); ?></a>
+        </div>
+    </div>
+</div>
