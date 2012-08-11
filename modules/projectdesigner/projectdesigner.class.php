884d5c708185b246f7a959001de12f3c3bbd5f66
D Keith Casey Jr <keith@blueparabola.com>
2012-08-11 13:37:52 -0500

    implemented the preStore hook to make sure we're updating an existing record (if it exists) instead of trying to insert duplicate rows;

--- modules/projectdesigner/projectdesigner.class.php
+++ modules/projectdesigner/projectdesigner.class.php
@@ -9,7 +9,7 @@ include_once (W2P_BASE_DIR . '/modules/projectdesigner/config.php');
 class CProjectDesigner extends w2p_Core_BaseObject
 {
 
-    public $pd_option_id = null;
+    public $pd_option_id = 0;
     public $pd_option_user = null;
     public $pd_option_view_project = null;
     public $pd_option_view_gantt = null;
@@ -21,6 +21,21 @@ class CProjectDesigner extends w2p_Core_BaseObject
     public function __construct()
     {
         parent::__construct('project_designer_options', 'pd_option_id');
+    }
+
+    protected function hook_preStore()
+    {
+        $this->pd_option_user = $this->_AppUI->user_id;
+
+        $pd_options = $this->loadAll(null, 'pd_option_user = ' . $this->pd_option_user);
+        if (count($pd_options)) {
+            foreach($pd_options as $options) {
+                $pd_options_id = $options['pd_option_id'];
+            }
+        }
+        $this->pd_option_id = $pd_options_id;
+
+        parent::hook_preStore();
     }
 
     /*
