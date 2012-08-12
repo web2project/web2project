0221df05a968f3694018df1b66ffb7de33c90fee
D Keith Casey Jr <keith@blueparabola.com>
2012-08-12 02:32:14 -0500

    updated the controller to our default structure;

--- modules/projectdesigner/do_projectdesigner_aed.php
+++ modules/projectdesigner/do_projectdesigner_aed.php
@@ -2,14 +2,13 @@
 if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
-global $AppUI;
-
-//Lets store the panels view options of the user:
-$pdo = new CProjectDesigner();
-$pdo->bind($_POST);
-$pdo->store();
 
 $project_id = (int) w2PgetParam($_POST, 'project_id', 0);
+$url = 'm=projectdesigner&project_id=' . $project_id;
 
-$AppUI->setMsg('Your workspace has been saved', UI_MSG_OK);
-$AppUI->redirect('m=projectdesigner&project_id=' . $project_id);
+$controller = new w2p_Controllers_Base(
+                    new CProjectDesigner(), false, 'Your workspace has been ', $url, $url
+                  );
+
+$AppUI = $controller->process($AppUI, $_POST);
+$AppUI->redirect($controller->resultPath);
