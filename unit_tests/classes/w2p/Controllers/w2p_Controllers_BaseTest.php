6dfd2b831f9940eb59a6b41c4b490897892ef700
D Keith Casey Jr <keith@blueparabola.com>
2012-08-18 23:52:19 -0500

    wired a couple more tests for the Base Controller;

--- unit_tests/classes/w2p/Controllers/w2p_Controllers_BaseTest.php
+++ unit_tests/classes/w2p/Controllers/w2p_Controllers_BaseTest.php
@@ -24,63 +24,58 @@ include_once 'CommonSetup.php';
 
 class w2p_Controllers_Base_Test extends CommonSetup
 {
-
-    /**
-     * An AppUI object for validation
-     *
-     * @param CAppUI
-     * @access private
-     */
-    private $appUI;
-
-    /**
-     * Create an AppUI before running tests
-     */
     protected function setUp()
     {
-        parent::setUp();
-        $this->appUI = new w2p_Core_CAppUI();
-        $_POST['login'] = 'login';
-        $_REQUEST['login'] = 'sql';
-        $this->appUI->login('admin', 'passwd');
-    }
+      parent::setUp();
 
+      $this->link    = new CLink();
+      $this->link->overrideDatabase($this->mockDB);
+
+      $this->obj = new w2p_Controllers_Base($this->link, false, 'prefix', '/success', '/failure');
+
+      $GLOBALS['acl'] = new w2p_Mocks_Permissions();
+
+      $this->post_data = array(
+          'dosql'             => 'do_link_aed',
+          'link_id'           => 0,
+          'link_name'         => 'web2project homepage',
+          'link_project'      => 0,
+          'link_task'         => 0,
+          'link_url'          => 'http://web2project.net',
+          'link_parent'       => '0',
+          'link_description'  => 'This is web2project',
+          'link_owner'        => 1,
+          'link_date'         => '2009-01-01',
+          'link_icon'         => '',
+          'link_category'     => 0
+      );
+    }
     /**
      * Tests that a new base controller object has the proper attributes
      */
     public function testNewBaseAttributes()
     {
-        $base_controller = new w2p_Controllers_Base(new CLink(), false, 'prefix', '/success', '/failure');
-
-        $this->assertInstanceOf('w2p_Controllers_Base',     $base_controller);
-        $this->assertObjectHasAttribute('delete',           $base_controller);
-        $this->assertObjectHasAttribute('successPath',      $base_controller);
-        $this->assertObjectHasAttribute('errorPath',        $base_controller);
-        $this->assertObjectHasAttribute('accessDeniedPath', $base_controller);
-        $this->assertObjectHasAttribute('object',           $base_controller);
-        $this->assertObjectHasAttribute('success',          $base_controller);
-        $this->assertObjectHasAttribute('resultPath',       $base_controller);
-        $this->assertObjectHasAttribute('resultMessage',    $base_controller);
-        $this->assertInstanceOf('CLink',                    $base_controller->object);
+        $this->assertInstanceOf('w2p_Controllers_Base',     $this->obj);
+        $this->assertObjectHasAttribute('delete',           $this->obj);
+        $this->assertObjectHasAttribute('successPath',      $this->obj);
+        $this->assertObjectHasAttribute('errorPath',        $this->obj);
+        $this->assertObjectHasAttribute('object',           $this->obj);
+        $this->assertObjectHasAttribute('success',          $this->obj);
+        $this->assertObjectHasAttribute('resultPath',       $this->obj);
+        $this->assertInstanceOf('CLink',                    $this->obj->object);
     }
 
     /**
-     * @todo Implement testSetAccessDeniedPath().
+     * Testing process() with first a well-formed POST and then a damaged POST.
      */
-    public function testSetAccessDeniedPath() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
-    }
+    public function testProcess()
+    {
+        $AppUI = $this->obj->process($this->_AppUI, $this->post_data);
+        $this->assertEquals('/success',   $this->obj->resultPath);
 
-    /**
-     * @todo Implement testProcess().
-     */
-    public function testProcess() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        unset($this->post_data['link_url']);
+        $this->obj->object = new CLink();
+        $AppUI = $this->obj->process($this->_AppUI, $this->post_data);
+        $this->assertEquals('/failure',   $this->obj->resultPath);
     }
 }
