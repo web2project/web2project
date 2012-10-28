c12f723fa58fc5a598ce8f3985a3e9323831f594
D Keith Casey Jr <keith@blueparabola.com>
2012-10-28 15:00:22 -0500

    wired the system config to *clear* all task reminders if 'Send Reminder' is turned off; WARNING: that means if you turn reminders back on, they're gone. to get them back, you must re-save the individual tasks Resolves: http://bugs.web2project.net/view.php?id=1049

--- classes/w2p/Core/EventQueue.class.php
+++ classes/w2p/Core/EventQueue.class.php
@@ -13,7 +13,11 @@
  * event notifications and other timed events, as well as
  * outgoing emails.
  *
- * Copyright 2005, the web2Project team.
+ * Copyright 2005           dotproject team
+ * Copyright 2007-2012      web2project team
+ *
+ * @todo TODO: This can probably be converted to extend the w2p_Core_BaseObject
+ *    and just get rid of a bunch of this code. ~ dkc 28 Oct 2012
  */
 
 class w2p_Core_EventQueue {
