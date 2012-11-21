7c8d96daecccdb10f302a7008920d347209ac1a5
D Keith Casey Jr <keith@blueparabola.com>
2012-11-21 17:22:39 -0600

    updating the module header and nav to reflect the simplified layouts;

--- modules/admin/viewuser.php
+++ modules/admin/viewuser.php
@@ -77,11 +77,11 @@ if ($canRead) {
     $titleBlock->addCrumb('?m=admin', 'users list');
 }
 if ($canEdit || $user_id == $AppUI->user_id) {
+    $titleBlock->addCell('<input type="button" class=button value="' . $AppUI->_('add user') . '" onclick="javascript:window.location=\'./index.php?m=admin&a=addedituser\';" />');
     $titleBlock->addCrumb('?m=admin&a=addedituser&user_id='.$user_id, 'edit this user');
     $titleBlock->addCrumb('?m=contacts&a=addedit&contact_id='.$user->contact_id, 'edit this contact');
     $titleBlock->addCrumb('?m=system&a=addeditpref&user_id='.$user_id, 'edit preferences');
     $titleBlock->addCrumbRight('<div class="crumb"><ul style="float:right;"><li><a href="javascript: void(0);" onclick="popChgPwd();return false"><span>' . $AppUI->_('change password') . '</span></a></li></ul></div>');
-    $titleBlock->addCell('<td align="right" width="100%"><input type="button" class=button value="' . $AppUI->_('add user') . '" onclick="javascript:window.location=\'./index.php?m=admin&a=addedituser\';" /></td>');
 }
 $titleBlock->show();
 ?>
