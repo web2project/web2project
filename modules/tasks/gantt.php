288f3c50cb2904ac335bf16baae6e1477f28a74a
D Keith Casey Jr <keith@blueparabola.com>
2012-11-26 00:39:00 -0600

    tabs for spaces

--- modules/tasks/gantt.php
+++ modules/tasks/gantt.php
@@ -1,6 +1,6 @@
 <?php /* $Id$ $URL$ */
 if (!defined('W2P_BASE_DIR')) {
-	die('You should not access this file directly.');
+    die('You should not access this file directly.');
 }
 
 global $gantt_arr, $w2Pconfig;
@@ -46,51 +46,51 @@ $projects = $project->getAllowedProjects($AppUI->user_id, false);
 $caller = w2PgetParam($_REQUEST, 'caller', null);
 
 if ($caller == 'todo') {
-	$user_id = w2PgetParam($_REQUEST, 'user_id', $AppUI->user_id);
+    $user_id = w2PgetParam($_REQUEST, 'user_id', $AppUI->user_id);
 
-	$projects[$project_id]['project_name'] = $AppUI->_('Todo for') . ' ' . CContact::getContactByUserid($user_id);
-	$projects[$project_id]['project_color_identifier'] = 'ff6000';
+    $projects[$project_id]['project_name'] = $AppUI->_('Todo for') . ' ' . CContact::getContactByUserid($user_id);
+    $projects[$project_id]['project_color_identifier'] = 'ff6000';
 
-	$q = new w2p_Database_Query;
-	$q->addQuery('t.*');
-	$q->addQuery('project_name, project_id, project_color_identifier');
-	$q->addQuery('tp.task_pinned');
-	$q->addTable('tasks', 't');
+    $q = new w2p_Database_Query;
+    $q->addQuery('t.*');
+    $q->addQuery('project_name, project_id, project_color_identifier');
+    $q->addQuery('tp.task_pinned');
+    $q->addTable('tasks', 't');
     $q->innerJoin('projects', 'pr', 'pr.project_id = t.task_project');
- 	$q->innerJoin('user_tasks', 'ut', 'ut.task_id = t.task_id AND ut.user_id = ' . (int) $user_id);
-	$q->leftJoin('user_task_pin', 'tp', 'tp.task_id = t.task_id and tp.user_id = ' . (int)$user_id);
-	$q->addWhere('(t.task_percent_complete < 100 OR t.task_percent_complete IS NULL)');
-	$q->addWhere('t.task_status = 0');
-	if (!$showArcProjs) {
-		$q->addWhere('pr.project_active = 1');
-		if (($template_status = w2PgetConfig('template_projects_status_id')) != '') {
-			$q->addWhere('pr.project_status <> ' . (int)$template_status);
-		}
-	}
-	if (!$showLowTasks) {
-		$q->addWhere('task_priority >= 0');
-	}
-	if (!$showHoldProjs) {
-		$q->addWhere('project_active = 1');
-	}
-	if (!$showDynTasks) {
-		$q->addWhere('task_dynamic <> 1');
-	}
-	if ($showPinned) {
-		$q->addWhere('task_pinned = 1');
-	}
+    $q->innerJoin('user_tasks', 'ut', 'ut.task_id = t.task_id AND ut.user_id = ' . (int) $user_id);
+    $q->leftJoin('user_task_pin', 'tp', 'tp.task_id = t.task_id and tp.user_id = ' . (int)$user_id);
+    $q->addWhere('(t.task_percent_complete < 100 OR t.task_percent_complete IS NULL)');
+    $q->addWhere('t.task_status = 0');
+    if (!$showArcProjs) {
+        $q->addWhere('pr.project_active = 1');
+        if (($template_status = w2PgetConfig('template_projects_status_id')) != '') {
+            $q->addWhere('pr.project_status <> ' . (int)$template_status);
+        }
+    }
+    if (!$showLowTasks) {
+        $q->addWhere('task_priority >= 0');
+    }
+    if (!$showHoldProjs) {
+        $q->addWhere('project_active = 1');
+    }
+    if (!$showDynTasks) {
+        $q->addWhere('task_dynamic <> 1');
+    }
+    if ($showPinned) {
+        $q->addWhere('task_pinned = 1');
+    }
 
     $q->addGroup('t.task_id');
     $q->addOrder('t.task_end_date, t.task_priority DESC');
 } else {
-	// pull tasks
-	$q = new w2p_Database_Query();
-	$q->addTable('tasks', 't');
-	$q->addQuery('t.task_id, task_parent, task_name, task_start_date, task_end_date,'.
-		' task_duration, task_duration_type, task_priority, task_percent_complete,'.
-		' task_hours_worked, task_order, task_project, task_milestone, task_access,'.
+    // pull tasks
+    $q = new w2p_Database_Query();
+    $q->addTable('tasks', 't');
+    $q->addQuery('t.task_id, task_parent, task_name, task_start_date, task_end_date,'.
+        ' task_duration, task_duration_type, task_priority, task_percent_complete,'.
+        ' task_hours_worked, task_order, task_project, task_milestone, task_access,'.
         ' task_owner, project_name, project_color_identifier, task_dynamic');
-	$q->addJoin('projects', 'p', 'project_id = t.task_project', 'inner');
+    $q->addJoin('projects', 'p', 'project_id = t.task_project', 'inner');
     $q->addOrder('p.project_id, t.task_end_date');
 
     // don't add milestones if box is checked//////////////////////////////////////////////////////////
@@ -103,28 +103,28 @@ if ($caller == 'todo') {
     if ($ganttTaskFilter) {
         $q->addWhere($where);
     }
-	if ($project_id) {
-		$q->addWhere('task_project = ' . (int)$project_id);
-	}
+    if ($project_id) {
+        $q->addWhere('task_project = ' . (int)$project_id);
+    }
 
-	switch ($f) {
-		case 'all':
-			$q->addWhere('task_status > -1');
-			break;
-		case 'myproj':
-			$q->addWhere('task_status > -1');
-			$q->addWhere('project_owner = ' . (int)$AppUI->user_id);
-			break;
-		case 'mycomp':
-			$q->addWhere('task_status > -1');
-			$q->addWhere('project_company = ' . (int)$AppUI->user_company);
-			break;
-		case 'myinact':
-		default:
-			$q->innerJoin('user_tasks', 'ut', 'ut.task_id = t.task_id');
-			$q->addWhere('ut.user_id = '.$AppUI->user_id);
-			break;
-	}
+    switch ($f) {
+        case 'all':
+            $q->addWhere('task_status > -1');
+            break;
+        case 'myproj':
+            $q->addWhere('task_status > -1');
+            $q->addWhere('project_owner = ' . (int)$AppUI->user_id);
+            break;
+        case 'mycomp':
+            $q->addWhere('task_status > -1');
+            $q->addWhere('project_company = ' . (int)$AppUI->user_company);
+            break;
+        case 'myinact':
+        default:
+            $q->innerJoin('user_tasks', 'ut', 'ut.task_id = t.task_id');
+            $q->addWhere('ut.user_id = '.$AppUI->user_id);
+            break;
+    }
 
     
 }
@@ -140,49 +140,49 @@ $start_min = date('Y-m-d H:i:s');
 
 //pull the tasks into an array
 if ($caller != 'todo') {
-	$criticalTasks = $project->getCriticalTasks($project_id);
+    $criticalTasks = $project->getCriticalTasks($project_id);
 }
 
 foreach ($proTasks as $row) {
-	//Check if start date exists, if not try giving it the end date.
-	//If the end date does not exist then set it for today.
-	//This avoids jpgraphs internal errors that render the gantt completely useless
-	if ($row['task_start_date'] == '0000-00-00 00:00:00') {
-		if ($row['task_end_date'] == '0000-00-00 00:00:00') {
-			$todaydate = new w2p_Utilities_Date();
-			$row['task_start_date'] = $todaydate->format(FMT_TIMESTAMP_DATE);
-		} else {
-			$row['task_start_date'] = $row['task_end_date'];
-		}
-	}
+    //Check if start date exists, if not try giving it the end date.
+    //If the end date does not exist then set it for today.
+    //This avoids jpgraphs internal errors that render the gantt completely useless
+    if ($row['task_start_date'] == '0000-00-00 00:00:00') {
+        if ($row['task_end_date'] == '0000-00-00 00:00:00') {
+            $todaydate = new w2p_Utilities_Date();
+            $row['task_start_date'] = $todaydate->format(FMT_TIMESTAMP_DATE);
+        } else {
+            $row['task_start_date'] = $row['task_end_date'];
+        }
+    }
 
-	$tsd = new w2p_Utilities_Date($row['task_start_date']);
-	if ($tsd->before(new w2p_Utilities_Date($start_min))) {
-		$start_min = $row['task_start_date'];
-	}
+    $tsd = new w2p_Utilities_Date($row['task_start_date']);
+    if ($tsd->before(new w2p_Utilities_Date($start_min))) {
+        $start_min = $row['task_start_date'];
+    }
 
-	//Check if end date exists, if not try giving it the start date.
-	//If the start date does not exist then set it for today.
-	//This avoids jpgraphs internal errors that render the gantt completely useless
-	if ($row['task_end_date'] == '0000-00-00 00:00:00') {
-		if ($row['task_duration']) {
-			$row['task_end_date'] = db_unix2dateTime(db_dateTime2unix($row['task_start_date']) + SECONDS_PER_DAY * convert2days($row['task_duration'], $row['task_duration_type']));
-		} else {
-			$todaydate = new w2p_Utilities_Date();
-			$row['task_end_date'] = $todaydate->format(FMT_TIMESTAMP_DATE);
-		}
-	}
+    //Check if end date exists, if not try giving it the start date.
+    //If the start date does not exist then set it for today.
+    //This avoids jpgraphs internal errors that render the gantt completely useless
+    if ($row['task_end_date'] == '0000-00-00 00:00:00') {
+        if ($row['task_duration']) {
+            $row['task_end_date'] = db_unix2dateTime(db_dateTime2unix($row['task_start_date']) + SECONDS_PER_DAY * convert2days($row['task_duration'], $row['task_duration_type']));
+        } else {
+            $todaydate = new w2p_Utilities_Date();
+            $row['task_end_date'] = $todaydate->format(FMT_TIMESTAMP_DATE);
+        }
+    }
 
-	$ted = new w2p_Utilities_Date($row['task_end_date']);
-	if ($ted->after(new w2p_Utilities_Date($end_max))) {
-		$end_max = $row['task_end_date'];
-	}
-	if ($ted->after(new w2p_Utilities_Date($projects[$row['task_project']]['project_end_date']))
+    $ted = new w2p_Utilities_Date($row['task_end_date']);
+    if ($ted->after(new w2p_Utilities_Date($end_max))) {
+        $end_max = $row['task_end_date'];
+    }
+    if ($ted->after(new w2p_Utilities_Date($projects[$row['task_project']]['project_end_date']))
         || $projects[$row['task_project']]['project_end_date'] == '') {
-		$projects[$row['task_project']]['project_end_date'] = $row['task_end_date'];
-	}
+        $projects[$row['task_project']]['project_end_date'] = $row['task_end_date'];
+    }
 
-	$projects[$row['task_project']]['tasks'][] = $row;
+    $projects[$row['task_project']]['tasks'][] = $row;
 }
 
 $width = min(w2PgetParam($_GET, 'width', 600), 1400);
@@ -191,16 +191,16 @@ $end_date = w2PgetParam($_GET, 'end_date', $end_max);
 
 //consider critical (concerning end date) tasks as well
 if ($caller != 'todo') {
-	$start_min = $projects[$project_id]['project_start_date'];
-	$end_max = (($projects[$project_id]['project_end_date'] > $criticalTasks[0]['task_end_date']) 
-				? $projects[$project_id]['project_end_date'] : $criticalTasks[0]['task_end_date']);
+    $start_min = $projects[$project_id]['project_start_date'];
+    $end_max = (($projects[$project_id]['project_end_date'] > $criticalTasks[0]['task_end_date']) 
+                ? $projects[$project_id]['project_end_date'] : $criticalTasks[0]['task_end_date']);
 }
 
 $count = 0;
 
 // If hyperlinks are to be added then the graph is of a set width///////
 if ($addLinksToGantt == '1') {
-	$width = 1450 ;
+    $width = 1450 ;
 }
 
 $gantt = new w2p_Output_GanttRenderer($AppUI, $width);
@@ -224,34 +224,34 @@ $gantt->setColumnHeaders($columnNames, $columnSizes);
 $gantt->setProperties(array('showhgrid' => true));
 
 if (!$start_date || !$end_date) {
-	// find out DateRange from gant_arr
-	$d_start = new w2p_Utilities_Date();
-	$d_end = new w2p_Utilities_Date();
+    // find out DateRange from gant_arr
+    $d_start = new w2p_Utilities_Date();
+    $d_end = new w2p_Utilities_Date();
     $taskArray = count($gantt_arr);
-	for ($i = 0, $i_cmp = $taskArray; $i < $i_cmp; $i++) {
-		$a = $gantt_arr[$i][0];
-		$start = substr($a['task_start_date'], 0, 10);
-		$end = substr($a['task_end_date'], 0, 10);
+    for ($i = 0, $i_cmp = $taskArray; $i < $i_cmp; $i++) {
+        $a = $gantt_arr[$i][0];
+        $start = substr($a['task_start_date'], 0, 10);
+        $end = substr($a['task_end_date'], 0, 10);
 
-		$d_start->Date($start);
-		$d_end->Date($end);
+        $d_start->Date($start);
+        $d_end->Date($end);
 
-		if ($i == 0) {
+        if ($i == 0) {
             $min_d_start = $d_start;
             $max_d_end = $d_end;
             $start_date = $start;
             $end_date = $end;
-		} else {
-			if (Date::compare($min_d_start, $d_start) > 0) {
-				$min_d_start = $d_start;
+        } else {
+            if (Date::compare($min_d_start, $d_start) > 0) {
+                $min_d_start = $d_start;
                 $start_date = $start;
-			}
-			if (Date::compare($max_d_end, $d_end) < 0) {
-				$max_d_end = $d_end;
+            }
+            if (Date::compare($max_d_end, $d_end) < 0) {
+                $max_d_end = $d_end;
                 $end_date = $end;
-			}
-		}
-	}
+            }
+        }
+    }
 }
 $start_date = $AppUI->formatTZAwareTime($start_date, '%Y-%m-%d %T');
 $end_date = $AppUI->formatTZAwareTime($end_date, '%Y-%m-%d %T');
@@ -264,8 +264,8 @@ $displayed = array();
 foreach ($projects as $p) {
     $tnums = count($p['tasks']);
 
-	for ($i = 0; $i < $tnums; $i++) {
-		$t = $p['tasks'][$i];
+    for ($i = 0; $i < $tnums; $i++) {
+        $t = $p['tasks'][$i];
         if ($caller == 'todo') {
             if ($showDynTasks) {
                 if ($t['task_parent'] == $t['task_id']) {
@@ -280,8 +280,8 @@ foreach ($projects as $p) {
                 showgtask($t);
                 findchild_gantt($p['tasks'], $t['task_id']);
             }
-		}
-	}
+        }
+    }
 }
 $gantt->loadTaskArray($gantt_arr);
 
@@ -289,7 +289,7 @@ $row = 0;
 for ($i = 0, $i_cmp = count($gantt_arr); $i < $i_cmp; $i++) {
     $a = $gantt_arr[$i][0];
     $level = $gantt_arr[$i][1];
-	$caption = '';
+    $caption = '';
 
     $tmpTask = new CTask();
     $tmpTask->load($a['task_id']);
@@ -307,7 +307,7 @@ for ($i = 0, $i_cmp = count($gantt_arr); $i < $i_cmp; $i++) {
         $start = $start->getDate();
 
         $end   = (int) ($a['task_end_date']) ? new w2p_Utilities_Date($AppUI->formatTZAwareTime($a['task_end_date'], '%Y-%m-%d %T')) : new w2p_Utilities_Date();
-		$end = $end->getDate();
+        $end = $end->getDate();
 
         $progress = (int) $a['task_percent_complete'];
 
@@ -333,14 +333,14 @@ for ($i = 0, $i_cmp = count($gantt_arr); $i < $i_cmp; $i++) {
         if ($showLabels == '1') {
             $res = $task->getAssignedUsers($a['task_id']);
             foreach ($res as $rw) {
-				switch ($rw['perc_assignment']) {
-					case 100:
-						$caption .= $rw['contact_display_name'] . ';';
-						break;
-					default:
-						$caption .= $rw['contact_display_name'] . ' [' . $rw['perc_assignment'] . '%];';
-						break;
-				}
+                switch ($rw['perc_assignment']) {
+                    case 100:
+                        $caption .= $rw['contact_display_name'] . ';';
+                        break;
+                    default:
+                        $caption .= $rw['contact_display_name'] . ' [' . $rw['perc_assignment'] . '%];';
+                        break;
+                }
             }
             $q->clear();
             $caption = mb_substr($caption, 0, mb_strlen($caption) - 1);
@@ -409,7 +409,7 @@ for ($i = 0, $i_cmp = count($gantt_arr); $i < $i_cmp; $i++) {
                     $captionToTheLeft = true;
                 }
                 $gantt->addMilestone($fieldArray, $task_start_date, $color, 0, $captionToTheLeft);
-            }	//this closes the code that is not processed if hide milestones is checked ///////////////
+            }    //this closes the code that is not processed if hide milestones is checked ///////////////
         } else {
             $type = $a['task_duration_type'];
             $dur = $a['task_duration'];
@@ -428,11 +428,11 @@ for ($i = 0, $i_cmp = count($gantt_arr); $i < $i_cmp; $i++) {
             } else {
                 if ($caller == 'todo') {
                     $columnValues = array('task_name' => $name, 'project_name' => $pname,
-						'duration' => $dur, 'start_date' => $start, 'end_date' => $end,
-						'actual_end' => '');
+                        'duration' => $dur, 'start_date' => $start, 'end_date' => $end,
+                        'actual_end' => '');
                 } else {
                     $columnValues = array('task_name' => $name, 'duration' => $dur,
-						'start_date' => $start, 'end_date' => $end, 'actual_end' => '');
+                        'start_date' => $start, 'end_date' => $end, 'actual_end' => '');
                 }
             }
             $gantt->addBar($columnValues, $caption, $height, '8F8FBD', true, $progress, $a['task_id']);
