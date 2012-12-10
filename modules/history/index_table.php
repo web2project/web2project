ff18e4cd06ddedfc80a6b36c6b1b36cf68d47d89
D Keith Casey Jr <keith@blueparabola.com>
2012-12-09 23:04:12 -0600

    built out the history viewer using the proper code structures, simlar to the links module;

--- modules/history/index_table.php
+++ modules/history/index_table.php
@@ -2,110 +2,64 @@
 if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
-global $AppUI, $deny1, $canRead, $canEdit, $project_id, $task_id, $showProject, $tab;
-global $filter_param;
-
-echo 'x'.$filter_param.'x';
-
-$filter = array();
-if ($filter_param) {
-	$in_filter = $_REQUEST['filter'];
-	$filter[] = 'history_table = \'' . $_REQUEST['filter'] . '\' ';
-} else {
-	$in_filter = '';
-}
-
-if (!empty($_REQUEST['project_id'])) {
-	$project_id = w2PgetParam($_REQUEST, 'project_id', 0);
-
-	$q = new w2p_Database_Query;
-	$q->addTable('tasks');
-	$q->addQuery('task_id');
-	$q->addWhere('task_project = ' . (int)$project_id);
-	$project_tasks = implode(',', $q->loadColumn());
-	if (!empty($project_tasks)) {
-		$project_tasks = 'OR (history_table = \'tasks\' AND history_item IN (' . $project_tasks . '))';
-	}
-
-	$q->addTable('files');
-	$q->addQuery('file_id');
-	$q->addWhere('file_project = ' . (int)$project_id);
-	$project_files = implode(',', $q->loadColumn());
-	if (!empty($project_files)) {
-		$project_files = 'OR (history_table = \'files\' AND history_item IN (' . $project_files . '))';
-	}
-
-	$filter[] = '((history_table = \'projects\' AND history_item = \'' . (int)$project_id .'\') ' . $project_tasks . ' ' . $project_files . ')';
-}
+global $AppUI, $filter_param;
 
 $page = (int) w2PgetParam($_GET, 'page', 1);
-$limit = (int) w2PgetParam($_GET, 'limit', 100);
-$offset = ($page - 1) * $limit;
-if ($filter_param != '' || $page) {
-	$q = new w2p_Database_Query;
-	$q->addQuery('COUNT(history_id) AS hits');
-	$q->addTable('history', 'h');
-	$q->addTable('users');
-	$q->addWhere('history_user = user_id');
-	$q->addTable('contacts');
-	$q->addWhere('contact_id = user_contact');
-	$q->addWhere($filter);
-	$count = (int) $q->loadResult();
 
-	$q = new w2p_Database_Query;
-	$q->addQuery('history_date as history_datetime, history_id, history_item, history_table, history_description, history_action');
-	$q->addQuery('contact_display_name AS contact_name');
-	$q->addTable('history', 'h');
-	$q->addTable('users');
-	$q->addWhere('history_user = user_id');
-	$q->addTable('contacts');
-	$q->addWhere('contact_id = user_contact');
-	$q->addWhere($filter);
-	$q->addOrder('history_date DESC');
-	$q->setLimit($limit, $offset);
-	$history = $q->loadList();
-} else {
-	$history = array();
-}
+$history = new CHistory();
+$where = (-1 == $filter_param) ? '' : "history_table = '".$filter_param."'";
+$histories = $history->loadAll('history_date DESC', $where);
+$items = array_values($histories);
 
-$pages = (int)($count / $limit) + 1;
-$max_pages = 20;
-if ($pages > $max_pages) {
-	$first_page = max($page - (int)($max_pages / 2), 1);
-	$last_page = min($first_page + $max_pages - 1, $pages);
-} else {
-	$first_page = 1;
-	$last_page = $pages;
-}
-?>
+$xpg_pagesize = w2PgetConfig('page_size', 50);
+$xpg_min = $xpg_pagesize * ($page - 1); // This is where we start our record set from
+// counts total recs from selection
+$xpg_totalrecs = count($items);
+$m .= '&filter='.$filter_param; // This is a hack to get the pagination to work as expected
+$pageNav = buildPaginationNav($AppUI, $m, $tab, $xpg_totalrecs, $xpg_pagesize, $page);
+echo $pageNav;
 
+$fieldList = array('history_date', 'history_description', 'history_user');
+$fieldNames = array('Date', 'Description', 'Owner');
 
-
-<table class="tbl list">
-<tr>
-	<th width="10">&nbsp;</th>
-	<th width="200"><?php echo $AppUI->_('Date'); ?></th>
-	<th nowrap="nowrap"><?php echo $AppUI->_('Description'); ?></th>
-	<th nowrap="nowrap"><?php echo $AppUI->_('User'); ?>&nbsp;&nbsp;</th>
-</tr>
-<?php
-// Checking permissions.
-// TODO: Enable the lines below to activate new permissions.
-$perms = &$AppUI->acl();
-
-$historyItem = new CHistory();
 $htmlHelper = new w2p_Output_HTMLHelper($AppUI);
+$htmlHelper->df .= ' ' . $AppUI->getPref('TIMEFORMAT');
 
-foreach ($history as $row) {
-	$module = $row['history_table'] == 'task_log' ? 'tasks' : $row['history_table'];
-    ?>
+?>
+<table class="tbl list history">
     <tr>
-        <td align="center"><a href='<?php echo '?m=history&a=addedit&history_id=' . $row['history_id'] ?>'><img src="<?php echo w2PfindImage('icons/pencil.gif'); ?>" alt="<?php echo $AppUI->_('Edit History') ?>" border="0" width="12" height="12" /></a></td>
-        <?php echo $htmlHelper->createCell('history_datetime', $row['history_datetime']); ?>
-        <td><?php echo $historyItem->show_history($row) ?></td>
-        <?php echo $htmlHelper->createCell('contact_name-unformatted', $row['contact_name']); ?>
+        <?php
+//TODO: The link below is commented out because this module doesn't support sorting... yet.
+        foreach ($fieldNames as $index => $name) {
+            ?><th nowrap="nowrap">
+<!--                <a href="?m=links&orderby=<?php echo $fieldList[$index]; ?>" class="hdr">-->
+                    <?php echo $AppUI->_($fieldNames[$index]); ?>
+<!--                </a>-->
+            </th><?php
+        }
+        ?>
     </tr>
-    <?php
+<?php
+
+$perms = $AppUI->acl();
+
+for ($i = ($page - 1) * $xpg_pagesize; $i < $page * $xpg_pagesize && $i < $xpg_totalrecs; $i++) {
+    $row = $items[$i];
+    $row['user_id'] = $row['history_user'];
+
+//TODO: we need to make sure sub-modules are handled properly
+    if (!$perms->checkModuleItem($row['history_table'], 'view', $row['history_item'])) {
+        continue;
+    }
+//TODO: do we care about linking when we have a create/update entry?
+    echo '<tr>';
+    $htmlHelper->stageRowData($row);
+    foreach ($fieldList as $index => $column) {
+        echo $htmlHelper->createCell($fieldList[$index], $row[$fieldList[$index]]);
+    }
+    echo '</tr>';
 }
 ?>
 </table>
+<?php
+echo $pageNav;
