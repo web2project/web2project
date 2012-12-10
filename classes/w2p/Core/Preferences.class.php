819e4dffec8d314e06000e1b3cca35955c80ecce
D Keith Casey Jr <keith@blueparabola.com>
2012-12-10 00:53:10 -0600

    tweaking w2p_Core_Preferences to start matching w2p_Core_BaseObject as eventually it should extend the BaseObject;

--- classes/w2p/Core/Preferences.class.php
+++ classes/w2p/Core/Preferences.class.php
@@ -16,11 +16,16 @@ class w2p_Core_Preferences {
 	public $pref_name = null;
 	public $pref_value = null;
 
-	public function __construct() {
+    protected $_query = null;
+
+	public function __construct()
+    {
 		// empty constructor
+        $this->_query = new w2p_Database_Query;
 	}
 
-	public function bind($hash) {
+	public function bind($hash)
+    {
 		if (!is_array($hash)) {
 			return 'w2p_Core_Preferences::bind failed';
 		} else {
@@ -30,17 +35,21 @@ class w2p_Core_Preferences {
 		}
 	}
 
-	public function check() {
-		// TODO MORE
-		return null; // object is ok
+    public function isValid()
+    {
+        return true;
+    }
+
+	public function check()
+    {
+		return array();
 	}
 
-	public function store() {
-		$msg = $this->check();
-		if ($msg) {
-			return 'CPreference::store-check failed ' . $msg;
-		}
-		if (($msg = $this->delete())) {
+	public function store()
+    {
+		$q = $this->_getQuery();
+
+        if (($msg = $this->delete())) {
 			return 'CPreference::store-delete failed ' . $msg;
 		}
 		$q = new w2p_Database_Query;
@@ -51,8 +60,10 @@ class w2p_Core_Preferences {
 		}
 	}
 
-	public function delete() {
-		$q = new w2p_Database_Query;
+	public function delete()
+    {
+		$q = $this->_getQuery();
+
 		$q->setDelete('user_preferences');
 		$q->addWhere('pref_user = ' . (int)$this->pref_user);
 		$q->addWhere('pref_name = \'' . $this->pref_name . '\'');
@@ -62,4 +73,19 @@ class w2p_Core_Preferences {
 			return null;
 		}
 	}
+
+    /**
+     * Returns a clean query object
+     *
+     * Clears out the query and then returns it for use
+     *
+     * @access protected
+     *
+     * @return w2p_Database_Query Clean query object
+     */
+    protected function _getQuery()
+    {
+        $this->_query->clear();
+        return $this->_query;
+    }
 }
