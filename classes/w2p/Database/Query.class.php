35f1259cc9ee43760ef3ba86f3149099c7f4ac0d
D Keith Casey Jr <keith@blueparabola.com>
2013-01-05 00:16:42 -0600

    updated as noted.

--- classes/w2p/Database/Query.class.php
+++ classes/w2p/Database/Query.class.php
@@ -1,15 +1,30 @@
-<?php /* $Id$ $URL$ */
-
+<?php
 /**
- *	@package    web2project
- *	@subpackage database
- *	@version    $Revision$
- *  @license	Clear BSD
+{{{ Copyright 2003,2004 Adam Donnison <adam@saki.com.au>
+
+    This file is part of the collected works of Adam Donnison.
+
+    This is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}}}
  *
- * Note: Previously, this class was mis-licensed as GPL in an otherwise BSD
- *   application. The GPL attempt was in 2003 while the project itself was not
- *   relicensed from BSD to GPL in 2005. In 2007, all further development was
- *   done under the Clear BSD license and all GPL modifications were removed.
+ *  @package    web2project
+ *	@subpackage database
+ *
+ *  Note: Previously, this class may have been erroneously re-licensed by
+ *    Keith Casey (caseydk). Its original copyright statement by Adam Donnison
+ *    is being restored until this can be clarified and/or corrected.
  */
 
 /**
