dd4b82d8f365f734fe6d32684a9ce8573768f0ae
Carlos Azevedo <carlos.azevedo@ctr.pt>
2013-03-27 16:31:46 +0000

    Cosmetic fixes to make the forms look better.

--- modules/tasks/ae_depend.php
+++ modules/tasks/ae_depend.php
@@ -64,61 +64,58 @@ $q->clear();
     <table width="100%" border="0" cellpadding="4" cellspacing="0" class="std addedit">
         <?php if ($can_edit_time_information) { ?>
         <tr>
-            <td align="center" nowrap="nowrap" colspan="3"><b><?php echo $AppUI->_('Dependency Tracking'); ?></b></td>
-        </tr>
-        <tr>
-            <td align="right" nowrap="nowrap"><?php echo $AppUI->_('On'); ?></td>
-            <td nowrap="nowrap">
-                <input type="radio" name="task_dynamic" value="31" <?php if ($task_id == 0 || $task->task_dynamic > '20') { echo "checked"; } ?> />
-            </td>
-        </tr>
-        <tr>
-            <td align="right" nowrap="nowrap"><?php echo $AppUI->_('Off'); ?></td>
-            <td id="no_dyn" nowrap="nowrap">
-                <input type="radio" name="task_dynamic" value="0" <?php if ($task_id && ($task->task_dynamic == '0' || $task->task_dynamic == '11')) { echo "checked"; } ?> />
-            </td>
-        </tr>
-        <tr>
-            <td align="right" nowrap="nowrap"><label for="task_dynamic"><?php echo $AppUI->_('Dynamic Task'); ?></label></td>
-            <td nowrap="nowrap">
-                <input type="checkbox" name="task_dynamic" id="task_dynamic" value="1" <?php if ($task->task_dynamic == "1") { echo 'checked="checked"'; } ?> />
-            </td>
-        </tr>
-        <tr>
-            <td align="right" nowrap="nowrap"><label for="task_dynamic_nodelay"><?php echo $AppUI->_('Do not track this task'); ?></label></td>
-            <td>
-                <input type="checkbox" name="task_dynamic_nodelay" id="task_dynamic_nodelay" value="1" <?php if (($task->task_dynamic > '10') && ($task->task_dynamic < 30)) { echo 'checked="checked"'; } ?> />
-            </td>
-        </tr>
+	    <td width="50%">&nbsp;</td>
+	    <td nowrap>
+		<table><tr><td rowspan="2">
+		<?php echo $AppUI->_('Dependency Tracking'); ?>:
+		</td><td>
+		<input type="radio" name="task_dynamic" value="31" id="td_on" <?php if ($task_id == 0 || $task->task_dynamic > '20') { echo "checked"; } ?> />
+		<label for="td_on"><?php echo $AppUI->_('On'); ?></label>
+		</td></tr><tr><td>
+		<input type="radio" name="task_dynamic" value="0" id="td_off" <?php if ($task_id && ($task->task_dynamic == '0' || $task->task_dynamic == '11')) { echo "checked"; } ?> />
+		<label for="td_off"><?php echo $AppUI->_('Off'); ?></label>
+		</td></tr></table>
+	    </td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	    </td><td nowrap>
+		<input type="checkbox" name="task_dynamic" id="task_dynamic" value="1" <?php if ($task->task_dynamic == "1") { echo 'checked="checked"'; } ?> />
+		<label for="task_dynamic"><?php echo $AppUI->_('Dynamic Task'); ?>:</label><br>
+		<input type="checkbox" name="task_dynamic_nodelay" id="task_dynamic_nodelay" value="1" <?php if (($task->task_dynamic > '10') && ($task->task_dynamic < 30)) { echo 'checked="checked"'; } ?> />
+		<label for="task_dynamic_nodelay"><?php echo $AppUI->_('Do not track this task'); ?></label><br>
+		<input type="checkbox" name="set_task_start_date" id="set_task_start_date" <?php if ($task_id == 0 || $task->task_dynamic > '20') { echo "checked"; } ?> />
+		<label for="set_task_start_date"><?php echo $AppUI->_('Set task start date based on dependency'); ?></label>
+	    </td>
+	    <td width="50%">&nbsp;</td>
+	</tr>
         <?php } else { ?>
-        <tr>
-            <td colspan="2"><?php echo $AppUI->_('Only the task owner, project owner, or system administrator is able to edit time related information.'); ?></td>
-        </tr>
+	<tr>
+	    <td colspan="5" align="center"><?php echo $AppUI->_('Only the task owner, project owner, or system administrator is able to edit time related information.'); ?></td>
+	</tr>
         <?php } // end of can_edit_time_information ?>
 
-        <tr>
-            <td><?php echo $AppUI->_('All Tasks'); ?>:</td>
-            <td><?php echo $AppUI->_('Task Dependencies'); ?>:</td>
-        </tr>
-        <tr>
-            <td>
-                <select name="all_tasks" class="text" style="width:220px" size="10" class="text" multiple="multiple">
-                    <?php echo str_replace('selected', '', $task_parent_options); // we need to remove selected added from task_parent options ?>
-                </select>
-            </td>
-            <td>
-                <?php echo arraySelect($taskDep, 'task_dependencies', 'style="width:220px" size="10" class="text" multiple="multiple" ', null); ?>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-            <input type="checkbox" name="set_task_start_date" id="set_task_start_date" <?php if ($task_id == 0 || $task->task_dynamic > '20') { echo "checked"; } ?>  /><label for="set_task_start_date"><?php echo $AppUI->_('Set task start date based on dependency'); ?></label>
-            </td>
-        </tr>
-        <tr>
-            <td align="right"><input type="button" class="button" value="&gt;" onclick="addTaskDependency(document.dependFrm, document.datesFrm)" /></td>
-            <td align="left"><input type="button" class="button" value="&lt;" onclick="removeTaskDependency(document.dependFrm, document.datesFrm)" /></td>
-        </tr>
+	<tr>
+	    <td width="50%">&nbsp;</td>
+	    <td><?php echo $AppUI->_('All Tasks'); ?>:</td>
+	    <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+	    <td><?php echo $AppUI->_('Task Dependencies'); ?>:</td>
+	    <td width="50%">&nbsp;</td>
+	</tr><tr>
+	    <td width="50%">&nbsp;</td>
+	    <td>
+		<select name="all_tasks" class="text" style="width:220px" size="10" class="text" multiple="multiple">
+		<?php echo str_replace('selected', '', $task_parent_options); // we need to remove selected added from task_parent options ?>
+		</select>
+	    </td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	    </td><td>
+		<?php echo arraySelect($taskDep, 'task_dependencies', 'style="width:220px" size="10" class="text" multiple="multiple" ', null); ?>
+	   </td>
+	    <td width="50%">&nbsp;</td>
+	</tr><tr>
+	    <td width="50%">&nbsp;</td>
+	    <td align="right"><input type="button" class="button" value="&gt;" onclick="addTaskDependency(document.dependFrm, document.datesFrm)" /></td>
+	    <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+	    <td align="left"><input type="button" class="button" value="&lt;" onclick="removeTaskDependency(document.dependFrm, document.datesFrm)" /></td>
+	    <td width="50%">&nbsp;</td>
+	</tr>
     </table>
 </form>
 <script language="javascript" type="text/javascript">
--- modules/tasks/ae_desc.php
+++ modules/tasks/ae_desc.php
@@ -19,52 +19,76 @@ $perms = &$AppUI->acl();
     <input type="hidden" name="task_id" value="<?php echo $task_id; ?>" />
     <table class="std addedit" width="100%" border="1" cellpadding="4" cellspacing="0">
         <tr>
-            <td width="50%" valign='top'>
+            <td width="30%" valign="top">
                 <table border="0">
                     <tr>
-                        <td>
-                            <?php if ($can_edit_time_information) { ?>
-                                <?php echo $AppUI->_('Task Owner'); ?><br />
+                        <?php if ($can_edit_time_information) { ?>
+	                    <td align="right">
+                                <?php echo $AppUI->_('Task Owner'); ?>:
+			    </td><td>
                                 <?php echo arraySelect($users, 'task_owner', 'class="text"', !isset($task->task_owner) ? $AppUI->user_id : $task->task_owner); ?>
-                                <br />
-                            <?php } ?>
-                            <?php echo $AppUI->_('Access'); ?>
-                            <br />
-                            <?php if(is_array($task_access)) { ?>
-                                <?php echo arraySelect($task_access, 'task_access', 'class="text"', (int) $task->task_access, true); ?>
-                            <?php } else { ?>
-                                <input name="task_access" type="hidden" value="<?php echo (int) $task->task_access; ?>" />
-                            <?php } ?>
-                            <br /><?php echo $AppUI->_('Web Address'); ?>
-                            <br /><input type="text" class="text" name="task_related_url" value="<?php echo $task->task_related_url; ?>" size="40" maxlength="255" />
-                        </td>
-                        <td valign='top'>
-                            <?php echo $AppUI->_('Task Type'); ?><br />
+			    </td>
+                        <?php } ?>
+		    </tr><tr>
+			<td align="right">
+                            <?php echo $AppUI->_('Task Type'); ?>:
+			</td><td>
                             <?php
                                 $task_types = w2PgetSysVal('TaskType');
                                 echo arraySelect($task_types, 'task_type', 'class="text"', $task->task_type, false);
                             ?>
-                            <br /><br />
+			</td>
+		    </tr><tr>
+                        <?php if(is_array($task_access)) { ?>
+			    <td align="right">
+	                        <?php echo $AppUI->_('Access'); ?>:
+			    </td><td>
+                                <?php echo arraySelect($task_access, 'task_access', 'class="text"', (int) $task->task_access, true); ?>
+			    </td>
+                        <?php } else { ?>
+                            <input name="task_access" type="hidden" value="<?php echo (int) $task->task_access; ?>" />
+                        <?php } ?>
+		    </tr><tr>
+			<td align="right">
+			    <?php echo $AppUI->_('Web Address'); ?>:
+			</td><td>
+                            <input type="text" class="text" name="task_related_url" value="<?php echo $task->task_related_url; ?>" size="40" maxlength="255" />
+			</td>
+		    </tr><tr>
+			<td align="right">
+                            <?php echo $AppUI->_('Task Parent'); ?>:
+			</td><td>
+	                    <select name='task_parent' class='text'>
+        	                <option value='<?php echo $task->task_id; ?>'><?php echo $AppUI->_('None'); ?></option>
+                	        <?php echo $task_parent_options; ?>
+	                    </select>
+			</td>
+		    </tr><tr>
+	                <?php if ($task_id > 0) { ?>
+			    <td align="right">
+	                        <?php echo $AppUI->_('Move this task (and its children), to project'); ?>:
+			    </td><td>
+	                        <?php echo arraySelect($projects, 'new_task_project', 'size="1" class="text" id="medium" onchange="submitIt(document.editFrm)"', $task_project); ?>
+			    </td>
+			<?php } ?>
+		    </tr>
+		</table>
+   	    </td><td align="center">
+                <?php
+                    if ($AppUI->isActiveModule('contacts') && canView('contacts')) {
+                        echo '<input type="button" class="button" value="' . $AppUI->_('Select contacts...') . '" onclick="javascript:popContacts();" />';
+                    }
+                    // Let's check if the actual company has departments registered
+                    if (count($department_selection_list) > 1) { ?><br />
+                        <?php echo $AppUI->_('Department'); ?><br />
+                        <?php echo arraySelect($department_selection_list, 'dept_ids[]', 'class="text" size="1"', $task->task_departments); ?>
+                    <?php } ?>
+		    <br><br>
+                    <table>
+			<tr><td colspan="2" align="center">
+                       	    <?php echo $AppUI->_('Target Budgets'); ?>:
+			</td></tr>
                             <?php
-                            if ($AppUI->isActiveModule('contacts') && canView('contacts')) {
-                                echo '<input type="button" class="button" value="' . $AppUI->_('Select contacts...') . '" onclick="javascript:popContacts();" />';
-                            }
-                            // Let's check if the actual company has departments registered
-                            if (count($department_selection_list) > 1) { ?><br />
-                                <?php echo $AppUI->_('Department'); ?><br />
-                                <?php echo arraySelect($department_selection_list, 'dept_ids[]', 'class="text" size="1"', $task->task_departments); ?>
-                            <?php } ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><?php echo $AppUI->_('Task Parent'); ?>:</td>
-                        <td rowspan="4">
-                            <table>
-                                <tr>
-                                    <td></td>
-                                    <td><?php echo $AppUI->_('Target Budgets'); ?>:</td>
-                                </tr>
-                                <?php
                                 $billingCategory = w2PgetSysVal('BudgetCategory');
                                 $totalBudget = 0;
                                 foreach ($billingCategory as $id => $category) {
@@ -72,47 +96,23 @@ $perms = &$AppUI->acl();
                                     $totalBudget += $amount;
                                     ?>
                                     <tr>
-                                        <td align="right" nowrap="nowrap"><?php echo $AppUI->_($category); ?></td>
+                                        <td align="right" nowrap="nowrap"><?php echo $AppUI->_($category); ?>:</td>
                                         <td nowrap="nowrap" style="text-align: left; padding-left: 40px;">
-                                            <?php echo $w2Pconfig['currency_symbol'] ?> <input name="budget_<?php echo $id; ?>" id="budget_<?php echo $id; ?>" type="text" value="<?php echo $amount; ?>" size="10" class="text" />
+                                        <?php echo $w2Pconfig['currency_symbol'] ?> <input name="budget_<?php echo $id; ?>" id="budget_<?php echo $id; ?>" type="text" value="<?php echo $amount; ?>" size="10" class="text" />
                                         </td>
                                     </tr>
                                     <?php
                                 }
-                                ?>
-                                <tr>
-                                    <td nowrap="nowrap"><?php echo $AppUI->_('Total Target Budget'); ?> <?php echo $w2Pconfig['currency_symbol'] ?></td>
-                                    <td align="right">
-                                        <?php echo $w2Pconfig['currency_symbol'] ?> <?php echo formatCurrency($totalBudget, $AppUI->getPref('CURRENCYFORM')); ?>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                <tr>
-                    <td>
-                        <select name='task_parent' class='text'>
-                            <option value='<?php echo $task->task_id; ?>'><?php echo $AppUI->_('None'); ?></option>
-                            <?php echo $task_parent_options; ?>
-                        </select>
-                    </td>
-<!--                    <td><?php echo $w2Pconfig['currency_symbol'] ?><input type="text" class="text" name="task_target_budget" value="<?php echo $task->task_target_budget; ?>" size="10" maxlength="10" /></td>-->
-                </tr>
-            <?php if ($task_id > 0) { ?>
-                <tr>
-                    <td>
-                        <?php echo $AppUI->_('Move this task (and its children), to project'); ?>:
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <?php echo arraySelect($projects, 'new_task_project', 'size="1" class="text" id="medium" onchange="submitIt(document.editFrm)"', $task_project); ?>
-                    </td>
-                </tr>
-            <?php } ?>
-                </table>
-            </td>
-            <td valign="top" align="center">
+                            ?>
+                        <tr>
+                            <td nowrap="nowrap"><?php echo $AppUI->_('Total Target Budget'); ?>:</td>
+                            <td align="right">
+                            <?php echo $w2Pconfig['currency_symbol'] ?> <?php echo formatCurrency($totalBudget, $AppUI->getPref('CURRENCYFORM')); ?>
+                            </td>
+                        </tr>
+<!--                    <tr><td><?php echo $w2Pconfig['currency_symbol'] ?><input type="text" class="text" name="task_target_budget" value="<?php echo $task->task_target_budget; ?>" size="10" maxlength="10" /></td></tr>-->
+                    </table>
+	    </td><td valign="top" align="center">
                 <table><tr><td align="left">
                 <?php echo $AppUI->_('Description'); ?>:
                 <br />
