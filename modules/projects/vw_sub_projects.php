0c81e053bf6f0e93b8280788db675cb47e4c6dc9
D Keith Casey Jr <keith@caseysoftware.com>
2013-03-31 18:30:41 -0500

    stripping out all the hardcoded styles; simplifying the css for the subproject view

--- modules/projects/vw_sub_projects.php
+++ modules/projects/vw_sub_projects.php
@@ -4,7 +4,6 @@ if (!defined('W2P_BASE_DIR')) {
 }
 global $project;
 
-$df = $AppUI->getPref('SHDATEFORMAT');
 $projectPriority = w2PgetSysVal('ProjectPriority');
 $projectStatus = w2PgetSysVal('ProjectStatus');
 
@@ -12,19 +11,23 @@ $original_project_id = $project->project_original_parent;
 $project->project_status = -1;
 $st_projects_arr = $project->getStructuredProjects();
 ?>
-<table width="100%" border="0" cellpadding="5" cellspacing="1" bgcolor="black">
-<tr>
-<th width="12">&nbsp;</th>
-<th class="hilite" width="12"><?php echo $AppUI->_('ID'); ?></th>
-<th><?php echo $AppUI->_('Project'); ?></th>
-<th><?php echo $AppUI->_('Company'); ?></th>
-<th><?php echo $AppUI->_('Start'); ?></th>
-<th><?php echo $AppUI->_('End'); ?></th>
-<th><?php echo $AppUI->_('P'); ?></th>
-<th><?php echo $AppUI->_('Status'); ?></th>
-</tr>
+<table cellpadding="5" cellspacing="1" class="list subprojects">
+    <tr>
+        <th width="12">&nbsp;</th>
+        <th class="hilite" width="12"><?php echo $AppUI->_('ID'); ?></th>
+        <th><?php echo $AppUI->_('Project'); ?></th>
+        <th><?php echo $AppUI->_('Company'); ?></th>
+        <th><?php echo $AppUI->_('Start'); ?></th>
+        <th><?php echo $AppUI->_('End'); ?></th>
+        <th><?php echo $AppUI->_('P'); ?></th>
+        <th><?php echo $AppUI->_('Status'); ?></th>
+    </tr>
 <?php
 $s = '';
+
+$htmlHelper = new w2p_Output_HTMLHelper($AppUI);
+$customLookups = array('project_status' => $projectStatus, 'project_priority' => $projectPriority);
+
 if (is_array($st_projects_arr)) {
     foreach ($st_projects_arr as $project) {
         $line = $project[0];
@@ -32,26 +35,25 @@ if (is_array($st_projects_arr)) {
         if ($line['project_id']) {
             $s_project = new CProject();
             $s_project->loadFull(null, $line['project_id']);
-            $start_date = intval($s_project->project_start_date) ? new w2p_Utilities_Date($s_project->project_start_date) : null;
-            $end_date = intval($s_project->project_end_date) ? new w2p_Utilities_Date($s_project->project_end_date) : null;
-            $actual_end_date = intval($s_project->project_actual_end_date) ? new w2p_Utilities_Date($s_project->project_actual_end_date) : null;
-            $style = (($actual_end_date > $end_date) && !empty($end_date)) ? 'style="color:red; font-weight:bold"' : '';
-            $x++;
-            $row_class = ($x % 2) ? 'style="background:#fff;"' : 'style="background:#f0f0f0;"';
-            $row_classr = ($x % 2) ? 'style="background:#fff;text-align:right;"' : 'style="background:#f0f0f0;text-align:right;"';
-            $s .= '<tr><td ' . $row_class . ' align="center"><a href="./index.php?m=projects&a=addedit&project_id=' . $s_project->project_id . '"><img src="' . w2PfindImage('icons/' . ($project_id == $s_project->project_id ? 'pin' : 'pencil') . '.gif') . '" border="0" alt="" /></b></a></td>';
-            $s .= '<td ' . $row_classr . ' nowrap="nowrap">' . $s_project->project_id . '</td>';
+
+            $row = get_object_vars($s_project);
+            $row['company_id'] = $row['project_company'];
+            $htmlHelper->stageRowData($row);
+
+            $s .= '<tr><td><a href="./index.php?m=projects&a=addedit&project_id=' . $s_project->project_id . '"><img src="' . w2PfindImage('icons/' . ($project_id == $s_project->project_id ? 'pin' : 'pencil') . '.gif') . '" border="0" alt="" /></a></td>';
+            $s .= '<td>' . $s_project->project_id . '</td>';
             if ($level) {
                 $sd = str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', ($level - 1)) . w2PshowImage('corner-dots.gif', 16, 12) . '&nbsp;' . '<a href="./index.php?m=projects&a=view&project_id=' . $s_project->project_id . '">' . $s_project->project_name . '</a>';
             } else {
                 $sd = '<a href="./index.php?m=projects&a=view&project_id=' . $s_project->project_id . '">' . $s_project->project_name . '</a>';
             }
-            $s .= '<td ' . $row_class . '>' . $sd . '</td>';
-            $s .= '<td ' . $row_class . '><a href="./index.php?m=companies&a=view&company_id=' . $s_project->project_company . '">' . $s_project->company_name . '</a></td>';
-            $s .= '<td ' . $row_class . ' align="center">' . ($start_date ? $start_date->format($df) : '-') . '</td>';
-            $s .= '<td ' . $row_class . ' align="center">' . ($end_date ? $end_date->format($df) : '-') . '</td>';
-            $s .= '<td ' . $row_class . ' align="center">' . $projectPriority[$s_project->project_priority] . '</td>';
-            $s .= '<td ' . $row_class . ' align="center">' . $projectStatus[$s_project->project_status] . '</td></tr>';
+            $s .= '<td class="data _name">' . $sd . '</td>';
+            $s .= $htmlHelper->createCell('company_name', $s_project->company_name);
+            $s .= $htmlHelper->createCell('project_start_date', $s_project->project_start_date);
+            $s .= $htmlHelper->createCell('project_end_date', $s_project->project_end_date);
+            $s .= $htmlHelper->createCell('project_priority', $s_project->project_priority, $customLookups);
+            $s .= $htmlHelper->createCell('project_status', $s_project->project_status, $customLookups);
+            $s .= '</tr>';
         }
     }
 }
