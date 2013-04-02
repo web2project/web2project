45ebf4cbc5410d632937e66173d262f3fbbf90b6
Jörg Walter <jwalt@queen.(none)>
2013-04-02 10:20:50 +0200

    disable forums PDF download, as a "@media print" stylesheet produces better results

--- modules/forums/view_messages.php
+++ modules/forums/view_messages.php
@@ -86,7 +86,7 @@ if (function_exists('styleRenderBoxTop')) {
                     $titleBlock = new w2p_Theme_TitleBlock('', '', $m, "$m.$a");
                     $titleBlock->addCrumb('?m=forums', 'forums list');
                     $titleBlock->addCrumb('?m=forums&a=viewer&forum_id=' . $forum_id, 'topics for this forum');
-                    $titleBlock->addCrumb('?m=forums&a=view_pdf&forum_id=' . $forum_id . '&message_id=' . $message_id . '&sort=' . $sort . '&suppressHeaders=1', 'view PDF file');
+//                    $titleBlock->addCrumb('?m=forums&a=view_pdf&forum_id=' . $forum_id . '&message_id=' . $message_id . '&sort=' . $sort . '&suppressHeaders=1', 'view PDF file');
                     $titleBlock->show();
                 ?>
             </td>
