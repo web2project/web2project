e2d26b368adee92ff26dd0a949d2b3e0ff04574f
Jörg Walter <jwalt@queen.(none)>
2013-04-06 01:26:01 +0200

    update password strength meter for modern requirements; better bad-password rejection

--- js/passwordstrength.js
+++ js/passwordstrength.js
@@ -1,44 +1,47 @@
-var commonPasswords = new Array('password', 'pass', '1234', '1246', 'test',
-                                'qwerty', '123456', 'q1w2e3r4', 'password1');
+// taken from a public list of most common passwords
+var commonPasswords = new Array('pass', 'word', '123', 'test', 'abc', 'azert', 'monkey', 'dragon',
+                                'baseball', 'iloveyou', 'trustno', 'sunshine', 'master',
+                                'welcome', 'shadow', 'football', 'jesus', 'ninja', 'mustang',
+                                'qwert', 'abc', 'letmein', 'q1w2e3', 'asdf', 'zxcv');
 
-var numbers = "0123456789";
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var punctuation = "!.@$£#*()%~<>{}[]";
+function checkPassword(password, user) {
 
-function checkPassword(password) {
+	// strip common parts so they don't take part in the calculation
+    for (var i = 0; i < commonPasswords.length; i++) {
+	    password = stripcommon(password, commonPasswords[i]);
+    }
+	password = stripcommon(password, user);
 
     var combinations = 0;
 
-    if (contains(password, numbers) > 0) {
+    if (/[0-9]/.test(password)) {
         combinations += 10;
     }
 
-    if (contains(password, lowercase) > 0) {
+    if (/[a-z]/.test(password)) {
         combinations += 26;
     }
 
-    if (contains(password, uppercase) > 0) {
+    if (/[A-Z]/.test(password)) {
         combinations += 26;
     }
 
-    if (contains(password, punctuation) > 0) {
-        combinations += punctuation.length;
+    if (/[ -/:-@[-`{-~]/.test(password)) {
+        combinations += 33;
+    }
+
+    if (/[^\x00-\x7f]/.test(password)) {
+        combinations += 128;
     }
 
     // work out the total combinations
     var totalCombinations = Math.pow(combinations, password.length);
 
-    // if the password is a common password, then everthing changes...
-    if (isCommonPassword(password)) {
-        totalCombinations = 75000 // about the size of the dictionary
-    }
-
-    // work out how long it would take to crack this (@ 200 attempts per second)
-    var timeInSeconds = (totalCombinations / 200) / 2;
+    // work out how long it would take to crack this (@ 1M attempts per second)
+    var timeInSeconds = totalCombinations / 1000000;
 
     // this is how many days? (there are 86,400 seconds in a day.
-    var timeInDays = timeInSeconds / 86400
+    var timeInDays = timeInSeconds / 86400;
 
     // how long we want it to last
     var lifetime = 365;
@@ -47,6 +50,7 @@ function checkPassword(password) {
     var percentage = timeInDays / lifetime;
 
     var friendlyPercentage = cap(Math.round(percentage * 100), 100);
+
     if (totalCombinations != 75000 && friendlyPercentage < (password.length * 5)) {
         friendlyPercentage += password.length * 5;
     }
@@ -91,26 +95,11 @@ function cap(number, max) {
     }
 }
 
-function isCommonPassword(password) {
-    for (var i = 0; i < commonPasswords.length; i++) {
-        var commonPassword = commonPasswords[i];
-        if (password == commonPassword) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-function contains(password, validChars) {
-    var count = 0;
-
-    for (var i = 0; i < password.length; i++) {
-        var chr = password.charAt(i);
-        if (validChars.indexOf(chr) > -1) {
-            count++;
-        }
-    }
-
-    return count;
+function stripcommon(password, str, reverse) {
+	var index = password.indexOf(str);
+	if (index >= 0) {
+		password = password.substr(0, index) + password.substr(index+str.length);
+	}
+	if (!reverse) return stripcommon(password, str.split(/(?:)/).reverse().join(""), true);
+	return password;
 }
