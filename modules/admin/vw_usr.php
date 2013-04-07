79e6cf352b3c7de95e8e429e76eafc4365d6ddc2
D Keith Casey Jr <keith@caseysoftware.com>
2013-04-07 17:49:16 -0500

    removed the User Type because it *looks* the same as User Role but at the same time doesn't actually do anything; resolves the oddity from http://bugs.web2project.net/view.php?id=1299

--- modules/admin/vw_usr.php
+++ modules/admin/vw_usr.php
@@ -18,9 +18,8 @@ if (count($fields) > 0) {
     //   state for versions earlier than v3.0
     //   At some point at/after v4.0, this should be deprecated
     $fieldList = array('contact_display_name', 'user_username',
-        'user_type', 'company_name', 'dept_name');
-    $fieldNames = array('Real Name', 'Login Name', 'Type',
-        'Company', 'Department');
+        'company_name', 'dept_name');
+    $fieldNames = array('Real Name', 'Login Name', 'Company', 'Department');
 //TODO: This doesn't save the columns yet as we can't allow customization yet.
 }
 if ($display_last_login) {
@@ -109,7 +108,7 @@ foreach ($users as $row) {
 	</td>
     <?php
         echo $htmlHelper->createCell('user_name', $row['user_username']);
-        echo $htmlHelper->createCell('user_type', $row['user_type'], $customLookups);
+        //echo $htmlHelper->createCell('user_type', $row['user_type'], $customLookups);
         echo $htmlHelper->createCell('contact_company', $row['contact_company']);
         echo $htmlHelper->createCell('dept_name', $row['dept_name']);
     ?>
