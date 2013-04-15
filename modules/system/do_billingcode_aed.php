397a76f1a2d168eb6d869b94cc9b890ccd4cfe56
D Keith Casey Jr <keith@caseysoftware.com>
2013-04-15 01:53:38 -0500

    updated to use the new controller

--- modules/system/do_billingcode_aed.php
+++ modules/system/do_billingcode_aed.php
@@ -3,25 +3,12 @@ if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
 
-$del = (int) w2PgetParam($_POST, 'del', 0);
+$delete = (int) w2PgetParam($_POST, 'del', 0);
 
-$obj = new CSystem_Bcode();
-if (!$obj->bind($_POST)) {
-    $AppUI->setMsg($obj->getError(), UI_MSG_ERROR);
-    $AppUI->redirect();
-}
+$controller = new w2p_Controllers_Base(
+                    new CSystem_Bcode(), $delete, 'Billing Codes', 
+                    'm=system&a=billingcode', 'm=system&a=billingcode'
+                  );
 
-$action = ($del) ? 'deleted' : 'stored';
-$result = ($del) ? $obj->delete() : $obj->store();
-
-if (is_array($result)) {
-    $AppUI->setMsg($result, UI_MSG_ERROR, true);
-    $AppUI->holdObject($obj);
-    $AppUI->redirect('m=system&a=billingcode');
-}
-if ($result) {
-    $AppUI->setMsg('Billing Codes '.$action, UI_MSG_OK, true);
-    $AppUI->redirect('m=system&a=billingcode');
-} else {
-    $AppUI->redirect(ACCESS_DENIED);
-}
+$AppUI = $controller->process($AppUI, $_POST);
+$AppUI->redirect($controller->resultPath);
