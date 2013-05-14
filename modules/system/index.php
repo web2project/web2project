f7c72ffa2b7fc1950eeb055d2b8e9956b5183855
D Keith Casey Jr <keith@caseysoftware.com>
2013-05-14 11:27:16 -0700

    on smaller screens, missing this br was making the entire block wrap as opposed to just this text

--- modules/system/index.php
+++ modules/system/index.php
@@ -32,7 +32,7 @@ $titleBlock->show();
             echo '<br />';
             if ($system->upgradeRequired()) {
               ?>
-              <a href="?m=system&u=upgrade"><?php echo $AppUI->_('Apply System Updates'); ?></a> -
+              <a href="?m=system&u=upgrade"><?php echo $AppUI->_('Apply System Updates'); ?></a> -<br />
               <span class="error"><?php echo $AppUI->_('Your upgrade is not complete. Please apply the updates immediately.'); ?></span>
               <?php
             } else {
