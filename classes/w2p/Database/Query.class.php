a6e143acf8c51375b89758b5f0f61b5690ebb6f5
D Keith Casey Jr <keith@caseysoftware.com>
2013-05-27 16:18:19 -0500

    breaking 'order by' into its own method

--- classes/w2p/Database/Query.class.php
+++ classes/w2p/Database/Query.class.php
@@ -91,6 +91,7 @@ class w2p_Database_Query extends w2p_Database_oldQuery
         $where = $this->_buildWhere();
         $joins = $this->_buildJoins();
         $limit = $this->_buildLimit();
+        $order = $this->_buildOrder();
 
         $fields = count($this->_fields) ? implode(',' , $this->_fields) : '*';
         $aliases = array();
@@ -100,9 +101,8 @@ class w2p_Database_Query extends w2p_Database_oldQuery
         $tables = implode(',', $aliases);
 
         $group_by = count($this->_group_by) ? 'GROUP BY ' . implode(',' , $this->_group_by) : '';
-        $order_by = count($this->_order_by) ? 'ORDER BY ' . implode(',' , $this->_order_by) : '';
 
-        $sql = "SELECT $fields FROM ($tables) $joins $where $group_by $order_by $limit";
+        $sql = "SELECT $fields FROM ($tables) $joins $where $group_by $order $limit";
 
         return $sql;
 	}
@@ -214,5 +214,15 @@ class w2p_Database_Query extends w2p_Database_oldQuery
         }
 
         return $limit;
+    }
+
+    protected function _buildOrder()
+    {
+        $order = '';
+        if(count($this->_order_by)) {
+            $order = 'ORDER BY ' . implode(',' , $this->_order_by);
+        }
+
+        return $order;
     }
 }
