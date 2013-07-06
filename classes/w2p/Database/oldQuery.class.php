bfb4001088057112714065e5dde18a013ce9270a
D Keith Casey Jr <keith@caseysoftware.com>
2013-07-06 01:25:19 -0500

    delete prepareInsert

--- classes/w2p/Database/oldQuery.class.php
+++ classes/w2p/Database/oldQuery.class.php
@@ -553,31 +553,6 @@ class w2p_Database_oldQuery {
 
 	/** Prepare the INSERT component of the SQL query
 	 */
-	public function prepareInsert() {
-        $q = 'INSERT INTO ';
-        if (isset($this->table_list)) {
-            if (is_array($this->table_list)) {
-                reset($this->table_list);
-                // Grab the first record
-                list($key, $table) = each($this->table_list);
-            } else {
-                $table = $this->table_list;
-            }
-        } else {
-            return false;
-        }
-        $q .= $this->quote_db($this->_table_prefix . $table);
-
-        $quotedFieldnames = array_map(array($this, 'quote_db'), array_keys($this->value_list));
-        $fieldlist = implode(',', $quotedFieldnames);
-        $valuelist = implode(',', $this->value_list);
-
-        $q .= '(' . $fieldlist . ') VALUES (' . $valuelist . ')';
-        return $q;
-	}
-
-	/** Prepare the INSERT component of the SQL query
-	 */
 	public function prepareInsertSelect() {
 		$q = 'INSERT INTO ';
 		if (isset($this->table_list)) {
