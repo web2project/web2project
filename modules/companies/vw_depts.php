86a5e553c607b09e3ff4d3df2c1ebd290a4a1fbc
D Keith Casey Jr <keith@caseysoftware.com>
2013-07-31 01:50:35 -0500

    collapsed the dept list to use the list helper

--- modules/companies/vw_depts.php
+++ modules/companies/vw_depts.php
@@ -3,57 +3,26 @@ if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
 
-##
-##	Companies: View Departments sub-table
-##
-
 global $AppUI, $company_id, $canEdit;
 
-$depts = CCompany::getDepartments($AppUI, $company_id);
-
-$fieldList = array();
-$fieldNames = array();
+$items = CCompany::getDepartments($AppUI, $company_id);
 
 $module = new w2p_Core_Module();
 $fields = $module->loadSettings('departments', 'company_view');
-if (count($fields) > 0) {
-    $fieldList = array_keys($fields);
-    $fieldNames = array_values($fields);
-} else {
-    // TODO: This is only in place to provide an pre-upgrade-safe
-    //   state for versions earlier than v3.0
-    //   At some point at/after v4.0, this should be deprecated
+
+if (0 == count($fields)) {
     $fieldList = array('dept_name', 'dept_users');
     $fieldNames = array('Name', 'Users');
 
     $module->storeSettings('departments', 'company_view', $fieldList, $fieldNames);
+
+    $fields = array_combine($fieldList, $fieldNames);
 }
 ?>
 <a name="departments-company_view"> </a>
-<table class="tbl list">
-    <tr>
-        <?php foreach ($fieldNames as $index => $name) { ?>
-            <th><?php echo $AppUI->_($fieldNames[$index]); ?></th>
-        <?php } ?>
-    </tr>
 <?php
-if (count($depts)) {
-	$htmlHelper = new w2p_Output_HTMLHelper($AppUI);
 
-    $dept_types = w2PgetSysVal('DepartmentType');
-    $customLookups = array('dept_type' => $dept_types);
+$dept_types = w2PgetSysVal('DepartmentType');
+$customLookups = array('dept_type' => $dept_types);
 
-    foreach ($depts as $row) {
-        echo '<tr>';
-        $htmlHelper->stageRowData($row);
-//TODO: how do we tweak this to get the parent/child relationship to display?
-        foreach ($fieldList as $index => $column) {
-            echo $htmlHelper->createCell($fieldList[$index], $row[$fieldList[$index]], $customLookups);
-        }
-        echo '</tr>';
-	}
-} else {
-    echo '<tr><td colspan="'.count($fieldNames).'">' . $AppUI->_('No data available') . '</td></tr>';
-}
-?>
-</table>
+include $AppUI->getTheme()->resolveTemplate('companies/vw_depts');
