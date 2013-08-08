50a19d6f1d1e9157589338fa4144d50f43e2f868
D Keith Casey Jr <keith@caseysoftware.com>
2013-08-07 20:40:28 -0700

    removed all the now unnecessary variables and code;
    all of these variables have been deprecated and/or just aren't used any
    more;
    these probably could have been removed sooner but there wasn't a
    focused cleanup effort here;

--- modules/calendar/companies_tab.view.events.php
+++ modules/calendar/companies_tab.view.events.php
@@ -3,12 +3,7 @@ if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
 
-global $AppUI, $company_id, $deny, $canRead, $canEdit, $w2Pconfig, $start_date, $end_date, $this_day, $event_filter, $event_filter_list;
-
-$perms = &$AppUI->acl();
-$user_id = $AppUI->user_id;
-$other_users = false;
-$no_modify = false;
+global $AppUI, $company_id;
 
 $start_date = new w2p_Utilities_Date('1999-12-31 00:00:00');
 $end_date = new w2p_Utilities_Date('2036-12-31 00:00:00');
@@ -16,30 +11,15 @@ $end_date = new w2p_Utilities_Date('2036-12-31 00:00:00');
 // assemble the links for the events
 $items = CEvent::getEventsForPeriod($start_date, $end_date, 'all', 0, 0, $company_id);
 
-$start_hour = w2PgetConfig('cal_day_start');
-$end_hour = w2PgetConfig('cal_day_end');
-
-$tf = $AppUI->getPref('TIMEFORMAT');
-$df = $AppUI->getPref('SHDATEFORMAT');
-
-$fieldList = array();
-$fieldNames = array();
-
 $module = new w2p_Core_Module();
 $fields = $module->loadSettings('events', 'company_view');
 
-if (count($fields) > 0) {
-    $fieldList = array_keys($fields);
-    $fieldNames = array_values($fields);
-} else {
-    // TODO: This is only in place to provide an pre-upgrade-safe
-    //   state for versions earlier than v3.0
-    //   At some point at/after v4.0, this should be deprecated
+if (0 == count($fields)) {
     $fieldList = array('event_start_date', 'event_end_date', 'event_type',
         'event_name');
     $fieldNames = array('Starting Time', 'Ending Time', 'Type', 'Name');
 
-    //$module->storeSettings('events', 'company_view', $fieldList, $fieldNames);
+    $module->storeSettings('events', 'company_view', $fieldList, $fieldNames);
 
     $fields = array_combine($fieldList, $fieldNames);
 }
--- modules/calendar/projects_tab.view.events.php
+++ modules/calendar/projects_tab.view.events.php
@@ -3,42 +3,22 @@ if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
 
-global $AppUI, $project_id, $deny, $canRead, $canEdit, $w2Pconfig, $start_date, $end_date, $this_day, $event_filter, $event_filter_list;
-
-$perms = &$AppUI->acl();
-$user_id = $AppUI->user_id;
-$other_users = false;
-$no_modify = false;
+global $AppUI, $project_id;
 
 $start_date =  new w2p_Utilities_Date('2001-01-01 00:00:00');
 $end_date =  new w2p_Utilities_Date('2100-12-31 23:59:59');
 
-// assemble the links for the events
 $items = CEvent::getEventsForPeriod($start_date, $end_date, 'all', 0, $project_id);
-
-$start_hour = w2PgetConfig('cal_day_start');
-$end_hour = w2PgetConfig('cal_day_end');
-
-
-$types = w2PgetSysVal('EventType');
-
-$fieldList = array();
-$fieldNames = array();
 
 $module = new w2p_Core_Module();
 $fields = $module->loadSettings('calendar', 'project_view');
-if (count($fields) > 0) {
-    $fieldList = array_keys($fields);
-    $fieldNames = array_values($fields);
-} else {
-    // TODO: This is only in place to provide an pre-upgrade-safe
-    //   state for versions earlier than v3.0
-    //   At some point at/after v4.0, this should be deprecated
+
+if (0 == count($fields)) {
     $fieldList = array('event_start_date', 'event_end_date', 'event_type',
         'event_name');
     $fieldNames = array('Start Date', 'End Date', 'Type', 'Event');
 
-    //$module->storeSettings('calendar', 'project_view', $fieldList, $fieldNames);
+    $module->storeSettings('calendar', 'project_view', $fieldList, $fieldNames);
 
     $fields = array_combine($fieldList, $fieldNames);
 }
