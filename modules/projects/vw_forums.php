9d8e2ba44c864c813c88aea2126d5eb8a90e7313
D Keith Casey Jr <keith@caseysoftware.com>
2013-08-23 12:18:25 -0500

    collapsed the table generation to the list helper

--- modules/projects/vw_forums.php
+++ modules/projects/vw_forums.php
@@ -6,13 +6,14 @@ if (!defined('W2P_BASE_DIR')) {
 global $AppUI, $project_id;
 // Forums mini-table in project view action
 
-$forums = CProject::getForums($AppUI, $project_id);
+$items = CProject::getForums($AppUI, $project_id);
 
 $fieldList = array();
 $fieldNames = array();
 
 $module = new w2p_Core_Module();
 $fields = $module->loadSettings('forums', 'projects_view');
+
 
 if (count($fields) > 0) {
     $fieldList = array_keys($fields);
@@ -25,38 +26,16 @@ if (count($fields) > 0) {
         'forum_last_date');
     $fieldNames = array('Forum Name', 'Description', 'Owner', 'Last Post Info');
 
-    $module->storeSettings('forums', 'projects_view', $fieldList, $fieldNames);
+    //$module->storeSettings('forums', 'projects_view', $fieldList, $fieldNames);
+    $fields = array_combine($fieldList, $fieldNames);
 }
 ?>
 <a name="forums-projects_view"> </a>
-<table class="tbl list">
-    <tr>
-        <th></th>
-        <?php foreach ($fieldNames as $index => $name) { ?>
-            <th><?php echo $AppUI->_($fieldNames[$index]); ?></th>
-        <?php } ?>
-    </tr>
-	<?php
-    if (count($forums) > 0) {
-        $htmlHelper = new w2p_Output_HTMLHelper($AppUI);
-        $htmlHelper->df .= ' ' . $AppUI->getPref('TIMEFORMAT');
+<?php
+$listTable = new w2p_Output_ListTable($AppUI);
+$listTable->df .= ' ' . $AppUI->getPref('TIMEFORMAT');
 
-        foreach ($forums as $row) {
-            ?>
-            <tr bgcolor="white" valign="top">
-                <td nowrap="nowrap" align="center">
-                    <?php if ($row["forum_owner"] == $AppUI->user_id) { ?>
-                        <a href="./index.php?m=forums&a=addedit&forum_id=<?php echo $row['forum_id']; ?>"><img src="<?php echo w2PfindImage('icons/pencil.gif'); ?>" alt="expand forum" border="0" width=12 height=12></a>
-                    <?php } ?>
-                </td>
-                <?php
-                $htmlHelper->stageRowData($row);
-                foreach ($fieldList as $index => $column) {
-                    echo $htmlHelper->createCell($fieldList[$index], $row[$fieldList[$index]], $customLookups);
-                }
-                ?>
-            </tr>
-        <?php
-        }
-    } ?>
-</table>
+echo $listTable->startTable();
+echo $listTable->buildHeader($fields);
+echo $listTable->buildRows($items);
+echo $listTable->endTable();
