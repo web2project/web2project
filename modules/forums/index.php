6742fbef3a2637c613d2234d6803926fbec2d6f6
D Keith Casey Jr <keith@caseysoftware.com>
2013-08-23 12:02:38 -0500

    collapsing the table generation to use the listhelper

--- modules/forums/index.php
+++ modules/forums/index.php
@@ -54,34 +54,28 @@ if (count($fields) > 0) {
     // TODO: This is only in place to provide an pre-upgrade-safe
     //   state for versions earlier than v3.0
     //   At some point at/after v4.0, this should be deprecated
-    $fieldList = array('watch_user', 'forum_name', 'forum_description', 'forum_owner',
+    $fieldList = array('watch_user', 'forum_name', 'forum_project', 'forum_description', 'forum_owner',
         'forum_topics', 'forum_replies', 'forum_last_date');
-    $fieldNames = array('Watch', 'Forum Name', 'Description', 'Owner', 'Topics',
+    $fieldNames = array('Watch', 'Forum Name', 'Project', 'Description', 'Owner', 'Topics',
         'Replies', 'Last Post Info');
 
     //$module->storeSettings('forums', 'index_list', $fieldList, $fieldNames);
     $fields = array_combine($fieldList, $fieldNames);
 }
 $columnCount = 1 + count($fieldList);
+
+$listTable = new w2p_Output_ListTable($AppUI);
+
 ?>
 
 <form name="watcher" action="./index.php?m=forums&f=<?php echo $f; ?>" method="post" accept-charset="utf-8">
 	<input type="hidden" name="dosql" value="do_watch_forum" />
 	<input type="hidden" name="watch" value="forum" />
 
-    <table class="tbl list">
-        <tr>
-            <?php
-            foreach ($fieldNames as $index => $name) {
-                ?><th nowrap="nowrap">
-                    <a href="?m=forums&orderby=<?php echo $fieldList[$index]; ?>" class="hdr">
-                        <?php echo $AppUI->_($fieldNames[$index]); ?>
-                    </a>
-                </th><?php
-            }
-            ?>
-        </tr>
-        <?php
+    <?php
+    echo $listTable->startTable();
+    echo $listTable->buildHeader($fields, true, 'forums');
+
         $p = '';
 
         foreach ($forums as $row) {
@@ -104,26 +98,18 @@ $columnCount = 1 + count($fieldList);
             ?>
             <tr>
                 <td class="data">
-                    <?php if ($row["forum_owner"] == $AppUI->user_id || canAdd('forums')) { ?>
-                        <a href="?m=forums&a=addedit&forum_id=<?php echo $row['forum_id']; ?>" title="<?php echo $AppUI->_('edit'); ?>">
-                        <?php echo w2PshowImage('icons/stock_edit-16.png', 16, 16, ''); ?>
-                        </a>
-                    <?php }
-                    if ($row['visit_count'] != $row['message_count']) {
-                        echo '&nbsp;' . w2PshowImage('icons/stock_new_small.png', false, false, 'You have unread messages in this forum');
-                    } ?>
-                </td>
-
-                <td class="data">
                     <input type="checkbox" name="forum_<?php echo $row['forum_id']; ?>" <?php echo $row['watch_user'] ? 'checked="checked"' : ''; ?> />
                 </td>
 
-                <?php echo $htmlHelper->createCell('forum_name', $row['forum_name']); ?>
-                <?php echo $htmlHelper->createCell('forum_description', $row['forum_description']); ?>
-                <?php echo $htmlHelper->createCell('forum_owner', $row['forum_owner']); ?>
-                <?php echo $htmlHelper->createCell('topic_count', $row['forum_topics']); ?>
-                <?php echo $htmlHelper->createCell('reply_count', $row['forum_replies']); ?>
-                <?php echo $htmlHelper->createCell('forum_last_datetime', $row['forum_last_date']); ?>
+                <?php
+                echo $htmlHelper->createCell('forum_name', $row['forum_name']);
+                echo $htmlHelper->createCell('forum_project', $row['forum_project']);
+                echo $htmlHelper->createCell('forum_description', $row['forum_description']);
+                echo $htmlHelper->createCell('forum_owner', $row['forum_owner']);
+                echo $htmlHelper->createCell('topic_count', $row['forum_topics']);
+                echo $htmlHelper->createCell('reply_count', $row['forum_replies']);
+                echo $htmlHelper->createCell('forum_last_datetime', $row['forum_last_date']);
+                ?>
             </tr>
         <?php } ?>
         <tr>
@@ -131,5 +117,7 @@ $columnCount = 1 + count($fieldList);
                 <input type="submit" class="button" value="<?php echo $AppUI->_('update watches'); ?>" />
             </td>
         </tr>
-    </table>
+    <?php
+    echo $listTable->endTable();
+    ?>
 </form>
