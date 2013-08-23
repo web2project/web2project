ef19829e57536b76054f7f3fcb21aee682fccd32
D Keith Casey Jr <keith@caseysoftware.com>
2013-08-23 11:26:33 -0500

    removed the rest of the unnecessary variables

--- modules/resources/vw_resources.php
+++ modules/resources/vw_resources.php
@@ -9,21 +9,10 @@ $obj = new CResource();
 $where = ($tab) ? 'resource_type = '. $tab : '';
 $items = $obj->loadAll('resource_name', $where);
 
-$htmlHelper = new w2p_Output_HTMLHelper($AppUI);
-
-$fieldList = array();
-$fieldNames = array();
-
 $module = new w2p_Core_Module();
 $fields = $module->loadSettings('resources', 'index_list');
 
-if (count($fields) > 0) {
-    $fieldList = array_keys($fields);
-    $fieldNames = array_values($fields);
-} else {
-    // TODO: This is only in place to provide an pre-upgrade-safe
-    //   state for versions earlier than v3.0
-    //   At some point at/after v4.0, this should be deprecated
+if (0 == count($fields)) {
     $fieldList = array('resource_key', 'resource_name', 'resource_max_allocation',
         'resource_type', 'resource_note');
     $fieldNames = array('Identifier', 'Resource Name', 'Max Alloc %',
