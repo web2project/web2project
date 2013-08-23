6435aa1252ca05ebcd7d7805370cbc4bde95e67a
D Keith Casey Jr <keith@caseysoftware.com>
2013-08-23 17:46:18 -0500

    collapsed the list row generation

--- modules/links/index_table.php
+++ modules/links/index_table.php
@@ -39,8 +39,8 @@ if (count($fields) > 0) {
     // TODO: This is only in place to provide an pre-upgrade-safe
     //   state for versions earlier than v3.0
     //   At some point at/after v4.0, this should be deprecated
-    $fieldList = array('link_name', 'link_description', 'link_category', 'link_task', 'link_owner', 'link_date');
-    $fieldNames = array('Link Name', 'Description', 'Category', 'Task Name', 'Owner', 'Date');
+    $fieldList = array('link_name', 'link_description', 'link_category', 'link_project', 'link_task', 'link_owner', 'link_date');
+    $fieldNames = array('Link Name', 'Description', 'Category', 'Project Task', 'Task Name', 'Owner', 'Date');
 
     //$module->storeSettings('links', 'index_list', $fieldList, $fieldNames);
     $fields = array_combine($fieldList, $fieldNames);
@@ -56,66 +56,16 @@ $items = array_slice($items, $xpg_min, $xpg_pagesize);
 $pageNav = buildPaginationNav($AppUI, $m, $tab, $xpg_totalrecs, $xpg_pagesize, $page);
 echo $pageNav;
 
-$listTable = new w2p_Output_ListTable($AppUI);
-$listTable->addBefore('edit', 'link_id');
-$listTable->addBefore('url', 'link_url');
-
-$listTable->df .= ' ' . $AppUI->getPref('TIMEFORMAT');
-
-echo $listTable->startTable();
-echo $listTable->buildHeader($fields);
-
-$fp = -1;
-
-
-
 $link_types = w2PgetSysVal('LinkType');
 $customLookups = array('link_category' => $link_types);
 
-$id = 0;
-//TODO:  put columns in order
-foreach ($items as $item) {
-    $row = $item;
+$listTable = new w2p_Output_ListTable($AppUI);
+$listTable->df .= ' ' . $AppUI->getPref('TIMEFORMAT');
+$listTable->addBefore('edit', 'link_id');
+$listTable->addBefore('url', 'link_url');
 
-    if ($fp != $row['link_project']) {
-        if (!$row['project_name']) {
-            $row['project_name'] = $AppUI->_('No Project Specified');
-            $row['project_color_identifier'] = 'f4efe3';
-        }
-        if ($showProject) {
-            $s = '<tr>';
-            $s .= '<td colspan="' . $columnCount . '" style=" border: outset #d1d1cd 1px; background-color:#' . $row['project_color_identifier'] . ';">';
-            if ($row['link_project'] > 0) {
-                $s .= '<a href="?m=projects&a=view&project_id=' . $row['link_project'] . '" style="color:'.bestColor($row['project_color_identifier']) . '; font-weight: bold; padding-top: 2px;">' . $row['project_name'] . '</a>';
-            } else {
-                $s .= $row['project_name'];
-            }
-            $s .= '</td></tr>';
-            echo $s;
-        }
-    }
-    $fp = $row['link_project'];
-    echo $listTable->buildRow($row, $customLookups);
-    ?>
-    <tr>
-        <td class="data _edit">
-            <?php if ($canEdit) {
-                echo '<a href="./index.php?m=' . $m . '&a=addedit&link_id=' . $row['link_id'] . '">' . w2PshowImage('icons/stock_edit-16.png', '16', '16') . '</a>';
-            }
-            echo '</td><td class="data">';
-            echo '<a href="' . $row['link_url'] . '" target="_blank">' . w2PshowImage('forward.png', '16', '16') . '</a>';
-            ?>
-        </td>
-        <?php
-        $listTable->stageRowData($row);
-        foreach ($fieldList as $index => $column) {
-            echo $listTable->createCell($fieldList[$index], $row[$fieldList[$index]], $customLookups);
-        }
-        ?>
-    </tr>
-    <?php
-}
-?>
-</table>
-<?php
+echo $listTable->startTable();
+echo $listTable->buildHeader($fields);
+echo $listTable->buildRows($items, $customLookups);
+echo $listTable->endTable();
 echo $pageNav;
