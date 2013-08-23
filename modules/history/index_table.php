d55cc5974278dd148e98530cf71dab498154c3d2
D Keith Casey Jr <keith@caseysoftware.com>
2013-08-23 10:15:52 -0500

    rewired the history module to use the listtable helper fixed up the pagination code too

--- modules/history/index_table.php
+++ modules/history/index_table.php
@@ -11,53 +11,41 @@ $where = (-1 == $filter_param) ? '' : "history_table = '".$filter_param."'";
 $histories = $history->loadAll('history_date DESC', $where);
 $items = array_values($histories);
 
-$xpg_pagesize = w2PgetConfig('page_size', 50);
-$xpg_min = $xpg_pagesize * ($page - 1); // This is where we start our record set from
-// counts total recs from selection
-$xpg_totalrecs = count($items);
-$m .= '&filter='.$filter_param; // This is a hack to get the pagination to work as expected
-$pageNav = buildPaginationNav($AppUI, $m, $tab, $xpg_totalrecs, $xpg_pagesize, $page);
-echo $pageNav;
-
-$fieldList = array('history_date', 'history_description', 'history_user');
-$fieldNames = array('Date', 'Description', 'Owner');
-
-$htmlHelper = new w2p_Output_HTMLHelper($AppUI);
-$htmlHelper->df .= ' ' . $AppUI->getPref('TIMEFORMAT');
-
-?>
-<table class="tbl list history">
-    <tr>
-        <?php foreach ($fieldNames as $index => $name) { ?>
-            <th><?php echo $AppUI->_($fieldNames[$index]); ?></th>
-        <?php } ?>
-    </tr>
-<?php
-
-$items_displayed = false;
 $perms = $AppUI->acl();
-
-for ($i = ($page - 1) * $xpg_pagesize; $i < $page * $xpg_pagesize && $i < $xpg_totalrecs; $i++) {
-    $row = $items[$i];
-    $row['user_id'] = $row['history_user'];
-
-//TODO: we need to make sure sub-modules are handled properly
-    if (!$perms->checkModuleItem($row['history_table'], 'view', $row['history_item'])) {
+foreach ($items as $item) {
+    if (!$perms->checkModuleItem($item['history_table'], 'view', $item['history_item'])) {
         continue;
     }
-//TODO: do we care about linking when we have a create/update entry?
-    echo '<tr>';
-    $htmlHelper->stageRowData($row);
-    echo $htmlHelper->createCell('history_datetime', $row['history_date']);
-    echo $htmlHelper->createCell('history_description', $row['history_description']);
-    echo $htmlHelper->createCell('history_user', $row['history_user']);
-    echo '</tr>';
-    $items_displayed = true;
+    // @note this next line is a little hack so our templating can resolve which history_user is which user
+    $item['user_id'] = $item['history_user'];
+    $display[] = $item;
 }
-if (!$items_displayed) {
-	echo '<tr><td colspan="'.count($fieldNames).'">' . $AppUI->_('No data available') . '</td></tr>';
+$items = $display;
+
+$module = new w2p_Core_Module();
+$fields = $module->loadSettings('history', 'index_list');
+
+if (0 == count($fields)) {
+    $fieldList = array('history_date', 'history_description', 'history_user');
+    $fieldNames = array('Date', 'Description', 'Owner');
+
+    $fields = array_combine($fieldList, $fieldNames);
 }
-?>
-</table>
-<?php
+
+$xpg_pagesize = w2PgetConfig('page_size', 50);
+$xpg_min = $xpg_pagesize * ($page - 1); // This is where we start our record set from
+$xpg_totalrecs = count($items);
+$items = array_slice($items, $xpg_min, $xpg_pagesize);
+
+$m .= '&filter='.$filter_param; // This is a hack to get the pagination to work as expected
+$pageNav = buildPaginationNav($AppUI, $m, 0, $xpg_totalrecs, $xpg_pagesize, $page);
+
+$listTable = new w2p_Output_ListTable($AppUI);
+$listTable->df .= ' ' . $AppUI->getPref('TIMEFORMAT');
+
+echo $pageNav;
+echo $listTable->startTable('tbl list history');
+echo $listTable->buildHeader($fields);
+echo $listTable->buildRows($items);
+echo $listTable->endTable();
 echo $pageNav;
