6ad29294d2139fcfd3f7f221dfe151504d6db47e
D Keith Casey Jr <keith@caseysoftware.com>
2013-10-12 17:07:07 -0400

    brought the header in line with the base header

--- style/web2project/header.php
+++ style/web2project/header.php
@@ -54,7 +54,6 @@ $theme = $AppUI->getTheme();
                         echo '<input type="hidden" name="file_id" value="' . $file_id . '" />';
                     }
                 ?>
-
                 <div class="header">
                     <div class="left nav">
                         <?php echo $theme->buildHeaderNavigation('ul', 'li'); ?>
@@ -81,8 +80,8 @@ $theme = $AppUI->getTheme();
                 </div>
                 <div class="std shadow">&nbsp;</div>
             </form>
-            <div>
-                <div class="left" style="margin: 5px;">
+            <div style="padding-left: 5px;">
+                <div class="left">
                     <?php
                         echo $AppUI->_('Welcome') . ' ' . ($AppUI->user_id > 0 ? $AppUI->user_display_name : $outsider);
                         echo '<br />';
@@ -93,27 +92,28 @@ $theme = $AppUI->getTheme();
                 </div>
                 <?php if ($AppUI->user_id > 0) { ?>
                     <div class="right quicknav">
-                        <div class="left">
+                        <div class="left" style="margin-top: -3px;">
                         <?php if (canAccess('smartsearch')) { ?>
                             <form name="frm_search" action="?m=smartsearch" method="post" accept-charset="utf-8">
                                 <img src="<?php echo w2PfindImage('search.png'); ?>" style="border: 0;" alt="" />&nbsp;<input class="text" size="20" type="text" id="keyword" name="keyword" value="<?php echo $AppUI->_('Global Search') . '...'; ?>" onclick="document.frm_search.keyword.value=''" onblur="document.frm_search.keyword.value='<?php echo $AppUI->_('Global Search') . '...'; ?>'" />
                             </form>
                             <?php } ?>
                         </div>
-                        <a class="button" href="javascript: void(0);" onclick="javascript:window.open('?m=help&amp;dialog=1&amp;hid=', 'contexthelp', 'width=800, height=600, left=50, top=50, scrollbars=yes, resizable=yes')"><div><?php echo $AppUI->_('Help'); ?></div></a>
+                        <a class="button" href="javascript: void(0);" onclick="javascript:window.open('?m=help&amp;dialog=1&amp;hid=', 'contexthelp', 'width=800, height=600, left=50, top=50, scrollbars=yes, resizable=yes')"><span><?php echo $AppUI->_('Help'); ?></span></a>
                         <a class="button" href="./index.php?m=admin&amp;a=view&amp;user_id=<?php echo $AppUI->user_id; ?>"><div><?php echo $AppUI->_('My Info'); ?></div></a>
                         <?php if (canAccess('tasks')) { ?>
-                            <a class="button" href="./index.php?m=tasks&amp;a=todo"><div><b><?php echo $AppUI->_('My Tasks'); ?></b></div></a>
+                            <a class="button" href="./index.php?m=tasks&amp;a=todo"><span><b><?php echo $AppUI->_('My Tasks'); ?></b></span></a>
                         <?php } ?>
                         <?php if (canAccess('calendar')) {
                             $now = new w2p_Utilities_Date(); ?>
-                            <a class="button" href="./index.php?m=calendar&amp;a=day_view&amp;date=<?php echo $now->format(FMT_TIMESTAMP_DATE); ?>"><div><?php echo $AppUI->_('Today'); ?></div></a>
+                            <a class="button" href="./index.php?m=calendar&amp;a=day_view&amp;date=<?php echo $now->format(FMT_TIMESTAMP_DATE); ?>"><span><?php echo $AppUI->_('Today'); ?></span></a>
                         <?php } ?>
-                        <a class="button" href="./index.php?logout=-1"><div><?php echo $AppUI->_('Logout'); ?></div></a>
+                        <a class="button" href="./index.php?logout=-1"><span><?php echo $AppUI->_('Logout'); ?></span></a>
                     </div>
                 <?php } ?>
             </div>
         <?php } ?>
+
         <table width="100%" cellspacing="0" cellpadding="4" border="0">
             <tr>
                 <td valign="top" align="left" width="98%">
