2f2f93c701a2af5dd612dd40be6a2196fd80331b
D Keith Casey Jr <keith@caseysoftware.com>
2013-10-14 19:37:32 -0500

    added a placeholder to help people who haven't upgraded

---
+++ modules/calendar/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo $AppUI->getTheme()->styleRenderBoxTop();
+?>
+<table class="std" width="100%">
+    <tr><td>
+        If you're seeing this message, you need to upgrade your Calendar Module. Have your System Administrator visit
+        the <a href="./index.php?m=system&u=modules">Module Configuration page.</a>
+    </td></tr>
+</table>
