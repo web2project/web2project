e0e26b97560e38fa885427fc388827d487ba88eb
D Keith Casey Jr <keith@caseysoftware.com>
2013-10-14 19:26:57 -0500

    skipped raw SQL in favor of the setup-based upgrader as we should use;

--- modules/calendar/setup.php
+++ modules/calendar/setup.php
@@ -5,7 +5,7 @@ if (!defined('W2P_BASE_DIR')) {
 
 $config = array();
 $config['mod_name'] = 'Calendar';
-$config['mod_version'] = '3.0.1';
+$config['mod_version'] = '3.1.0';
 $config['mod_directory'] = 'events';                     // tell web2Project where to find this module
 $config['mod_setup_class'] = 'CSetupEvents';             // the name of the PHP setup class (used below)
 $config['mod_type'] = 'user';                           // 'core' for modules distributed with w2P by standard, 'user' for additional modules from dotmods
@@ -38,9 +38,47 @@ class CSetupEvents extends w2p_System_Setup
         // NOTE: All cases should fall through so all updates are executed.
         switch ($old_version) {
             case '3.0.0':
-//TODO: wire the permissions change
-//TODO: wire the module update
-                break;
+                $q = new w2p_Database_Query();
+                $q->addTable('modules');
+                $q->addUpdate('mod_directory', 'events');
+                $q->addWhere("mod_directory = 'calendar'");
+                $q->exec();
+
+                $q = new w2p_Database_Query();
+                $q->addTable('gacl_axo');
+                $q->addUpdate('value', 'events');
+                $q->addWhere("value = 'calendar'");
+                $q->exec();
+
+                $q = new w2p_Database_Query();
+                $q->addTable('gacl_axo');
+                $q->addUpdate('section_value', 'events');
+                $q->addWhere("section_value = 'calendar'");
+                $q->exec();
+
+                $q = new w2p_Database_Query();
+                $q->addTable('gacl_axo_map');
+                $q->addUpdate('value', 'events');
+                $q->addWhere("value = 'calendar'");
+                $q->exec();
+
+                $q = new w2p_Database_Query();
+                $q->addTable('gacl_axo_map');
+                $q->addUpdate('section_value', 'events');
+                $q->addWhere("section_value = 'calendar'");
+                $q->exec();
+
+                $q = new w2p_Database_Query();
+                $q->addTable('gacl_permissions');
+                $q->addUpdate('module', 'events');
+                $q->addWhere("module = 'calendar'");
+                $q->exec();
+
+                $q = new w2p_Database_Query();
+                $q->addTable('module_config');
+                $q->addUpdate('module_name', 'events');
+                $q->addWhere("module_name = 'calendar'");
+                $q->exec();
             default:
                 break;
         }
