3a4496d2b3ea109c703542db6e87f377db4933d0
D Keith Casey Jr <keith@caseysoftware.com>
2013-10-27 18:28:32 -0700

    shifted the deprecated classes into the deprecated file, cleans up the class directories

--- classes/w2p/web2project.php
+++ classes/w2p/web2project.php
@@ -2,7 +2,27 @@
 
 function web2project_autoload($className) {
     $library_name = 'w2p_';
-    
+
+    /**
+     * This portion of the autoloader is non-standard and exists only to catch our deprecated classes.
+     */
+    switch($className)
+    {
+        case 'w2p_API_iCalendar':
+        case 'w2p_Core_Config':
+        case 'w2p_Core_Dispatcher':
+        case 'w2p_Core_Event':
+        case 'w2p_Core_EventQueue':
+        case 'w2p_Core_HookHandler':
+        case 'w2p_Core_Module':
+        case 'w2p_Core_Preferences':
+        case 'w2p_Core_Setup':
+        case 'w2p_Core_UpgradeManager':
+            return include W2P_BASE_DIR . '/classes/deprecated.class.php';
+        default:
+            //fall through
+    }
+
     if (substr($className, 0, strlen($library_name)) != $library_name) {
         return false;
     }
