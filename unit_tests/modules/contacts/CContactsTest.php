bf46370295a2b008991eea75d2e1dd53be892b87
D Keith Casey Jr <keith@caseysoftware.com>
2013-11-09 01:02:19 -0500

    correcting a test;

--- unit_tests/modules/contacts/CContactsTest.php
+++ unit_tests/modules/contacts/CContactsTest.php
@@ -239,7 +239,7 @@ class CContacts_Test extends CommonSetup
         $this->obj->contact_id = 1;
         $this->mockDB->stageHashList($this->obj->contact_id, 'ASDFASDFASDF');
 
-        $this->assertEquals('ASDFASDFASDF',         $this->obj->getUpdateKey());
+        $this->assertEquals('A',         $this->obj->getUpdateKey());
     }
 
     public function testClearUpdateKey() {
