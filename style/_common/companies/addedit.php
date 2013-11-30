18daaf314e4a5ea531cce892b65dd3a0f785fbef
Keith Casey <keith@caseysoftware.com>
2013-11-30 15:17:50 -0600

    wired the addedit form to use the form helper

--- style/_common/companies/addedit.php
+++ style/_common/companies/addedit.php
@@ -9,89 +9,34 @@ $form = new w2p_Output_HTML_FormHelper($AppUI);
 
     <div class="std addedit companies">
         <div class="column left">
-            <p>
-                <label><?php echo $AppUI->_('Company Name'); ?>:</label>
-                <?php echo $form->addField('company_name', $company->company_name, array('maxlength' => 255)); ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('Email'); ?>:</label>
-                <?php echo $form->addField('company_email', $company->company_email, array('maxlength' => 255)); ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('Phone'); ?>:</label>
-                <?php echo $form->addField('company_phone1', $company->company_phone1, array('maxlength' => 30)); ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('Phone'); ?>2:</label>
-                <?php echo $form->addField('company_phone2', $company->company_phone2, array('maxlength' => 50)); ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('URL'); ?>2:</label>
-                <?php echo $form->addField('company_primary_url', $company->company_primary_url, array('maxlength' => 255)); ?>
-                <a href="javascript: void(0);" onclick="testURL('CompanyURLOne')">[<?php echo $AppUI->_('test'); ?>]</a>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('Description'); ?>:</label>
-                <?php echo $form->addField('company_description', $company->company_description); ?>
-            </p>
-            <p>
-                <?php
-                $custom_fields = new w2p_Core_CustomFields($m, $a, $company->company_id, "edit");
-                $custom_fields->printHTML();
-                ?>
-            </p>
-            <p>
-                <?php echo $form->addCancelButton(); ?>
-            </p>
+            <?php
+            echo $form->addRow('company_name', $company->company_name, array('maxlength' => 255));
+            echo $form->addRow('company_email', $company->company_email, array('maxlength' => 255));
+            echo $form->addRow('company_phone1', $company->company_phone1, array('maxlength' => 30));
+            echo $form->addRow('company_phone2', $company->company_phone2, array('maxlength' => 50));
+            echo $form->addRow('company_primary_url', $company->company_primary_url, array('maxlength' => 255));
+            echo $form->addRow('company_description', $company->company_description);
+            $custom_fields = new w2p_Core_CustomFields($m, $a, $company->company_id, "edit");
+            echo '<p>' . $custom_fields->getHTML() . '</p>';
+            echo '<p>' . $form->addCancelButton() . '</p>';
+            ?>
         </div>
         <div class="column right">
-            <p>
-                <label><?php echo $AppUI->_('Address'); ?>1:</label>
-                <?php echo $form->addField('company_address1', $company->company_address1, array('maxlength' => 255)); ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('Address'); ?>2:</label>
-                <?php echo $form->addField('company_address2', $company->company_address2, array('maxlength' => 255)); ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('City'); ?>:</label>
-                <?php echo $form->addField('company_city', $company->company_city, array('maxlength' => 50)); ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('State'); ?>:</label>
-                <?php echo $form->addField('company_state', $company->company_state, array('maxlength' => 50)); ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('Zip'); ?>:</label>
-                <?php echo $form->addField('company_zip', $company->company_zip, array('maxlength' => 15)); ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('Country'); ?>:</label>
-                <?php
-                echo $form->addField('company_country', $company->company_country, array(), $countries);
-                ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('Fax'); ?>:</label>
-                <?php echo $form->addField('company_fax', $company->company_fax, array('maxlength' => 30)); ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('Company Owner'); ?>:</label>
-                <?php
-                $perms = &$AppUI->acl();
-                $users = $perms->getPermittedUsers('companies');
-                echo $form->addField('company_owner', $company->company_owner, array(), $users);
-                ?>
-            </p>
-            <p>
-                <label><?php echo $AppUI->_('Type'); ?>:</label>
-                <?php
-                echo $form->addField('company_type', $company->company_type, array(), $types);
-                ?>
-            </p>
-            <p>
-                <?php echo $form->addSaveButton(); ?>
-            </p>
+            <?php
+            echo $form->addRow('company_address1', $company->company_address1, array('maxlength' => 255));
+            echo $form->addRow('company_address2', $company->company_address2, array('maxlength' => 255));
+            echo $form->addRow('company_city', $company->company_city, array('maxlength' => 50));
+            echo $form->addRow('company_state', $company->company_state, array('maxlength' => 50));
+            echo $form->addRow('company_zip', $company->company_zip, array('maxlength' => 15));
+            echo $form->addRow('company_country', $company->company_country, array(), $countries);
+            echo $form->addRow('company_fax', $company->company_fax, array('maxlength' => 30));
+
+            $perms = &$AppUI->acl();
+            $users = $perms->getPermittedUsers('companies');
+            echo $form->addRow('company_owner', $company->company_owner, array(), $users);
+            echo $form->addRow('company_type', $company->company_type, array(), $types);
+            echo '<p>' . $form->addSaveButton() . '</p>';
+            ?>
         </div>
     </div>
 </form>
