a2d3c467a28c747a4bc2f2f0942f1d8ae6be02be
Keith Casey <keith@caseysoftware.com>
2013-12-27 15:39:46 -0600

    simplified version of the company refactoring since we have the permissions check in the load(), we don’t need to do it again outside it;

--- modules/links/view.php
+++ modules/links/view.php
@@ -6,15 +6,15 @@ if (!defined('W2P_BASE_DIR')) {
 $link_id = (int) w2PgetParam($_GET, 'link_id', 0);
 
 $link = new CLink();
-$link->link_id = $link_id;
 
-$canView   = $link->canView();
-$canAccess = $link->canAccess();
-
-if (!$canAccess || !$canView) {
-	$AppUI->redirect(ACCESS_DENIED);
+if (!$link->load($link_id)) {
+    $AppUI->redirect(ACCESS_DENIED);
 }
 
-$link->load();
+if (!$link) {
+    $AppUI->setMsg('Link');
+    $AppUI->setMsg('invalidID', UI_MSG_ERROR, true);
+    $AppUI->redirect();
+}
 
 header("Location: " . $link->link_url);
