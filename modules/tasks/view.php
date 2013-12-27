de2cb1c61e0e18a3e5e99c517f810698e04e83ca
Keith Casey <keith@caseysoftware.com>
2013-12-27 16:20:25 -0600

    same refactoring as the companies module;
    used the new permissions check in load() instead of an explicit one;
    removed the unneeded permissions checks;
    shifted the permissions checks to make sure we handle all of them first
    before bothering with anything else;

--- modules/tasks/view.php
+++ modules/tasks/view.php
@@ -3,34 +3,31 @@ if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
 // @todo    convert to template
-
 $task_id = (int) w2PgetParam($_GET, 'task_id', 0);
 $task_log_id = (int) w2PgetParam($_GET, 'task_log_id', 0);
-$reminded = (int) w2PgetParam($_GET, 'reminded', 0);
-
 $obj = new CTask();
-$obj->task_id = $task_id;
+
+if (!$obj->load($task_id)) {
+    $AppUI->redirect(ACCESS_DENIED);
+}
+
+if (!$obj) {
+    $AppUI->setMsg('Task');
+    $AppUI->setMsg('invalidID', UI_MSG_ERROR, true);
+    $AppUI->redirect();
+}
 
 $canEdit   = $obj->canEdit();
-$canRead   = $obj->canView();
-$canCreate = $obj->canCreate();
-$canAccess = $obj->canAccess();
 $canDelete = $obj->canDelete();
 
-if (!$canAccess || !$canRead) {
-	$AppUI->redirect(ACCESS_DENIED);
-}
-
-$obj->loadFull(null, $task_id);
-if (!$obj) {
-	$AppUI->setMsg('Task');
-	$AppUI->setMsg('invalidID', UI_MSG_ERROR, true);
-	$AppUI->redirect();
-}
 
 $tab = $AppUI->processIntState('TaskLogVwTab', $_GET, 'tab', 0);
 
-// Clear any reminders
+/**
+ * Clear any reminders
+ * @todo THIS SHOULD NOT HAPPEN HERE.. VIEWING SHOULD BE IDEMPOTENT
+ */
+$reminded = (int) w2PgetParam($_GET, 'reminded', 0);
 if ($reminded) {
 	$obj->clearReminder();
 }
@@ -47,18 +44,19 @@ $billingCategory = w2PgetSysVal('BudgetCategory');
 // setup the title block
 $titleBlock = new w2p_Theme_TitleBlock('View Task', 'icon.png', $m, $m . '.' . $a);
 $titleBlock->addCell();
+if ($canReadProject) {
+    $titleBlock->addCrumb('?m=projects&a=view&project_id=' . $obj->task_project, 'view this project');
+}
+
 if ($canEdit) {
     $titleBlock->addButton('new log',  '?m=tasks&a=view&task_id=' . $task_id . '&tab=1');
     $titleBlock->addButton('new link', '?m=links&a=addedit&task_id=' . $task_id . '&project_id=' . $obj->task_project);
     $titleBlock->addButton('new file', '?m=files&a=addedit&project_id=' . $obj->task_project . '&file_task=' . $obj->task_id);
     $titleBlock->addButton('new task', '?m=tasks&a=addedit&task_project=' . $obj->task_project . '&task_parent=' . $task_id);
-}
 
-if ($canReadProject) {
-	$titleBlock->addCrumb('?m=projects&a=view&project_id=' . $obj->task_project, 'view this project');
-}
-if ($canEdit && 0 == $obj->task_represents_project) {
-	$titleBlock->addCrumb('?m=tasks&a=addedit&task_id=' . $task_id, 'edit this task');
+    if (!$obj->task_represents_project) {
+	    $titleBlock->addCrumb('?m=tasks&a=addedit&task_id=' . $task_id, 'edit this task');
+    }
 }
 if ($obj->task_represents_project) {
     $titleBlock->addCrumb('?m=projects&a=view&project_id=' . $obj->task_represents_project, 'view subproject');
