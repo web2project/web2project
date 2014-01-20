4794da7094e0a44cd6eafc4c4a5e4bcdf5607d09
D Keith Casey Jr <keith@caseysoftware.com>
2014-01-19 20:26:34 -0600

    wired the hook_search test

--- unit_tests/modules/departments/CDepartmentsTest.php
+++ unit_tests/modules/departments/CDepartmentsTest.php
@@ -297,9 +297,9 @@ class CDepartments_Test extends CommonSetup
      * @todo Implement testHook_search().
      */
     public function testHook_search() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        $search = $this->obj->hook_search();
+
+        $this->assertTrue(array_key_exists('search_fields', $search));
+        $this->assertEquals(count($search), 8);
     }
 }
