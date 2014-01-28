8be694326a43a4b25a3f96c07af6ecf5e6bf7d9b
Keith Casey <keith@caseysoftware.com>
2014-01-28 13:21:39 +0100

    refactoring some common bits into a base class, building the view class next;

--- classes/w2p/Output/HTML/FormHelper.class.php
+++ classes/w2p/Output/HTML/FormHelper.class.php
@@ -1,28 +1,9 @@
 <?php
-
-class w2p_Output_HTML_FormHelper
+/**
+ * Class w2p_Output_HTML_FormHelper
+ */
+class w2p_Output_HTML_FormHelper extends w2p_Output_HTML_Base
 {
-    protected $AppUI = null;
-    protected $df = null;
-    protected $dtf = null;
-
-    public function __construct($AppUI)
-    {
-        $this->AppUI = $AppUI;
-        $this->df     = $AppUI->getPref('SHDATEFORMAT');
-        $this->dtf    = $this->df . ' ' . $AppUI->getPref('TIMEFORMAT');
-    }
-
-    public function addLabel($label)
-    {
-        return '<label>' . $this->AppUI->_($label) . ':</label>';
-    }
-
-    public function showLabel($label)
-    {
-        echo $this->addLabel($label);
-    }
-
     public function addField($fieldName, $fieldValue, $options = array(), $values = array())
     {
         $pieces = explode('_', $fieldName);
