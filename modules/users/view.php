786c9d6116e9984373d9f354ac55a5023e23cc54
Keith Casey <keith@caseysoftware.com>
2014-04-19 00:38:09 -0500

    broke out the user templates

--- modules/users/view.php
+++ modules/users/view.php
@@ -65,111 +65,23 @@ if ($canEdit || $user_id == $AppUI->user_id) {
     $titleBlock->addCrumb('?m=system&a=addeditpref&user_id='.$user_id, 'edit preferences');
 }
 $titleBlock->show();
-
-$htmlHelper = new w2p_Output_HTMLHelper($AppUI);
-$htmlHelper->stageRowData((array) $user);
 ?>
-    <script language="javascript" type="text/javascript">
-        <?php
-            // security improvement:
-            // some javascript functions may not appear on client side in case of user not having write permissions
-            // else users would be able to arbitrarily run 'bad' functions
-            if ($canEdit || $user_id == $AppUI->user_id) {
-        ?>
-        function popChgPwd() {
-            window.open( './index.php?m=public&a=chpwd&dialog=1&user_id=<?php echo $user->user_id; ?>', 'chpwd', 'top=250,left=250,width=350, height=220, scrollbars=no' );
-        }
-        <?php } ?>
-    </script>
-    <table class="std view admin">
-        <tr>
-            <th colspan="2"><?php echo $user->user_username; ?></th>
-        </tr>
-        <tr valign="top">
-            <td width="50%">
-                <strong><?php echo $AppUI->_('Details'); ?></strong>
-                <table cellspacing="1" cellpadding="2" border="0" width="100%" class="well">
-                    <tr>
-                        <td align="right" nowrap="nowrap"><?php echo $AppUI->_('User Type'); ?>:</td>
-                        <?php echo $htmlHelper->createCell('user_type', $AppUI->_($utypes[$user->user_type])); ?>
-                    </tr>
-                    <tr>
-                        <td align="right" nowrap="nowrap"><?php echo $AppUI->_('Real Name'); ?>:</td>
-                        <?php echo $htmlHelper->createCell('contact_displayname', $user->contact_display_name); ?>
-                    </tr>
-                    <tr>
-                        <td align="right" nowrap="nowrap"><?php echo $AppUI->_('Company'); ?>:</td>
-                        <?php echo $htmlHelper->createCell('contact_company', $user->contact_company); ?>
-                    </tr>
-                    <tr>
-                        <td align="right" nowrap="nowrap"><?php echo $AppUI->_('Department'); ?>:</td>
-                        <?php echo $htmlHelper->createCell('contact_department', $user->contact_department); ?>
-                    </tr>
-                    <tr>
-                        <td align="right" nowrap="nowrap"><?php echo $AppUI->_('Phone'); ?>:</td>
-                        <?php echo $htmlHelper->createCell('contact_phone', $user->contact_phone); ?>
-                    </tr>
-                    <tr>
-                        <td align="right" nowrap="nowrap"><?php echo $AppUI->_('Email'); ?>:</td>
-                        <?php echo $htmlHelper->createCell('contact_email', $user->contact_email); ?>
-                    </tr>
-                    <tr valign="top">
-                        <td align="right" nowrap="nowrap"><?php echo $AppUI->_('Address'); ?>:</td>
-                        <td width="100%">
-                            <?php echo $user->contact_address1; ?><br />
-                            <?php echo ($user->contact_address2 == '') ? '' : $user->contact_address2.'<br />'; ?>
-                            <?php echo $user->contact_city . ', ' . $user->contact_state . ' ' . $user->contact_zip; ?><br />
-                            <?php echo isset($countries[$user->contact_country]) ? $countries[$user->contact_country] : $user->contact_country; ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="right" nowrap="nowrap"><?php echo $AppUI->_('Birthday'); ?>:</td>
-                        <?php echo $htmlHelper->createCell('_date', $user->contact_birthday); ?>
-                    </tr>
-                </table>
-            </td>
-            <td width="50%">
-                <strong><?php echo $AppUI->_('Contact Information'); ?></strong>
-                <table cellspacing="1" cellpadding="2" border="0" width="100%" class="well">
-                    <?php
-                    $fields = $methods['fields'];
-                    foreach ($fields as $key => $field): ?>
-                        <tr>
-                            <td align="right" width="100" nowrap="nowrap"><?php echo $AppUI->_($methodLabels[$field]); ?>:</td>
-                            <?php echo $htmlHelper->createCell('_'.substr($field, 0, strpos($field, '_')), $methods['values'][$key]); ?>
-                        </tr>
-                    <?php endforeach; ?>
-                    <tr>
-                        <td align="right" nowrap="nowrap"><?php echo $AppUI->_('Calendar Feed'); ?>:</td>
-                        <td width="100%">
-                            <?php if ($user->feed_token != '') {
-                                $calendarFeed = W2P_BASE_URL.'/calendar.php?token='.$user->feed_token.'&amp;ext=.ics';
-                                ?>
-                                <a href="<?php echo $calendarFeed; ?>">calendar feed</a>
-                            <?php } ?>
-                            &nbsp;&nbsp;&nbsp;
-                            <form name="regenerateToken" action="./index.php?m=users" method="post" accept-charset="utf-8">
-                                <input type="hidden" name="user_id" value="<?php echo (int) $user->user_id; ?>" />
-                                <input type="hidden" name="dosql" value="do_user_token" />
-                                <input type="hidden" name="token" value="<?php echo $user->feed_token; ?>" />
-                                <input type="submit" name="regenerate token" value="<?php echo $AppUI->_('regenerate feed url'); ?>" class="button btn btn-primary btn-mini" />
-                            </form>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2"><strong><?php echo $AppUI->_('Signature'); ?>:</strong></td>
-                    </tr>
-                    <tr>
-                        <td width="100%" colspan="2">
-                            <?php echo w2p_textarea($user->user_signature); ?>&nbsp;
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
-
+<script language="javascript" type="text/javascript">
+    <?php
+        // security improvement:
+        // some javascript functions may not appear on client side in case of user not having write permissions
+        // else users would be able to arbitrarily run 'bad' functions
+        if ($canEdit || $user_id == $AppUI->user_id) {
+    ?>
+    function popChgPwd() {
+        window.open( './index.php?m=public&a=chpwd&dialog=1&user_id=<?php echo $user->user_id; ?>', 'chpwd', 'top=250,left=250,width=350, height=220, scrollbars=no' );
+    }
+    <?php } ?>
+</script>
 <?php
+
+include $AppUI->getTheme()->resolveTemplate('users/view');
+
 // tabbed information boxes
 $min_view = true;
 $tabBox = new CTabBox('?m=users&a=view&user_id='.$user_id, '', $tab);
---
+++ style/_common/users/addedit.php
@@ -0,0 +1,86 @@
+<?php
+
+$form = new w2p_Output_HTML_FormHelper($AppUI);
+
+?>
+<form name="editFrm" action="?m=<?php echo $m; ?>" method="post" accept-charset="utf-8" class="admin addedit">
+    <input type="hidden" name="user_id" value="<?php echo (int) $object->user_id; ?>" />
+    <input type="hidden" name="contact_id" value="<?php echo (int) $object->contact_id; ?>" />
+    <input type="hidden" name="dosql" value="do_user_aed" />
+    <input type="hidden" name="username_min_len" value="<?php echo w2PgetConfig('username_min_len'); ?>)" />
+    <input type="hidden" name="password_min_len" value="<?php echo w2PgetConfig('password_min_len'); ?>)" />
+    <?php echo $form->addNonce(); ?>
+
+    <div class="std addedit users">
+        <div class="column left">
+            <p>
+                <?php $form->showLabel('Login Name'); ?>
+                <?php
+                if ($object->user_username) {
+                    echo '<input type="hidden" class="text" name="user_username" value="' . $object->user_username . '" />';
+                    echo '<strong>' . $object->user_username . '</strong>';
+                } else {
+                    echo '<input type="text" class="text" name="user_username" value="' . $object->user_username . '" maxlength="255" size="40" />';
+                }
+                ?>
+            </p>
+            <?php if (!$object->user_id) { ?>
+                <p>
+                    <?php $form->showLabel('Password'); ?>
+                    <input type="password" class="text" name="user_password" value="<?php echo $object->user_password; ?>" maxlength="32" size="32" onKeyUp="checkPassword(this.value);" />
+                </p>
+                <p>
+                    <?php $form->showLabel('Confirm Password'); ?>
+                    <input type="password" class="text" name="password_check" value="<?php echo $object->user_password; ?>" maxlength="32" size="32" />
+                </p>
+            <?php } ?>
+            <p>
+                <?php $form->showLabel('Name'); ?>
+                <?php $form->showField('contact_first_name', $object->contact_first_name, array('maxlength' => 50)); ?>
+                <?php $form->showField('contact_last_name', $object->contact_last_name, array('maxlength' => 50)); ?>
+            </p>
+            <p>
+                <?php $form->showLabel('Company'); ?>
+                <?php
+                echo arraySelect($companies, 'contact_company', 'class=text size=1', $object->contact_company);
+                ?>
+            </p>
+            <p>
+                <?php $form->showLabel('Department'); ?>
+                <input type="hidden" name="contact_department" value="<?php echo $object->contact_department; ?>" />
+                <input type="text" class="text" name="dept_name" value="<?php echo $object->dept_name; ?>" size="40" disabled="disabled" />
+                <input type="button" class="button btn btn-primary btn-mini" value="<?php echo $AppUI->_('select dept'); ?>..." onclick="popDept()" />
+            </p>
+            <?php $form->showCancelButton(); ?>
+        </div>
+        <div class="column right">
+            <?php if ($canEdit && !$user_id) { ?>
+                <p>
+                    <?php $form->showLabel('User Role'); ?>
+                    <?php echo arraySelect($roles_arr, 'user_role', 'size="1" class="text"', '', true); ?>
+                </p>
+            <?php } ?>
+            <?php if (!$object->user_id) { ?>
+                <p>
+                    <?php $form->showLabel('Password Strength'); ?>
+                <div id="password-strength" class="text">
+                    <div id="progressBar"></div>
+                </div>
+                </p>
+            <?php } ?>
+            <p>
+                <?php $form->showLabel('Email'); ?>
+                <?php $form->showField('contact_email', $object->contact_email, array('maxlength' => 255)); ?>
+            </p>
+            <p>
+                <?php $form->showLabel('Email Signature'); ?>
+                <?php $form->showField('user_signature', $object->user_signature); ?>
+            </p>
+            <p>
+                <?php $form->showLabel('Inform new user of account details?'); ?>
+                <input type="checkbox" value="1" name="send_user_mail" id="send_user_mail" />
+            </p>
+            <?php $form->showSaveButton(); ?>
+        </div>
+    </div>
+</form>
