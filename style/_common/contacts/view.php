70f6be5c5caf1af3dfb176739a74554618207f82
Keith Casey <keith@caseysoftware.com>
2014-04-19 02:07:12 -0500

    converted more table-based layouts to div-based

--- style/_common/contacts/view.php
+++ style/_common/contacts/view.php
@@ -1,147 +1,67 @@
-<table class="std view contacts">
-    <tr>
-        <td valign="top">
-            <table border="0" cellpadding="1" cellspacing="1">
-                <tr>
-                    <td align="right" nowrap="nowrap"><?php echo $AppUI->_('First Name'); ?>:</td>
-                    <?php
+<?php
 
-                    // TODO HTMLhelper was confused renamed field name so HTMLhelper is sane...
-                    echo $htmlHelper->createCell('contact_firstname', $contact->contact_first_name); ?>
-                </tr>
-                <tr>
-                    <td align="right" nowrap="nowrap">&nbsp;&nbsp;<?php echo $AppUI->_('Last Name'); ?>:</td>
-                    <?php
+$view = new w2p_Output_HTML_ViewHelper($AppUI);
 
-                    // TODO HTMLhelper was confused renamed field name so HTMLhelper is sane...
-                    echo $htmlHelper->createCell('contact_lastname', $contact->contact_last_name); ?>
-                </tr>
-                <tr>
-                    <td align="right" width="100"><?php echo $AppUI->_('Display Name'); ?>: </td>
-                    <?php
+?>
+<div class="std addedit contacts">
+    <div class="column left">
 
-                    // TODO HTMLhelper was confused renamed field name so HTMLhelper is sane...
-                    echo $htmlHelper->createCell('contact_displayname', $contact->contact_display_name); ?>
-                </tr>
-                <tr>
-                    <td align="right"><?php echo $AppUI->_('Job Title'); ?>:</td>
-                    <?php echo $htmlHelper->createCell('contact_job', $contact->contact_job); ?>
-                </tr>
-                <tr>
-                    <td align="right" width="100"><?php echo $AppUI->_('Company'); ?>:</td>
-                    <td nowrap="nowrap" width="100%">
-                        <?php
-                        $perms = &$AppUI->acl();
-                        if ($perms->checkModuleItem('companies', 'access', $contact->contact_company)) { ?>
-                            <?php echo "<a href='?m=companies&a=view&company_id=" . $contact->contact_company . "'>" . htmlspecialchars($company_detail['company_name'], ENT_QUOTES) . '</a>'; ?>
-                        <?php } else { ?>
-                            <?php echo htmlspecialchars($company_detail['company_name'], ENT_QUOTES); ?>
-                        <?php } ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td align="right" width="100"><?php echo $AppUI->_('Department'); ?>:</td>
-                    <?php echo $htmlHelper->createCell('contact_department', $contact->contact_department); ?>
-                </tr>
-                <tr>
-                    <td align="right"><?php echo $AppUI->_('Title'); ?>:</td>
-                    <?php echo $htmlHelper->createCell('contact_title', $contact->contact_title); ?>
-                </tr>
-                <tr>
-                    <td align="right" valign="top" width="100"><?php echo $AppUI->_('Address'); ?>:</td>
-                    <td width="100%">
-                        <?php echo $contact->contact_address1; ?><br />
-                        <?php echo $contact->contact_address2; ?><br />
-                        <?php echo $contact->contact_city . ', ' . $contact->contact_state . ' ' . $contact->contact_zip; ?><br />
-                        <?php echo isset($countries[$contact->contact_country]) ? $countries[$contact->contact_country] : $contact->contact_country; ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td align="right" width="100"><?php echo $AppUI->_('Map Address'); ?>:</td>
-                    <td width="100%"><a target="_blank" href="http://maps.google.com/maps?q=<?php echo $contact->contact_address1; ?>+<?php echo $contact->contact_address2; ?>+<?php echo $contact->contact_city; ?>+<?php echo $contact->contact_state; ?>+<?php echo $contact->contact_zip; ?>+<?php echo $contact->contact_country; ?>"><?php echo w2PshowImage('googlemaps.gif', 55, 22, 'Find It on Google'); ?></a></td>
-                </tr>
-            </table>
-        </td>
-        <td>
-            <table border="0" cellpadding="1" cellspacing="1">
-                <tr>
-                    <td align="right"><?php echo $AppUI->_('Birthday'); ?>:</td>
-                    <?php echo $htmlHelper->createCell('_date', $contact->contact_birthday); ?>
-                </tr>
-                <tr>
-                    <td align="right"><?php echo $AppUI->_('Phone'); ?>:</td>
-                    <?php echo $htmlHelper->createCell('contact_phone', $contact->contact_phone); ?>
-                </tr>
-                <tr>
-                    <td align="right"><?php echo $AppUI->_('Email'); ?>:</td>
-                    <?php echo $htmlHelper->createCell('contact_email', $contact->contact_email); ?>
-                </tr>
-                <?php
-                $fields = $methods['fields'];
-                foreach ($fields as $key => $field): ?>
-                    <tr>
-                        <td align="right" width="100" nowrap="nowrap"><?php echo $AppUI->_($methodLabels[$field]); ?>:</td>
-                        <?php echo $htmlHelper->createCell('_'.substr($field, 0, strpos($field, '_')), $methods['values'][$key]); ?>
-                    </tr>
-                <?php endforeach; ?>
-            </table>
-        </td>
-        <td valign="top" align="right">
-            <table border="0" cellpadding="1" cellspacing="1">
-                <th colspan="2">
-                    <strong><?php echo $AppUI->_('Contact Update Info'); ?></strong>
-                </th>
-                <tr>
-                    <td align="right" width="100" nowrap="nowrap"><?php echo $AppUI->_('Waiting Update'); ?>?:</td>
-                    <td align="center">
-                        <input type="checkbox" value="1" name="contact_updateasked" disabled="disabled" <?php echo $contact->contact_updatekey ? 'checked="checked"' : ''; ?> />
-                    </td>
-                </tr>
-                <tr>
-                    <td align="right" nowrap="nowrap"><?php echo $AppUI->_('Last Update Requested'); ?>:</td>
-                    <td align="center" nowrap="nowrap"><?php echo $contact->contact_updateasked ? $last_ask->format($df) : ''; ?></td>
-                </tr>
-                <tr>
-                <tr>
-                    <td align="right" width="100" nowrap="nowrap"><?php echo $AppUI->_('Last Updated'); ?>:</td>
-                    <td align="center" nowrap="nowrap">
-                        <?php
-                        echo ($contact->contact_lastupdate && !($contact->contact_lastupdate == 0)) ? $AppUI->formatTZAwareTime($contact->contact_lastupdate) : '';
-                        ?>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <td valign="top" width="50%">
-            <table border="0" cellpadding="1" cellspacing="1" class="details" width="100%">
-                <?php
-                $custom_fields = new w2p_Core_CustomFields($m, $a, $contact->contact_id, 'view');
-                if ($custom_fields->count()) { ?>
-                    <th colspan="2">
-                        <strong><?php echo $AppUI->_('Contacts Custom Fields'); ?></strong>
-                    </th>
-                    <tr>
-                        <td colspan="2">
-                            <?php
-                            $custom_fields->printHTML();
-                            ?>
-                        </td>
-                    </tr>
-                <?php
-                }
-                ?>
-            </table>
-        </td>
-        <td valign="top" width="50%">
-            <strong><?php echo $AppUI->_('Contact Notes'); ?></strong><br />
-            <?php echo w2p_textarea($contact->contact_notes); ?>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <input type="button" value="<?php echo $AppUI->_('back'); ?>" class="button" onclick="javascript:window.location='./index.php?m=contacts';" />
-        </td>
-    </tr>
-</table>
+        <p><?php $view->showLabel('First Name'); ?>
+            <?php $view->showField('contact_firstname', $contact->contact_first_name); ?>
+        </p>
+        <p><?php $view->showLabel('Last Name'); ?>
+            <?php $view->showField('contact_lastname', $contact->contact_last_name); ?>
+        </p>
+        <p><?php $view->showLabel('Display Name'); ?>
+            <?php $view->showField('contact_displayname', $contact->contact_display_name); ?>
+        </p>
+        <p><?php $view->showLabel('Title'); ?>
+            <?php $view->showField('contact_title', $contact->contact_title); ?>
+        </p>
+        <p><?php $view->showLabel('Job Title'); ?>
+            <?php $view->showField('contact_job', $contact->contact_job); ?>
+        </p>
+        <p><?php $view->showLabel('Company'); ?>
+            <?php $view->showField('contact_company', $contact->contact_company); ?>
+        </p>
+        <p><?php $view->showLabel('Department'); ?>
+            <?php $view->showField('contact_department', $contact->contact_department); ?>
+        </p>
+        <p><?php $view->showLabel('Address'); ?>
+            <?php $view->showAddress('contact', $contact); ?>
+        </p>
+    </div>
+    <div class="column right">
+        <p><?php $view->showLabel('Birthday'); ?>
+            <?php $view->showField('contact_birthday', $contact->contact_birthday); ?>
+        </p>
+        <p><?php $view->showLabel('Phone'); ?>
+            <?php $view->showField('contact_phone', $contact->contact_phone); ?>
+        </p>
+        <p><?php $view->showLabel('Email'); ?>
+            <?php $view->showField('contact_email', $contact->contact_email); ?>
+        </p>
+        <p><?php $view->showLabel('Description'); ?>
+            <?php $view->showField('contact_notes', $contact->contact_notes); ?>
+        </p>
+        <p>
+            <?php
+            $custom_fields = new w2p_Core_CustomFields($m, $a, $contact->contact_id, 'view');
+            $custom_fields->printHTML();
+            ?>
+        </p>
+        <?php
+        $fields = $methods['fields'];
+        foreach ($fields as $key => $field): ?>
+            <p><?php $view->showLabel($methodLabels[$field]); ?>
+                <?php echo $methods['values'][$key]; ?>
+            </p>
+        <?php endforeach; ?>
+        <p><?php $view->showLabel('Waiting Update'); ?>
+            <input type="checkbox" value="1" name="contact_updateasked" disabled="disabled" <?php echo $contact->contact_updatekey ? 'checked="checked"' : ''; ?> />
+        </p>
+        <p><?php $view->showLabel('Last Updated'); ?>
+            <?php $view->showField('contact_lastupdate', $contact->contact_lastupdate); ?>
+        </p>
+    </div>
+</div>
