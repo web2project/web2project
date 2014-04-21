f6079f8bbc8d311d9dc7c9689b051df4fd9b0a6f
Keith Casey <keith@caseysoftware.com>
2014-04-21 02:09:30 -0500

    updating some table character sets

--- install/sql/051_deprecate_table.sql
+++ install/sql/051_deprecate_table.sql
@@ -1,3 +1,8 @@
 -- This deprecates an old table that is not used anymore. It's a dotproject relic
 
 ALTER TABLE  `event_contacts` COMMENT = 'deprecated';
+
+-- This handles converting a couple tables from their original character set to the proper utf8
+
+ALTER TABLE `user_prefs_list` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE `contacts_methods` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
