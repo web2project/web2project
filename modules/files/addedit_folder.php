e88109ce1332095e193f368d5cb1f55ea3f7160f
Keith Casey <keith@caseysoftware.com>
2014-04-30 14:46:16 -0500

    updated the module to use the tempting

--- modules/files/addedit_folder.php
+++ modules/files/addedit_folder.php
@@ -73,35 +73,4 @@ function delIt() {
 </script>
 <?php
 
-$form = new w2p_Output_HTML_FormHelper($AppUI);
-
-?>
-<form name="folderFrm" action="?m=<?php echo $m; ?>" enctype="multipart/form-data" method="post" class="addedit files-folder">
-	<input type="hidden" name="dosql" value="do_folder_aed" />
-	<input type="hidden" name="del" value="0" />
-	<input type="hidden" name="file_folder_id" value="<?php echo $folder_id; ?>" />
-	<input type="hidden" name="redirect" value="<?php echo $referrer; ?>" />
-    <?php echo $form->addNonce(); ?>
-
-    <div class="std addedit departments">
-        <div class="column left">
-            <p>
-                <?php $form->showLabel('Subfolder of'); ?>
-                <?php
-                $parent_folder = ($folder_id > 0) ? $folder->file_folder_parent : $file_folder_parent;
-                echo arraySelect($folders, 'file_folder_parent', 'style="width:175px;" class="text"', $parent_folder);
-                ?>
-            </p>
-            <p>
-                <?php $form->showLabel('Folder Name'); ?>
-                <?php $form->showField('file_folder_name', $folder->file_folder_name, array('maxlength' => 255)); ?>
-            </p>
-            <p>
-                <?php $form->showLabel('Description'); ?>
-                <?php $form->showField('file_folder_description', $folder->file_folder_description); ?>
-            </p>
-            <?php $form->showCancelButton(); ?>
-            <?php $form->showSaveButton(); ?>
-        </div>
-    </div>
-</form>
+include $AppUI->getTheme()->resolveTemplate('files/addedit_folder');
