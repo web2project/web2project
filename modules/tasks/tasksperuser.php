43ea7579fc0b309b4fd68270d8897b9107ff0678
Keith Casey <keith@caseysoftware.com>
2014-05-26 13:47:40 -0500

    fixed the button alignment

--- modules/tasks/tasksperuser.php
+++ modules/tasks/tasksperuser.php
@@ -30,8 +30,8 @@ $compFilter = arrayMerge(array('all' => $AppUI->_('All Companies')), $companies)
 
 // setup the title block
 $titleBlock = new w2p_Theme_TitleBlock('Tasks per User', 'icon.png', $m);
-$titleBlock->addCell($AppUI->_('Company') . ':');
 $titleBlock->addCell(arraySelect($compFilter, 'company_id', 'size="1" class="text" onChange="document.companyFilter.submit();"', $company_id, false), '', '<form action="?m=tasks&amp;a=tasksperuser" method="post" name="companyFilter" accept-charset="utf-8">', '</form>');
+$titleBlock->addCell($AppUI->_('Company') . ':');
 $titleBlock->addCrumb('?m=tasks', 'tasks list');
 $titleBlock->addCrumb('?m=tasks&amp;a=todo&amp;user_id=' . $user_id, 'my todo');
 $titleBlock->show();
