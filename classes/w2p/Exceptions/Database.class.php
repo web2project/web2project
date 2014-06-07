979a74c5f23103da34be208bb8b74d0fcf9c8230
Keith Casey <keith@caseysoftware.com>
2014-06-07 15:14:41 -0500

    broke the exceptions into their own namespace

---
+++ classes/w2p/Exceptions/Database.class.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Class w2p_Exception_Database
+ *
+ * This is the overall exception handler for the Database classes. Initially it doesn't do anything but by
+ *   separating it out, we can catch it separately and do things with it.
+ *
+ * @package     web2project\exceptions
+ */
+class w2p_Exceptions_Database extends Exception { }
---
+++ classes/w2p/Exceptions/FileSystem.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Class w2p_Exception_FileSystem
+ *
+ * This is the overall exception handler for the FileSystem classes. Initially it doesn't do anything but by
+ *   separating it out, we can catch it separately and do things with it.
+ *
+ * @package     web2project\exceptions
+ */
+
+class w2p_Exception_FileSystem extends Exception { }
