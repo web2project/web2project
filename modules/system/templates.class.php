fd51c80327a07702e6c31988db2c1ebebffade28
Keith Casey <keith@caseysoftware.com>
2014-06-14 23:33:50 -0500

    built a proper template class based on the base object

---
+++ modules/system/templates.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package     web2project\modules\core
+ */
+
+class CSystem_Template extends w2p_Core_BaseObject
+{
+    public $email_template_id = null;
+    public $email_template_name = '';
+    public $email_template_identifier = null;
+    public $email_template_language = null;
+    public $email_template_subject = null;
+    public $email_template_body = null;
+
+    public function __construct()
+    {
+        parent::__construct('email_templates', 'email_template_id', 'system');
+    }
+}
