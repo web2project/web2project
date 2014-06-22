61b2d55714c6a715ab6fb2dae68282d0ea88a19b
Keith Casey <keith@caseysoftware.com>
2014-06-21 23:13:02 -0500

    refactored the controller to match our common pattern, extending a core controller next

--- modules/system/roles/do_perms_aed.php
+++ modules/system/roles/do_perms_aed.php
@@ -3,31 +3,27 @@ if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
 
-$del = (int) w2PgetParam($_POST, 'del', 0);
+$delete = (int) w2PgetParam($_POST, 'del', 0);
 
 $perms = &$AppUI->acl();
 if (!canEdit('system')) {
 	$AppUI->redirect(ACCESS_DENIED);
 }
 
-
-
-
 $obj = &$AppUI->acl();
 
-$AppUI->setMsg('Permission');
-if ($del) {
-	if ($obj->del_acl($_POST['permission_id'])) {
-		$AppUI->setMsg('deleted', UI_MSG_ALERT, true);
-		$obj->removeACLPermissions(w2PgetParam($_POST, 'permission_id', null));
-	} else {
-		$AppUI->setMsg($obj->msg(), UI_MSG_ERROR);
-	}
+$action = ($delete) ? 'deleted' : 'stored';
+
+$success = ($delete) ?
+    $obj->del_acl((int) $_POST['permission_id']) :
+    $obj->addRolePermission();
+
+if ($success) {
+    if ($delete) {
+        $obj->removeACLPermissions(w2PgetParam($_POST, 'permission_id', null));
+    }
 } else {
-	if ($obj->addRolePermission()) {
-		$AppUI->setMsg('added', UI_MSG_OK, true);
-	} else {
-		$AppUI->setMsg($obj->msg(), UI_MSG_ERROR);
-	}
+    $AppUI->setMsg($obj->msg(), UI_MSG_ERROR);
 }
+
 $AppUI->redirect('m=system&u=roles');
