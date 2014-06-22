954eed5c2678a2b103f26cdb21a3896a4baa563d
Keith Casey <keith@caseysoftware.com>
2014-06-22 00:05:52 -0500

    wired up a controller to support the Roles management

--- modules/system/roles/do_role_aed.php
+++ modules/system/roles/do_role_aed.php
@@ -3,29 +3,10 @@ if (!defined('W2P_BASE_DIR')) {
 	die('You should not access this file directly.');
 }
 
-if (!canEdit('roles')) {
-	$AppUI->redirect(ACCESS_DENIED);
-}
+$delete = (int) w2PgetParam($_POST, 'del', 0);
 
-$del = (int) w2PgetParam($_POST, 'del', 0);
+$controller = new \Web2project\Actions\AddEditRoles(new CSystem_Role(), $delete, 'Role',
+    'm=system&u=roles', 'm=system&u=roles');
 
-$role = new CSystem_Role();
-
-if (!$role->bind($_POST)) {
-	$AppUI->setMsg($msg, UI_MSG_ERROR);
-    $AppUI->redirect('m=system&u=roles');
-}
-
-$action = ($del) ? 'deleted' : 'stored';
-$success = ($del) ? $role->delete() : $role->store();
-
-if ($success) {
-    $AppUI->setMsg('Role '.$action, UI_MSG_OK, true);
-    $copy_role_id = w2PgetParam($_POST, 'copy_role_id', null);
-    if ($copy_role_id) {
-        $role->copyPermissions($copy_role_id, $role->role_id);
-    }
-} else {
-    $AppUI->setMsg($role->getError(), UI_MSG_ERROR);
-}
-$AppUI->redirect('m=system&u=roles');
+$AppUI = $controller->process($AppUI, $_POST);
+$AppUI->redirect($controller->resultPath);
