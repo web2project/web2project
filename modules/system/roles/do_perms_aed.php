0d25ed3058ce6aeb00983292c8ff88224b5e191a
Keith Casey <keith@caseysoftware.com>
2014-06-21 23:24:41 -0500

    wired up a controller to support the Role Permssion management

--- modules/system/roles/do_perms_aed.php
+++ modules/system/roles/do_perms_aed.php
@@ -5,25 +5,8 @@ if (!defined('W2P_BASE_DIR')) {
 
 $delete = (int) w2PgetParam($_POST, 'del', 0);
 
-$perms = &$AppUI->acl();
-if (!canEdit('system')) {
-	$AppUI->redirect(ACCESS_DENIED);
-}
+$controller = new \Web2project\Actions\AddEditRolePermissions($AppUI->acl(), $delete, 'Role Permission',
+    'm=system&u=roles', 'm=system&u=roles');
 
-$obj = &$AppUI->acl();
-
-$action = ($delete) ? 'deleted' : 'stored';
-
-$success = ($delete) ?
-    $obj->del_acl((int) $_POST['permission_id']) :
-    $obj->addRolePermission();
-
-if ($success) {
-    if ($delete) {
-        $obj->removeACLPermissions(w2PgetParam($_POST, 'permission_id', null));
-    }
-} else {
-    $AppUI->setMsg($obj->msg(), UI_MSG_ERROR);
-}
-
-$AppUI->redirect('m=system&u=roles');
+$AppUI = $controller->process($AppUI, $_POST);
+$AppUI->redirect($controller->resultPath);
