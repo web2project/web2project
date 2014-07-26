7b6d3afc793b2e3ecb108a7f3665c714873cead5
Keith Casey <keith@caseysoftware.com>
2014-07-25 20:56:23 -0500

    reordering the controller to be a little more consistent with the others

--- modules/events/do_event_aed.php
+++ modules/events/do_event_aed.php
@@ -5,15 +5,15 @@ if (!defined('W2P_BASE_DIR')) {
 
 $delete = (int) w2PgetParam($_POST, 'del', 0);
 
-$controller = new \Web2project\Actions\AddEdit(
-    new CEvent(), $delete, 'Event', 'm=events', 'm=events&a=addedit'
-);
-
 $start_date = new w2p_Utilities_Date($_POST['event_start_date'] . $_POST['start_time']);
 $_POST['event_start_date'] = $start_date->format(FMT_DATETIME_MYSQL);
 
 $end_date = new w2p_Utilities_Date($_POST['event_end_date'] . $_POST['end_time']);
 $_POST['event_end_date'] = $end_date->format(FMT_DATETIME_MYSQL);
+
+$controller = new \Web2project\Actions\AddEdit(
+    new CEvent(), $delete, 'Event', 'm=events', 'm=events&a=addedit'
+);
 
 $AppUI = $controller->process($AppUI, $_POST);
 $AppUI->redirect($controller->resultPath);
