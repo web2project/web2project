a581b9013575f5804f4ca4da41f6bac0c3f2cfc7
Keith Casey <keith@caseysoftware.com>
2014-08-16 21:53:59 -0500

    Restored milestone logo
    Resolves http://bugs.web2project.net/view.php?id=1597

--- includes/version.php
+++ includes/version.php
@@ -2,5 +2,5 @@
 // Set the version components separately.
 $w2p_version_major = 3;
 $w2p_version_minor = 2;
-$w2p_version_patch = 57; // Can be set to null if not used
+$w2p_version_patch = 62; // Can be set to null if not used
 $w2p_version_prepatch = null; // Set to null if a release version.
