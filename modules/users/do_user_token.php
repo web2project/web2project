98dff29d4818820a79832ed7770c303256542ae2
Keith Casey <keith@caseysoftware.com>
2014-09-08 22:42:57 -0500

    corrected the return path after a user feed token is set

--- modules/users/do_user_token.php
+++ modules/users/do_user_token.php
@@ -6,8 +6,8 @@ $user_id = w2PgetParam($_POST, 'user_id', '');
 $user = new CUser();
 $user->generateToken($user_id, $token);
 
-if ($userId) {
-    $redirect = 'm=users&a=view&user_id='.$userId;
+if ($user_id) {
+    $redirect = 'm=users&a=view&user_id='.$user_id;
 } else {
     $redirect = 'm=users';
 }
