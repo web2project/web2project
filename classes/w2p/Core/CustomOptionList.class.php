256aaf33852ca52b66dd55f9e036535b97e06e13
Keith Casey <keith@caseysoftware.com>
2014-10-11 16:12:03 -0500

    removing the deprecated method calls

--- classes/w2p/Core/CustomOptionList.class.php
+++ classes/w2p/Core/CustomOptionList.class.php
@@ -20,12 +20,10 @@ class w2p_Core_CustomOptionList {
         $q->addTable('custom_fields_lists');
         $q->addWhere('field_id = ' . $this->field_id);
         $q->addOrder('list_value');
-        if (!$q->exec()) {
-            return $db->ErrorMsg();
-        }
+        $rows = $q->loadHashList('list_option_id');
 
-        while ($opt_row = $q->fetchRow()) {
-            $this->options[$opt_row['list_option_id']] = $opt_row['list_value'];
+        foreach($rows as $row) {
+            $this->options[$row['list_option_id']] = $row['list_value'];
         }
     }
 
