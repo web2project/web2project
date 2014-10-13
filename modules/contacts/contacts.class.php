964d369ebb151e604cbe5bb2da56827cddcd6d5e
Keith Casey <keith@caseysoftware.com>
2014-10-13 00:47:44 -0500

    simplifying the query

--- modules/contacts/contacts.class.php
+++ modules/contacts/contacts.class.php
@@ -487,9 +487,8 @@ class CContact extends w2p_Core_BaseObject
         $q->addQuery('contact_display_name as contact_name');
         $q->addJoin('contacts', 'con', 'contact_id = user_contact', 'inner');
         $q->addWhere('user_id = ' . (int) $userId);
-        $q->setLimit(1);
-        $r = $q->loadList();
-        $result = (is_array($r) && isset($r[0])) ? $r[0]['contact_name'] : 'User Not Found';
+        $r = $q->loadHash();
+        $result = is_array($r) ? $r['contact_name'] : 'User Not Found';
 
         return $result;
     }
--- modules/users/users.class.php
+++ modules/users/users.class.php
@@ -192,7 +192,8 @@ class CUser extends w2p_Core_BaseObject
         return w2PgetUsers();
     }
 
-    public function load($userId = null, $unused = true)
+    /** @deprecated */
+    public function loadFull($userId)
     {
         $q = $this->_getQuery();
         $q->addTable('users', 'u');
