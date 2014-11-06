7d9b9c43423ae14545d9b4812b157f781f2deb10
Keith Casey <keith@caseysoftware.com>
2014-11-05 22:18:04 -0600

    shifting the tests to the mock for easier testing

--- unit_tests/src/Web2project/Mocks/EmailTest.php
+++ unit_tests/src/Web2project/Mocks/EmailTest.php
@@ -17,10 +17,70 @@
 
 class w2p_Mocks_EmailTest extends CommonSetup
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->obj = new w2p_Utilities_Mail();
+    }
+
     public function testW2Pacl_nuclear()
     {
-        $object = new w2p_Mocks_Email();
+        $object = new \Web2project\Mocks\Email();
 
         $this->assertTrue($object->Send());
+    }
+
+    public function testSubject()
+    {
+        $this->assertEquals('', $this->obj->Subject);
+
+        $this->obj->Subject('This is a subject');
+        $this->assertEquals('[web2Project] This is a subject', $this->obj->Subject);
+    }
+
+    public function testFrom()
+    {
+        $result = $this->obj->From(null);
+        $this->assertFalse($result);
+    }
+
+    public function testReplyTo()
+    {
+        $result = $this->obj->From(null);
+        $this->assertFalse($result);
+    }
+
+    public function testReceipt()
+    {
+        $this->obj->Receipt();
+        $this->assertTrue($this->obj->receipt);
+    }
+
+    public function testValidEmail()
+    {
+        $result = $this->obj->ValidEmail('admin@web2project.net');
+        $this->assertTrue($result);
+
+        $result = $this->obj->ValidEmail('admin+web2project.net');
+        $this->assertFalse($result);
+
+        $result = $this->obj->ValidEmail('admin');
+        $this->assertFalse($result);
+
+        $result = $this->obj->ValidEmail('web2project.net');
+        $this->assertFalse($result);
+    }
+
+    public function testPriority()
+    {
+        $this->obj->Priority(3);
+        $this->assertEquals(3, $this->obj->Priority);
+
+        $this->obj->Priority(0);
+        $this->assertEquals(1, $this->obj->Priority);
+
+        $this->obj->Priority(12);
+        $this->assertEquals(5, $this->obj->Priority);
     }
 }
