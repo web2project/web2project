68f16699c7c17a1e3b466668c696b2e72db1ff1f
Keith Casey <keith@caseysoftware.com>
2014-11-09 14:25:05 -0600

    wired a simple mock base object so we can override permissions

---
+++ src/Web2project/Mocks/BaseObject.php
@@ -0,0 +1,39 @@
+<?php
+namespace Web2project\Mocks;
+/**
+ * @package     web2project\mocks
+ * @author      D. Keith Casey, Jr. <caseydk@users.sourceforge.net>
+ */
+
+class BaseObject extends \w2p_Core_BaseObject
+{
+    public $deletable  = true;
+    public $viewable   = true;
+    public $editable   = true;
+    public $accessible = true;
+
+    public function __construct()
+    {
+        parent::__construct('no table', 'no key');
+    }
+
+    public function canDelete()
+    {
+        return $this->deletable;
+    }
+
+    public function canView()
+    {
+        return $this->viewable;
+    }
+
+    public function canEdit()
+    {
+        return $this->editable;
+    }
+
+    public function canAccess()
+    {
+        return $this->accessible;
+    }
+}
