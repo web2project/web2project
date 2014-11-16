7cf6f2ff7bf47b7d0fe2348b3246c2136b786cc9
Keith Casey <keith@caseysoftware.com>
2014-11-15 22:55:58 -0600

    Fixed an issue where the custom fields weren't showng in the edit view
    Resolves http://bugs.web2project.net/view.php?id=1639

--- style/_common/tasks/addedit_desc.php
+++ style/_common/tasks/addedit_desc.php
@@ -1,3 +1,8 @@
+<?php
+
+// @note Not sure why this is necessary as it's not in the other addedit.php templates.. maybe because this is a sub-template?
+global $m;
+?>
 <form action="?m=tasks&a=addedit&task_project=<?php echo $task_project; ?>" method="post" name="detailFrm" accept-charset="utf-8">
     <input type="hidden" name="dosql" value="do_task_aed" />
     <input type="hidden" name="task_id" value="<?php echo $object->getId(); ?>" />
