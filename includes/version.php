1854d8782ec746582006a4626214a5a69cf21411
Keith Casey <keith@caseysoftware.com>
2014-12-01 00:10:30 -0600

    correcting the version number

--- includes/version.php
+++ includes/version.php
@@ -2,5 +2,5 @@
 // Set the version components separately.
 $w2p_version_major = 3;
 $w2p_version_minor = 3;
-$w2p_version_patch = 20; // Can be set to null if not used
+$w2p_version_patch = 21; // Can be set to null if not used
 $w2p_version_prepatch = null; // Set to null if a release version.
