1e9b096ea10e570a05d7834aa4f2635781733168
Keith Casey <keith@caseysoftware.com>
2015-03-10 23:56:18 -0500

    fixed it so the button displays even if there's not a calendar url
    Ref:
    https://github.com/mirage335/web2project/commit/4c514e34fe1363ee172e3c2c
    8c47ff3123501801

--- style/_common/users/view.php
+++ style/_common/users/view.php
@@ -46,14 +46,14 @@ $view = new w2p_Output_HTML_ViewHelper($AppUI);
             <?php if ($user->feed_token != '') {
                 $calendarFeed = W2P_BASE_URL.'/calendar.php?token='.$user->feed_token.'&amp;ext=.ics';
                 ?>
-                <form name="regenerateToken" action="./index.php?m=users" method="post" accept-charset="utf-8">
-                    <input type="hidden" name="user_id" value="<?php echo (int) $user->user_id; ?>" />
-                    <input type="hidden" name="dosql" value="do_user_token" />
-                    <input type="hidden" name="token" value="<?php echo $user->feed_token; ?>" />
-                    <input type="submit" name="regenerate token" value="<?php echo $AppUI->_('regenerate feed url'); ?>" class="button btn btn-primary btn-mini" />
-                </form>
                 <a href="<?php echo $calendarFeed; ?>">calendar feed</a>
             <?php } ?>
+            <form name="regenerateToken" action="./index.php?m=users" method="post" accept-charset="utf-8">
+                <input type="hidden" name="user_id" value="<?php echo (int) $user->user_id; ?>" />
+                <input type="hidden" name="dosql" value="do_user_token" />
+                <input type="hidden" name="token" value="<?php echo $user->feed_token; ?>" />
+                <input type="submit" name="regenerate token" value="<?php echo $AppUI->_('regenerate feed url'); ?>" class="button btn btn-primary btn-mini" />
+            </form>
         </p>
         <p><?php $view->showLabel('Signature'); ?>
             <?php $view->showField('user_signature', $user->user_signature); ?>
