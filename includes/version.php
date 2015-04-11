e787d0beab29b4190eb7219dac6a36bcddbe68a7
Keith Casey <keith@caseysoftware.com>
2015-04-11 00:43:40 -0500

    allowing periods in the username

--- includes/version.php
+++ includes/version.php
@@ -2,5 +2,5 @@
 // Set the version components separately.
 $w2p_version_major = 3;
 $w2p_version_minor = 3;
-$w2p_version_patch = 64; // Can be set to null if not used
+$w2p_version_patch = 65; // Can be set to null if not used
 $w2p_version_prepatch = null; // Set to null if a release version.
