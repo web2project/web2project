5253c6229f35d6b06aa5fe7fd271192133bdfbaf
Klaus Buecher <opto@optosolar.com>
2016-07-05 22:46:12 +0300

    subsubversion bump

--- includes/version.php
+++ includes/version.php
@@ -3,4 +3,4 @@
 $w2p_version_major = 3;
 $w2p_version_minor = 4;
 $w2p_version_patch = 00; // Can be set to null if not used
-$w2p_version_prepatch = 'pre2'; // Set to null if a release version.
+$w2p_version_prepatch = 'pre3'; // Set to null if a release version.
