c29cea0a3cc96aaa9a5b6c74eac4e378eedf874a
Keith Casey <keith@caseysoftware.com>
2023-12-28 23:16:46 -0600

    Restoring the auto increment to the user_access_log

--- install/sql/059_pre_v4.0_fixes.sql
+++ install/sql/059_pre_v4.0_fixes.sql
@@ -1,2 +1,6 @@
 
+-- This changes the column to meet our naming convention to make our auto-formatting work
 ALTER TABLE `files` CHANGE `file_date` `file_datetime` DATETIME NULL DEFAULT NULL;
+
+-- The user_access_log table was recreated in update 056 and I left out the auto_increment 
+ALTER TABLE `user_access_log` CHANGE `user_access_log_id` `user_access_log_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;
