889119fbfe44309446ce4dc5e56dfdb6d4204c87
Keith Casey <keith@caseysoftware.com>
2023-12-28 23:47:43 -0600

    updating the budget table with defaults

--- install/sql/059_pre_v4.0_fixes.sql
+++ install/sql/059_pre_v4.0_fixes.sql
@@ -4,3 +4,9 @@ ALTER TABLE `files` CHANGE `file_date` `file_datetime` DATETIME NULL DEFAULT NUL
 
 -- The user_access_log table was recreated in update 056 and I left out the auto_increment 
 ALTER TABLE `user_access_log` CHANGE `user_access_log_id` `user_access_log_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+-- Setting up the defaults for the budgets table
+ALTER TABLE `budgets_assigned` CHANGE `budget_project` `budget_project` INT(11) NOT NULL DEFAULT '0';
+ALTER TABLE `budgets_assigned` CHANGE `budget_task` `budget_task` INT(11) NOT NULL DEFAULT '0';
+ALTER TABLE `budgets_assigned` CHANGE `budget_category` `budget_category` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0';
+ALTER TABLE `budgets_assigned` CHANGE `budget_amount` `budget_amount` DECIMAL(10,2) NOT NULL DEFAULT '0';
