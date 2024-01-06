3fc93123c3b858258674fc60eb5ee0f82a8e5e8b
Keith Casey <keith@caseysoftware.com>
2024-01-05 23:22:31 -0600

    updating events

---
+++ install/sql/060_pre_v4.0_standardizing.sql
@@ -0,0 +1,10 @@
+
+-- Updating column names to leverage our naming conventions and simplify the templating
+
+-- events
+ALTER TABLE `events` CHANGE `event_start_date` `event_start_datetime` DATETIME NULL DEFAULT NULL;
+ALTER TABLE `events` CHANGE `event_end_date` `event_end_datetime` DATETIME NULL DEFAULT NULL;
+
+UPDATE `module_config` SET `module_config_value` = 'event_start_datetime' WHERE `module_config_value` = 'event_start_date'
+UPDATE `module_config` SET `module_config_value` = 'event_end_datetime' WHERE `module_config_value` = 'event_end_date'
+
