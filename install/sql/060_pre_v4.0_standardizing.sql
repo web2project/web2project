7e37cfbe1f17126353db9867af42d844f01ae1ea
Keith Casey <keith@caseysoftware.com>
2024-01-06 13:33:39 -0600

    updating the forums module

--- install/sql/060_pre_v4.0_standardizing.sql
+++ install/sql/060_pre_v4.0_standardizing.sql
@@ -11,19 +11,24 @@ UPDATE `module_config` SET `module_config_value` = 'event_end_datetime' WHERE `m
 ALTER TABLE `files` CHANGE `file_datetime` `file_datetime` DATETIME NULL DEFAULT CURRENT_TIMESTAMP;
 UPDATE `module_config` SET `module_config_value` = 'file_datetime' WHERE `module_config_value` = 'file_date';
 
+-- forums
+ALTER TABLE `forums` CHANGE `forum_create_date` `forum_created` DATETIME NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `forums` CHANGE `forum_last_date` `forum_updated` DATETIME NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `forum_messages` CHANGE `message_date` `message_datetime` DATETIME NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `forum_visits` CHANGE `visit_datetime` `visit_datetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+UPDATE `module_config` SET `module_config_value` = 'forum_created' WHERE `module_config_value` = 'forum_create_date';
+UPDATE `module_config` SET `module_config_value` = 'forum_updated' WHERE `module_config_value` = 'forum_last_date';
+UPDATE `module_config` SET `module_config_value` = 'message_datetime' WHERE `module_config_value` = 'message_date';
+
 -- history
 ALTER TABLE `history` CHANGE `history_date` `history_datetime` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
-
 UPDATE `module_config` SET `module_config_value` = 'history_datetime' WHERE `module_config_value` = 'history_date';
 
 -- links
 ALTER TABLE `links` CHANGE `link_date` `link_datetime` DATETIME NULL DEFAULT CURRENT_TIMESTAMP;
-
 UPDATE `module_config` SET `module_config_value` = 'link_datetime' WHERE `module_config_value` = 'link_date';
 
 -- user_access_log
 ALTER TABLE `user_access_log` CHANGE `date_time_in` `date_time_in` DATETIME NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE `user_access_log` CHANGE `date_time_out` `date_time_out` DATETIME NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE `user_access_log` CHANGE `date_time_last_action` `date_time_last_action` DATETIME NULL DEFAULT CURRENT_TIMESTAMP;
-
-
