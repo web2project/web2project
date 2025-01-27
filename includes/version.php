43586bde2143bde9f3523b69a763d8fb473400a0
Keith Casey <caseysoftware@users.noreply.github.com>
2025-01-27 01:05:15 -0600

    tagging new version

--- includes/version.php
+++ includes/version.php
@@ -2,5 +2,5 @@
 // Set the version components separately.
 $w2p_version_major = 4;
 $w2p_version_minor = 0;
-$w2p_version_patch = 0; // Can be set to null if not used
-$w2p_version_prepatch = 'rc1'; // Set to null if a release version.
+$w2p_version_patch = 1; // Can be set to null if not used
+$w2p_version_prepatch = null; // Set to null if a release version.
