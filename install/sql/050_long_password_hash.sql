315cfad4500a9389f0c8fb5b6ed49d174eeec0bd
Danger Casey <caseysoftware@users.noreply.github.com>
2026-03-10 14:02:52 -0400

    reworked the users table

--- install/sql/050_long_password_hash.sql
+++ install/sql/050_long_password_hash.sql
@@ -1,4 +1,7 @@
 -- This prepares us to eventually kill off the md5-based hashing of passwords.
 
-ALTER TABLE  `users` CHANGE  `user_password`  `user_password` VARCHAR( 255 ) CHARACTER
-    SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '';
+# 2026 Update - eliminating '0000-00-00 00:00:00' as values
+UPDATE `users` SET `user_birthday` = NULL WHERE CAST(`user_birthday` AS CHAR(20)) = '0000-00-00 00:00:00';
+
+ALTER TABLE  `users` CHANGE  `user_password`  `user_password` VARCHAR( 255 ) 
+    CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '';
