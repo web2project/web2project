9edec5a4032b73c2076a41b82f618c1dfe303cf7
Danger Casey <caseysoftware@users.noreply.github.com>
2026-03-10 01:19:16 -0400

    more cleanup on the contacts table to handle nulls appropriately

--- install/sql/026_contact_methods_phone.sql
+++ install/sql/026_contact_methods_phone.sql
@@ -3,25 +3,25 @@
 --   bit more smoothly.
 
 ALTER TABLE `contacts`
-    ADD `contact_email` VARCHAR( 255 ) NOT NULL AFTER `contact_type` ,
-    ADD `contact_phone` VARCHAR( 30 ) NOT NULL AFTER `contact_email` ;
+    ADD `contact_email` VARCHAR( 255 ) NULL DEFAULT '' AFTER `contact_type` ,
+    ADD `contact_phone` VARCHAR( 30 )  NULL DEFAULT '' AFTER `contact_email` ;
 
 UPDATE contacts c
-  SET contact_phone = (
-      SELECT method_value
-          FROM contacts_methods cm where method_name = 'phone_primary'
-          AND cm.contact_id = c.contact_id
-);
+    SET contact_phone = (
+        SELECT method_value
+            FROM contacts_methods cm where method_name = 'phone_primary'
+            AND cm.contact_id = c.contact_id
+    );
 
 DELETE FROM `contacts_methods` WHERE `method_name` IN ('phone_primary');
 DELETE FROM `sysvals` WHERE `sysval_title` = 'ContactMethods' AND
     `sysval_value_id` = 'phone_primary';
 
 UPDATE contacts c
-  SET contact_email = (
-      SELECT method_value
-          FROM contacts_methods cm where method_name = 'email_primary'
-          AND cm.contact_id = c.contact_id
+    SET contact_email = (
+        SELECT method_value
+            FROM contacts_methods cm where method_name = 'email_primary'
+            AND cm.contact_id = c.contact_id
 );
 
 DELETE FROM `contacts_methods` WHERE `method_name` = 'email_primary';
--- install/sql/033_database_cleanup2.sql
+++ install/sql/033_database_cleanup2.sql
@@ -1,7 +1,9 @@
 -- Minor database cleanups
 
+UPDATE `contacts` SET `contact_email` = NULL WHERE LENGTH(`contact_email`) = 0;
 ALTER TABLE `contacts` CHANGE `contact_email` `contact_email` VARCHAR( 255 )
     CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
 
+UPDATE `contacts` SET `contact_phone` = NULL WHERE LENGTH(`contact_phone`) = 0;
 ALTER TABLE `contacts` CHANGE `contact_phone` `contact_phone` VARCHAR( 30 )
     CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
