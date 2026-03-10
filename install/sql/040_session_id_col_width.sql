195927dbdbc0da9f13243b0a5096b32e1e0a7528
Danger Casey <caseysoftware@users.noreply.github.com>
2026-03-10 13:53:53 -0400

    reworked the sessions table

--- install/sql/040_session_id_col_width.sql
+++ install/sql/040_session_id_col_width.sql
@@ -2,5 +2,10 @@
 -- Increase the column width on the session_id
 -- to be compatible with PHP 5.3 hash functions
 
+# 2026 Update - eliminating '0000-00-00 00:00:00' as defaults
+ALTER TABLE `sessions`
+    MODIFY `session_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    MODIFY `session_updated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
 ALTER TABLE `sessions` CHANGE `session_id` `session_id` VARCHAR( 128 ) 
     CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
