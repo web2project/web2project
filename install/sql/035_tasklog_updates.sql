96ee55827b0c53a19cf520f06caa55f8fc893c0f
Danger Casey <caseysoftware@users.noreply.github.com>
2026-03-10 12:51:51 -0400

    Reworked the task_logs table

--- install/sql/035_tasklog_updates.sql
+++ install/sql/035_tasklog_updates.sql
@@ -4,20 +4,18 @@
 
 ALTER TABLE  `task_log` ADD  `task_log_percent_complete` TINYINT( 4 ) NOT NULL DEFAULT '0' AFTER `task_log_date`;
 
-UPDATE `task_log` SET `task_log_percent_complete` = 
-(
-    SELECT `task_percent_complete` FROM `tasks` 
+UPDATE `task_log` SET `task_log_percent_complete` = IFNULL(
+    (SELECT `task_percent_complete` FROM `tasks` 
         WHERE tasks.task_id = task_log_task
-        GROUP BY tasks.task_id
+        GROUP BY tasks.task_id), 0
 );
 
-ALTER TABLE  `task_log` ADD  `task_log_task_end_date` DATETIME NOT NULL AFTER  `task_log_percent_complete`;
+ALTER TABLE  `task_log` ADD  `task_log_task_end_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `task_log_percent_complete`;
 
-UPDATE `task_log` SET `task_log_task_end_date` = 
-(
-    SELECT `task_end_date` FROM `tasks` 
+UPDATE `task_log` SET `task_log_task_end_date` = IFNULL(
+    (SELECT `task_end_date` FROM `tasks` 
         WHERE tasks.task_id = task_log_task
-        GROUP BY tasks.task_id
+        GROUP BY tasks.task_id), NOW()
 );
 
 -- These are some random fixes scheduled for this release.
